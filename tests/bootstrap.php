255f041f7d15943aaef2c581bc5e4973c3a8a936
TomasVotruba <tomas.vot@gmail.com>
2017-04-14 19:22:11 +0200

    subsplit fixes

---
+++ tests/bootstrap.php
@@ -0,0 +1,6 @@
+<?php declare(strict_types=1);
+
+require __DIR__ . '/../vendor/autoload.php';
+
+// packages/sniff-runner
+Symplify\EasyCodingStandard\SniffRunner\Legacy\LegacyCompatibilityLayer::add();
