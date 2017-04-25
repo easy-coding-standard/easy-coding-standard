ef21050ed6a303a5b70514891c412de7bf11541a
TomasVotruba <tomas.vot@gmail.com>
2017-04-26 00:54:06 +0200

    [EasyCodingStandard] fix compatibility layer

--- src/Configuration/ConfigurationOptions.php
+++ src/Configuration/ConfigurationOptions.php
@@ -4,11 +4,6 @@ namespace Symplify\EasyCodingStandard\Configuration;
 
 final class ConfigurationOptions
 {
-//    /**
-//     * @var string
-//     */
-//    public const SKIP = 'skip';
-//
     /**
      * @var string
      */
