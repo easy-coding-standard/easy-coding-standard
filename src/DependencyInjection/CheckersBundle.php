5272a463e071895a95cc39a234ff2ec59ded2647
TomasVotruba <tomas.vot@gmail.com>
2017-06-12 01:11:07 +0200

    [EasyCodingStandard] use Symfony DI [wip]

---
+++ src/DependencyInjection/CheckersBundle.php
@@ -0,0 +1,66 @@
+<?php declare(strict_types=1);
+
+namespace Symplify\EasyCodingStandard\DependencyInjection;
+
+use Symfony\Component\Config\Loader\DelegatingLoader;
+use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\Config\Loader\LoaderResolver;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpKernel\Bundle\BundleInterface;
+use Symfony\Component\HttpKernel\Kernel;
+use Symplify\EasyCodingStandard\Configuration\Loader\NeonLoader;
+use Symplify\EasyCodingStandard\DependencyInjection\CompilerPass\CollectorCompilerPass;
+
+final class AppKernel extends Kernel
+{
+    /**
+     * @var string
+     */
+    private const CONFIG_NAME = 'easy-coding-standard.neon';
+
+    public function registerContainerConfiguration(LoaderInterface $loader): void
+    {
+        $loader->load(__DIR__ . '/../config/services.yml');
+
+        $localConfig = getcwd() . '/' . self::CONFIG_NAME;
+        if (file_exists($localConfig)) {
+            $loader->load($localConfig);
+        }
+    }
+
+    /**
+     * @return BundleInterface[]
+     */
+    public function registerBundles(): array
+    {
+        return [];
+    }
+
+    public function getCacheDir(): string
+    {
+        return sys_get_temp_dir() . '/_easy_coding_standard_cache';
+    }
+
+    public function getLogDir(): string
+    {
+        return sys_get_temp_dir() . '/_easy_coding_standard_log';
+    }
+
+    protected function build(ContainerBuilder $containerBuilder): void
+    {
+        $containerBuilder->addCompilerPass(new CollectorCompilerPass);
+    }
+
+    protected function getContainerLoader(ContainerInterface $container): DelegatingLoader
+    {
+        /** @var DelegatingLoader $delegationLoader */
+        $delegationLoader = parent::getContainerLoader($container);
+
+        /** @var LoaderResolver $resolver */
+        $resolver = $delegationLoader->getResolver();
+        $resolver->addLoader(new NeonLoader);
+
+        return $delegationLoader;
+    }
+}
---
+++ src/DependencyInjection/Extension/CheckersExtension.php
@@ -0,0 +1,23 @@
+<?php declare(strict_types=1);
+
+namespace Symplify\EasyCodingStandard\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+
+final class CheckersExtension extends Extension
+{
+
+    /**
+     * Loads a specific configuration.
+     *
+     * @param array $configs An array of configuration values
+     * @param ContainerBuilder $container A ContainerBuilder instance
+     *
+     * @throws \InvalidArgumentException When provided tag is not defined in this extension
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        // TODO: Implement load() method.
+    }
+}
