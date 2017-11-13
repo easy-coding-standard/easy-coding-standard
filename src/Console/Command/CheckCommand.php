8a87b6f8600764d94c682a21c17aa46eb923bb20
TomasVotruba <tomas.vot@gmail.com>
2017-11-13 20:02:26 +0100

    fix cs

--- src/Console/Command/CheckCommand.php
+++ src/Console/Command/CheckCommand.php
@@ -2,7 +2,6 @@
 
 namespace Symplify\EasyCodingStandard\Console\Command;
 
-use Missing;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
