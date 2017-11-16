eacad150a752ec6b3b34da9c5155b039cea9b952
TomasVotruba <tomas.vot@gmail.com>
2017-11-16 03:05:48 +0100

    fix cs

--- packages/FixerRunner/src/Application/FixerFileProcessor.php
+++ packages/FixerRunner/src/Application/FixerFileProcessor.php
@@ -127,7 +127,6 @@ final class FixerFileProcessor implements FileProcessorInterface
             $appliedFixers[] = $fixer->getName();
         }
 
-
         if (! $appliedFixers) {
             Tokens::clearCache();
             return;
