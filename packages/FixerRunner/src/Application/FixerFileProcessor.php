05cd270bb5305d3f20c02d18439bdb177ab1a8b2
TomasVotruba <tomas.vot@gmail.com>
2017-11-16 03:22:56 +0100

    fix cs

--- packages/FixerRunner/src/Application/FixerFileProcessor.php
+++ packages/FixerRunner/src/Application/FixerFileProcessor.php
@@ -79,6 +79,7 @@ final class FixerFileProcessor implements FileProcessorInterface
         if (! $this->areFixersSorted) {
             $this->sortFixers();
         }
+
         return $this->fixers;
     }
 
