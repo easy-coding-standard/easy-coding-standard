e804389f3e3bea5ed95e9212ea26546d721b37ac
TomasVotruba <tomas.vot@gmail.com>
2017-11-23 20:41:44 +0100

    [EasyCodingStandard] add level option to Check command help

--- src/Console/Command/CheckCommand.php
+++ src/Console/Command/CheckCommand.php
@@ -228,7 +228,7 @@ final class CheckCommand extends Command
         if ($totalCheckersLoaded === 0) {
             throw new NoCheckersLoadedException(
                 'No checkers were found. Registers them in your config in "checkers:" '
-                . 'section or load them via "--config <file>.neon" option.'
+                . 'section, load them via "--config <file>.neon" or "--level <level> option.'
             );
         }
     }
