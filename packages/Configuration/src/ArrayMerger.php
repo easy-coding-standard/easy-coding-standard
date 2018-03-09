5b6007af711abb9a35a27607ccdedfe8e9f00a51
TomasVotruba <tomas.vot@gmail.com>
2018-03-09 12:53:57 +0100

    ArrayMerger: put Nette merger version, to keep compatible

--- packages/Configuration/src/ArrayMerger.php
+++ packages/Configuration/src/ArrayMerger.php
@@ -10,6 +10,44 @@ final class ArrayMerger
      */
     public static function mergeRecursively(array $arrays): array
     {
-        return array_merge_recursive(...$arrays);
+        $result = [];
+        foreach ($arrays as $array) {
+            $result = self::merge($result, $array);
+        }
+
+        return $result;
+    }
+
+    /**
+     * With modifications
+     *
+     * @param mixed $left
+     * @param mixed $right
+     * @return mixed
+     *
+     * @author David Grudl
+     * @source https://github.com/nette/di/blob/401dca5375c2bc991bff59e632c89270efc75542/src/DI/Config/Helpers.php#L31-L56
+     */
+    public static function merge($left, $right)
+    {
+        if (is_array($left) && is_array($right)) {
+            foreach ($left as $key => $val) {
+                if (is_int($key)) {
+                    $right[] = $val;
+                } else {
+                    if (isset($right[$key])) {
+                        $val = static::merge($val, $right[$key]);
+                    }
+
+                    $right[$key] = $val;
+                }
+            }
+
+            return $right;
+        } elseif ($left === null && is_array($right)) {
+            return $right;
+        }
+
+        return $left;
     }
 }
