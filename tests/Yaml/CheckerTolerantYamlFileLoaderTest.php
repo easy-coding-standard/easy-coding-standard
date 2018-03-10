5490ac7f641a19136503d959b05ed5afb691a5ce
TomasVotruba <tomas.vot@gmail.com>
2018-03-10 23:02:13 +0100

    add imports test

--- tests/Yaml/CheckerTolerantYamlFileLoaderTest.php
+++ tests/Yaml/CheckerTolerantYamlFileLoaderTest.php
@@ -6,15 +6,23 @@ use PHPUnit\Framework\TestCase;
 use Symplify\EasyCodingStandard\DependencyInjection\ContainerFactory;
 use Symplify\EasyCodingStandard\FixerRunner\Application\FixerFileProcessor;
 
-/**
- * @todo Backbone of everything!
- */
 final class CheckerTolerantYamlFileLoaderTest extends TestCase
 {
     public function test(): void
     {
         $container = (new ContainerFactory())->createWithConfig(
             __DIR__ . '/CheckerTolerantYamlFileLoaderSource/config.yml'
+        );
+
+        /** @var FixerFileProcessor $fixerFileProcessor */
+        $fixerFileProcessor = $container->get(FixerFileProcessor::class);
+        $this->assertCount(1, $fixerFileProcessor->getCheckers());
+    }
+
+    public function testImports(): void
+    {
+        $container = (new ContainerFactory())->createWithConfig(
+            __DIR__ . '/CheckerTolerantYamlFileLoaderSource/config-with-imports.yml'
         );
 
         /** @var FixerFileProcessor $fixerFileProcessor */
