a659356c535efcc806f7495e61d2e22bf8008ae2
TomasVotruba <tomas.vot@gmail.com>
2018-03-10 10:39:00 +0100

    add ExcludedCheckersTest

---
+++ tests/DependencyInjection/ExcludedCheckersTest.php
@@ -0,0 +1,21 @@
+<?php declare(strict_types=1);
+
+namespace Symplify\EasyCodingStandard\Tests\DependencyInjection;
+
+use PHPUnit\Framework\TestCase;
+use Symplify\EasyCodingStandard\DependencyInjection\ContainerFactory;
+use Symplify\EasyCodingStandard\FixerRunner\Application\FixerFileProcessor;
+use Symplify\EasyCodingStandard\SniffRunner\Application\SniffFileProcessor;
+
+final class ExcludedCheckersTest extends TestCase
+{
+    public function test(): void
+    {
+        $container = (new ContainerFactory())->createWithConfig(
+            __DIR__ . '/ExcludedCheckersSource/config.yml'
+        );
+
+        $fixerFileProcessor = $container->get(FixerFileProcessor::class);
+        $this->assertCount(0, $fixerFileProcessor->getCheckers());
+    }
+}
