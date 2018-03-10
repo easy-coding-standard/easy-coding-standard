4473e9f2c7cea16c8b12fe77d3e340154c5cee37
TomasVotruba <tomas.vot@gmail.com>
2018-03-10 10:54:02 +0100

    add ConflictingCheckersTest

---
+++ tests/DependencyInjection/ConflictingCheckersTest.php
@@ -0,0 +1,19 @@
+<?php declare(strict_types=1);
+
+namespace Symplify\EasyCodingStandard\Tests\DependencyInjection;
+
+use PHPUnit\Framework\TestCase;
+use Symplify\EasyCodingStandard\Configuration\Exception\ConflictingCheckersLoadedException;
+use Symplify\EasyCodingStandard\DependencyInjection\ContainerFactory;
+
+final class ConflictingCheckersTest extends TestCase
+{
+    public function test(): void
+    {
+        $this->expectException(ConflictingCheckersLoadedException::class);
+
+        (new ContainerFactory())->createWithConfig(
+            __DIR__ . '/ConflictingCheckersSource/config.yml'
+        );
+    }
+}
