68728c976c45f41c99f99f7baf2919102b15e456
Jáchym Toušek <enumag@gmail.com>
2018-03-14 16:08:59 +0100

    [ECS] Fix CheckerTolerantYamlFileLoader (closes #681)

--- src/Yaml/CheckerTolerantYamlFileLoader.php
+++ src/Yaml/CheckerTolerantYamlFileLoader.php
@@ -152,8 +152,15 @@ final class CheckerTolerantYamlFileLoader extends YamlFileLoader
         return Strings::replace($value, '#@#', '@@');
     }
 
-    private function isReservedKey(string $key): bool
+    /**
+     * @param string|int $key
+     */
+    private function isReservedKey($key): bool
     {
+        if (is_int($key)) {
+            return false;
+        }
+
         return in_array($key, $this->getServiceKeywords(), true);
     }
 
