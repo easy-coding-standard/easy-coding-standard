2139b58cb07e0916dabeafd7becc9b39773386a6
Tomas Votruba <tomas.vot@gmail.com>
2018-03-22 17:21:08 +0100

    [ECS] use consitent naming for bin files, rename bootstrap to autoload

--- bin/ecs-container.php
+++ bin/ecs-container.php
@@ -5,20 +5,21 @@ use Symplify\EasyCodingStandard\DependencyInjection\ContainerFactory;
 use Symplify\PackageBuilder\Configuration\ConfigFileFinder;
 use Symplify\PackageBuilder\Configuration\LevelFileFinder;
 
-require_once __DIR__ . '/easy-coding-standard-bootstrap.php';
+require_once __DIR__ . '/ecs-autoload.php';
 
 // 1. Detect configuration from level option
 $configFile = (new LevelFileFinder())->detectFromInputAndDirectory(new ArgvInput(), __DIR__ . '/../config/');
 
-// 2. Detect configuration from config option
+// 2. Fallback to config option
 if ($configFile === null) {
     ConfigFileFinder::detectFromInput('ecs', new ArgvInput());
+    // 3. Fallback to root file
     $configFile = ConfigFileFinder::provide('ecs', ['easy-coding-standard.yml', 'easy-coding-standard.yaml']);
 } else {
     ConfigFileFinder::set('ecs', $configFile);
 }
 
-// 3. Build DI container
+// 4. Build DI container
 $containerFactory = new ContainerFactory();
 if ($configFile) {
     return $containerFactory->createWithConfig($configFile);
