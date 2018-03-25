d35677dad7cae413f286dd4c6366402eb1c7607d
Tomas Votruba <tomas.vot@gmail.com>
2018-03-25 03:28:18 +0200

    remove inneficient cache

--- packages/FixerRunner/src/Parser/FileToTokensParser.php
+++ packages/FixerRunner/src/Parser/FileToTokensParser.php
@@ -3,23 +3,11 @@
 namespace Symplify\EasyCodingStandard\FixerRunner\Parser;
 
 use PhpCsFixer\Tokenizer\Tokens;
-use SplFileInfo;
-use Symplify\EasyCodingStandard\FileSystem\CachedFileLoader;
 
 final class FileToTokensParser
 {
-    /**
-     * @var CachedFileLoader
-     */
-    private $cachedFileLoader;
-
-    public function __construct(CachedFileLoader $cachedFileLoader)
-    {
-        $this->cachedFileLoader = $cachedFileLoader;
-    }
-
     public function parseFromFilePath(string $filePath): Tokens
     {
-        return Tokens::fromCode($this->cachedFileLoader->getFileContent(new SplFileInfo($filePath)));
+        return Tokens::fromCode(file_get_contents($filePath));
     }
 }
