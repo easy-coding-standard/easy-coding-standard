42bcad9d5732cea029c425ab94da227cd623eb12
Tomas Votruba <tomas.vot@gmail.com>
2018-03-27 21:11:14 +0200

    final approach: use parent solution with Reflectoin + just merge parameters instead of override

--- src/Yaml/CheckerTolerantYamlFileLoader.php
+++ src/Yaml/CheckerTolerantYamlFileLoader.php
@@ -2,10 +2,13 @@
 
 namespace Symplify\EasyCodingStandard\Yaml;
 
+use function get_parent_class;
+use InvalidArgumentException;
 use Symfony\Component\Config\FileLocatorInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
-use Throwable;
+use Symplify\PackageBuilder\Reflection\PrivatesCaller;
+use Symplify\PackageBuilder\Reflection\PrivatesSetter;
 
 /**
  * The need: https://github.com/symfony/symfony/pull/21313#issuecomment-372037445
@@ -32,16 +35,28 @@ final class CheckerTolerantYamlFileLoader extends YamlFileLoader
      */
     private $parametersMerger;
 
+    /**
+     * @var PrivatesCaller
+     */
+    private $privatesCaller;
+
+    /**
+     * @var PrivatesSetter
+     */
+    private $privatesSetter;
+
     public function __construct(ContainerBuilder $containerBuilder, FileLocatorInterface $fileLocator)
     {
         $this->checkerServiceParametersShifter = new CheckerServiceParametersShifter();
         $this->parametersMerger = new ParametersMerger();
+        $this->privatesCaller = new PrivatesCaller();
+        $this->privatesSetter = new PrivatesSetter();
 
         parent::__construct($containerBuilder, $fileLocator);
     }
 
     /**
-     * This method override is needed, so imported parameter values are not overrided by child ones
+     * Same as parent, just merging parameters instead overriding them
      *
      * @see https://github.com/Symplify/Symplify/pull/697
      *
@@ -50,16 +65,64 @@ final class CheckerTolerantYamlFileLoader extends YamlFileLoader
      */
     public function load($resource, $type = null): void
     {
-        // parent cannot by overridden fully, because it has many private methods and properties
-        parent::load($resource, $type);
+        $path = $this->locator->locate($resource);
 
-        $this->loadParametersFromResource($resource);
+        $content = $this->loadFile($path);
 
-        // @todo there needs to be way to load imports before parameter loading
-        // so we can override imported values with main config - simple :)
+        $this->container->fileExists($path);
+
+        // empty file
+        if ($content === null) {
+            return;
+        }
+
+        // imports
+        // $this->parseImports($content, $path);
+        $this->privatesCaller->callPrivateMethod($this, 'parseImports', $content, $path);
+
+        // parameters
+        if (isset($content[self::PARAMETERS_KEY])) {
+            if (! is_array($content[self::PARAMETERS_KEY])) {
+                throw new InvalidArgumentException(sprintf('The "parameters" key should contain an array in %s. Check your YAML syntax.', $path));
+            }
+
+            foreach ($content[self::PARAMETERS_KEY] as $key => $value) {
+                // $this->resolveServices($value, $path, true),
+                $resolvedValue = $this->privatesCaller->callPrivateMethod($this, 'resolveServices', $value, $path, true);
+
+                // only this section is different
+                if ($this->container->hasParameter($key)) {
+                    $newValue = (new ParametersMerger())->merge(
+                        $resolvedValue,
+                        $this->container->getParameter($key)
+                    );
+
+                    $this->container->setParameter($key, $newValue);
+                } else {
+                    $this->container->setParameter($key, $resolvedValue);
+                }
+            }
+        }
+
+        // extensions
+        // $this->loadFromExtensions($content);
+        $this->privatesCaller->callPrivateMethod($this, 'loadFromExtensions', $content);
+
+        // services - not accessible, private parent properties, luckily not needed
+        // $this->anonymousServicesCount = 0;
+        // $this->anonymousServicesSuffix = ContainerBuilder::hash($path);
+        $this->setCurrentDir(dirname($path));
+        try {
+            // $this->parseDefinitions($content, $path);
+            $this->privatesCaller->callPrivateMethod($this, 'parseDefinitions', $content, $path);
+        } finally {
+            $this->instanceof = array();
+        }
     }
 
     /**
+     * Handles checker parameters
+     *
      * @param string $file
      * @return array|mixed|mixed[]
      */
@@ -76,76 +139,5 @@ final class CheckerTolerantYamlFileLoader extends YamlFileLoader
         );
 
         return $decodedYaml;
-    }
-
-    /**
-     * @param mixed[] $content
-     */
-    private function parseImportParameters(array $content, string $file)
-    {
-        if (! isset($content['imports']) || ! is_array($content['imports'])) {
-            return;
-        }
-
-        $defaultDirectory = dirname($file);
-        foreach ($content['imports'] as $import) {
-            if (! is_array($import)) {
-                $import = ['resource' => $import];
-            }
-            if (! isset($import['resource'])) {
-                throw new \InvalidArgumentException(
-                    sprintf('An import should provide a resource in %s. Check your YAML syntax.', $file)
-                );
-            }
-
-            $this->setCurrentDir($defaultDirectory);
-
-            $this->loadParametersFromResource($import['resource'], isset($import['ignore_errors']) ? (bool) $import['ignore_errors'] : false);
-        }
-    }
-
-    private function loadParametersFromFile($content, $path): void
-    {
-        // imports
-        $this->parseImportParameters($content, $path);
-
-        // parameters
-        if (! isset($content[self::PARAMETERS_KEY]) || ! is_array($content[self::PARAMETERS_KEY])) {
-            return;
-        }
-
-        $mergedParameters = $this->parametersMerger->merge(
-            $content[self::PARAMETERS_KEY],
-            $this->container->getParameterBag()->all()
-        );
-
-        $this->container->getParameterBag()->add($mergedParameters);
-    }
-
-    private function loadParametersFromResource($resource, $ignoreErrors = false): void
-    {
-        // 1. possibly reload parameters here again with importing as well
-
-//        dump($this->locator);
-//        die;
-
-        try {
-            $path = $this->locator->locate($resource);
-            $content = $this->loadFile($path);
-        } catch (Throwable $throwable) {
-            if (! $ignoreErrors) {
-                throw $throwable;
-            }
-            return;
-        }
-
-        $this->container->fileExists($path);
-
-        // empty file
-        if ($content === null) {
-            return;
-        }
-
-        $this->loadParametersFromFile($content, $path);
     }
 }
