de3969f7fd5b85fe5c4ff8d9b1d2a6430338dcf5
Tomas Votruba <tomas.vot@gmail.com>
2018-03-27 23:17:49 +0200

    prepare failing test for CWD feature

---
+++ tests/Yaml/CurrentWorkingDirectoryTest.php
@@ -0,0 +1,24 @@
+<?php declare(strict_types=1);
+
+namespace Symplify\EasyCodingStandard\Tests\Yaml\CheckerTolerantYamlFileLoader;
+
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symplify\EasyCodingStandard\DependencyInjection\DelegatingLoaderFactory;
+use Symplify\EasyCodingStandard\DependencyInjection\EasyCodingStandardKernel;
+
+final class CurrentWorkingDirectoryTest extends TestCase
+{
+    public function test(): void
+    {
+        $containerBuilder = new ContainerBuilder();
+        $delegatingLoader = (new DelegatingLoaderFactory())->createFromContainerBuilderAndKernel(
+            $containerBuilder,
+            new EasyCodingStandardKernel()
+        );
+
+        $delegatingLoader->load(__DIR__ . '/CurrentWorkingDirectorySource/config-with-import-param.yml');
+
+        $this->assertArrayHasKey('parameters', $containerBuilder->getParameterBag()->all());
+    }
+}
