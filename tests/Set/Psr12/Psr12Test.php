62cafe81b6139eaf696f52463b1b12ed6a68f451
Tomas Votruba <tomas.vot@gmail.com>
2018-04-01 01:19:28 +0200

    Psr12Test - add 2nd part to tests

--- tests/Set/Psr12/Psr12Test.php
+++ tests/Set/Psr12/Psr12Test.php
@@ -2,16 +2,23 @@
 
 namespace Symplify\EasyCodingStandard\Tests\Set\Psr12;
 
-use Symplify\EasyCodingStandard\Testing\AbstractCheckerTestCase;
+use Iterator;
+use Symplify\EasyCodingStandardTester\Testing\AbstractCheckerTestCase;
 
 final class Psr12Test extends AbstractCheckerTestCase
 {
-    public function test(): void
+    /**
+     * @dataProvider wrongToFixedCases()
+     */
+    public function test(string $wrongFile, string $fixedFile): void
     {
-        $this->doTestWrongToFixedFile(
-            __DIR__ . '/wrong/wrong.php.inc',
-            __DIR__ . '/correct/correct.php.inc'
-        );
+        $this->doTestWrongToFixedFile($wrongFile, $fixedFile);
+    }
+
+    public function wrongToFixedCases(): Iterator
+    {
+        yield [__DIR__ . '/wrong/wrong.php.inc', __DIR__ . '/fixed/fixed.php.inc'];
+        yield [__DIR__ . '/wrong/wrong2.php.inc', __DIR__ . '/fixed/fixed2.php.inc'];
     }
 
     protected function provideConfig(): string
