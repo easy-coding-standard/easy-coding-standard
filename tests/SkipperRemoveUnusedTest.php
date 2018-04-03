9063a6f73c6313c4a82635bee23bd74c33c8ce4d
Tomas Votruba <tomas.vot@gmail.com>
2018-04-03 20:41:07 +0200

    decouple SkipperRemoveUnusedTest

---
+++ tests/SkipperRemoveUnusedTest.php
@@ -0,0 +1,67 @@
+<?php declare(strict_types=1);
+
+namespace Symplify\EasyCodingStandard\Tests;
+
+use PhpCsFixer\Fixer\Strict\DeclareStrictTypesFixer;
+use PHPUnit\Framework\TestCase;
+use Symplify\CodingStandard\Fixer\Solid\FinalInterfaceFixer;
+use Symplify\EasyCodingStandard\Skipper;
+
+final class SkipperRemoveUnusedTest extends TestCase
+{
+    /**
+     * @var Skipper
+     */
+    private $skipper;
+
+    protected function setUp(): void
+    {
+        $this->skipper = new Skipper($this->createSkipParameter(), []);
+    }
+
+    public function testKeepAll(): void
+    {
+        $this->assertSame([
+            DeclareStrictTypesFixer::class => [
+                0 => 'someFile',
+                1 => '*/someDirectory/*',
+            ],
+            'someSniff.someCode' => null,
+            'someSniff.someOtherCode' => [
+                '*/someDirectory/*',
+            ],
+        ], $this->skipper->getUnusedSkipped());
+    }
+
+    public function testRemoveFile()
+    {
+        $this->skipper->removeFileFromUnused('someFile');
+
+        $this->assertSame([
+            DeclareStrictTypesFixer::class => [
+                1 => '*/someDirectory/*',
+            ],
+            'someSniff.someCode' => null,
+            'someSniff.someOtherCode' => [
+                '*/someDirectory/*',
+            ],
+        ], $this->skipper->getUnusedSkipped());
+    }
+
+    /**
+     * @return mixed[]
+     */
+    private function createSkipParameter(): array
+    {
+        return [
+            DeclareStrictTypesFixer::class => [
+                'someFile',
+                '*/someDirectory/*',
+            ],
+            'someSniff.someCode' => null,
+            'someSniff.someOtherCode' => [
+                '*/someDirectory/*',
+            ],
+        ];
+    }
+}
