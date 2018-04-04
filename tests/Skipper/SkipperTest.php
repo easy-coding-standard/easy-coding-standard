f8f551719e3b55e217b5b4bfc3390123cb30e57a
Tomas Votruba <tomas.vot@gmail.com>
2018-04-04 10:28:08 +0200

    move Skipper test classes under /Skipper namespace

--- tests/Skipper/SkipperTest.php
+++ tests/Skipper/SkipperTest.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-namespace Symplify\EasyCodingStandard\Tests;
+namespace Symplify\EasyCodingStandard\Tests\Skipper;
 
 use PhpCsFixer\Fixer\Strict\DeclareStrictTypesFixer;
 use PHPUnit\Framework\TestCase;
