9d97efb3c0582ffdf318cbb43a596aad62e2f275
Tomas Votruba <tomas.vot@gmail.com>
2018-07-11 19:04:58 +0200

    [EasyCodingStandard] decouple ChangeFilesDetector cache from Kernel cache [ref #940]

---
+++ packages/ChangedFilesDetector/src/CompilerPass/DetectParametersCompilerPass.php
@@ -0,0 +1,14 @@
+<?php declare(strict_types=1);
+
+namespace Symplify\EasyCodingStandard\ChangedFilesDetector\CompilerPass;
+
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+final class DetectParametersCompilerPass implements CompilerPassInterface
+{
+    public function process(ContainerBuilder $containerBuilder): void
+    {
+        $containerBuilder->setParameter('sys_get_temp_dir', sys_get_temp_dir());
+    }
+}
