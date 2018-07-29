2b2c3bcba6cb1ced15246d2524c5f4b6ce26e7a8
Gabriel Ostrolucký <gadelat@gmail.com>
2018-07-29 22:15:14 +0200

    Extract to separate test case instead

---
+++ packages/SniffRunner/tests/Application/FileProcessorSource/ReferenceUsedNamesOnlySniff/FileProvingNeedOfProperSupportOfChangesets.php
@@ -0,0 +1,16 @@
+<?php declare(strict_types=1);
+
+namespace Foo;
+
+final class FileProvingNeedOfProperSupportOfChangesets
+{
+    /**
+     * @var \Foo\Baz
+     */
+    private $foo;
+
+    /**
+     * @var \Foo\Baz
+     */
+    private $bar;
+}
