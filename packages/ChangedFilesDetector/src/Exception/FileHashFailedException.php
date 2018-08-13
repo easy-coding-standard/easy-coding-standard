3e92271248152d7d4a0603ecd396244347c4f7c0
Tomas Votruba <tomas.vot@gmail.com>
2018-08-13 15:44:21 +0200

    bump to phsptan 0.10.3 + fix the md5_file()

---
+++ packages/ChangedFilesDetector/src/Exception/FileHashFailedException.php
@@ -0,0 +1,9 @@
+<?php declare(strict_types=1);
+
+namespace Symplify\EasyCodingStandard\ChangedFilesDetector\Exception;
+
+use Exception;
+
+final class FileHashFailedException extends Exception
+{
+}
