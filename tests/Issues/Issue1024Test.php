28cbb9f4fee7ded65debfa5a39bab600f424c803
Tomas Votruba <tomas.vot@gmail.com>
2018-08-21 09:56:52 +0200

    [ECS] Add test case for 1024

---
+++ tests/Issues/Issue1024Test.php
@@ -0,0 +1,18 @@
+<?php declare(strict_types=1);
+
+namespace Symplify\EasyCodingStandard\Tests\Issues;
+
+use Symplify\EasyCodingStandardTester\Testing\AbstractCheckerTestCase;
+
+final class Issue1024Test extends AbstractCheckerTestCase
+{
+    public function test(): void
+    {
+        $this->doTestWrongToFixedFile(__DIR__ . '/wrong/wrong1024.php.inc', __DIR__ . '/fixed/fixed1024.php.inc');
+    }
+
+    protected function provideConfig(): string
+    {
+        return __DIR__ . '/config/config1024.yml';
+    }
+}
