7949b65f20d7d89e2a2d82ae7fc68bbaadc8841a
Tomas Votruba <tomas.vot@gmail.com>
2018-09-21 18:49:29 +0200

    [Statie] Apply SmartFileInfo

--- src/Finder/FinderSanitizer.php
+++ src/Finder/FinderSanitizer.php
@@ -6,35 +6,31 @@ use Nette\Utils\Finder as NetteFinder;
 use SplFileInfo;
 use Symfony\Component\Finder\Finder as SymfonyFinder;
 use Symfony\Component\Finder\SplFileInfo as SymfonySplFileInfo;
+use Symplify\PackageBuilder\FileSystem\FinderSanitizer as PackageBuilderFinderSanitizer;
 use Symplify\PackageBuilder\FileSystem\SmartFileInfo;
 
+/**
+ * @deprecated Will be removed in Symplify 6.0
+ * @see \Symplify\PackageBuilder\FileSystem\FinderSanitizer instead
+ */
 final class FinderSanitizer
 {
+    /**
+     * @var PackageBuilderFinderSanitizer
+     */
+    private $packageBuilderFinderSanitizer;
+
+    public function __construct(PackageBuilderFinderSanitizer $packageBuilderFinderSanitizer)
+    {
+        $this->packageBuilderFinderSanitizer = $packageBuilderFinderSanitizer;
+    }
+
     /**
      * @param NetteFinder|SymfonyFinder|SplFileInfo[]|SymfonySplFileInfo[]|string[] $files
      * @return SmartFileInfo[]
      */
     public function sanitize(iterable $files): array
     {
-        $smartFileInfos = [];
-        foreach ($files as $file) {
-            $fileInfo = is_string($file) ? new SplFileInfo($file) : $file;
-            if (! $this->isFileInfoValid($fileInfo)) {
-                continue;
-            }
-
-            $smartFileInfos[] = new SmartFileInfo($fileInfo->getRealPath());
-        }
-
-        return $smartFileInfos;
-    }
-
-    private function isFileInfoValid(SplFileInfo $fileInfo): bool
-    {
-        if ($fileInfo->getRealPath() === false) {
-            return false;
-        }
-
-        return (bool) filesize($fileInfo->getRealPath());
+        return $this->packageBuilderFinderSanitizer->sanitize($files);
     }
 }
