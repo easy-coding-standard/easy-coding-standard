54d592d646c9dead789760702c71338fa3ce189f
Tomas Votruba <tomas.vot@gmail.com>
2018-10-01 00:04:27 +0800

    static fixes

--- packages/Configuration/src/Configuration.php
+++ packages/Configuration/src/Configuration.php
@@ -35,7 +35,9 @@ final class Configuration
 
     public function resolveFromInput(InputInterface $input): void
     {
-        $this->setSources($input->getArgument(Option::SOURCE));
+        /** @var string[] $sources */
+        $sources = $input->getArgument(Option::SOURCE);
+        $this->setSources($sources);
         $this->isFixer = (bool) $input->getOption(Option::FIX);
         $this->shouldClearCache = (bool) $input->getOption(Option::CLEAR_CACHE);
         $this->showProgressBar = ! (bool) $input->getOption(Option::NO_PROGRESS_BAR);
