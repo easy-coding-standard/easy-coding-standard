eca60eaed72fe820787b1696600457a5ee0f4ebb
Tomas Votruba <tomas.vot@gmail.com>
2018-11-06 19:59:06 +0100

    [ECS] Cleanup SkipperTest

---
+++ tests/AbstractConfigContainerAwareTestCase.php
@@ -0,0 +1,27 @@
+<?php declare(strict_types=1);
+
+namespace Symplify\EasyCodingStandard\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Psr\Container\ContainerInterface;
+use Symplify\EasyCodingStandard\DependencyInjection\ContainerFactory;
+
+abstract class AbstractConfigContainerAwareTestCase extends TestCase
+{
+    /**
+     * @var ContainerInterface
+     */
+    protected $container;
+
+    /**
+     * @param mixed[] $data
+     */
+    public function __construct(?string $name = null, array $data = [], string $dataName = '')
+    {
+        $this->container = (new ContainerFactory())->createWithConfigs([$this->provideConfig()]);
+
+        parent::__construct($name, $data, $dataName);
+    }
+
+    abstract protected function provideConfig(): string;
+}
--- tests/Skipper/SkipperTest.php
+++ tests/Skipper/SkipperTest.php
@@ -2,16 +2,14 @@
 
 namespace Symplify\EasyCodingStandard\Tests\Skipper;
 
+use Iterator;
 use PhpCsFixer\Fixer\Strict\DeclareStrictTypesFixer;
-use PHPUnit\Framework\TestCase;
 use Symplify\CodingStandard\Fixer\Solid\FinalInterfaceFixer;
-use Symplify\EasyCodingStandard\DependencyInjection\ContainerFactory;
-use Symplify\EasyCodingStandard\Error\ErrorAndDiffCollector;
 use Symplify\EasyCodingStandard\Skipper;
-use Symplify\EasyCodingStandard\SniffRunner\Application\SniffFileProcessor;
+use Symplify\EasyCodingStandard\Tests\AbstractConfigContainerAwareTestCase;
 use Symplify\PackageBuilder\FileSystem\SmartFileInfo;
 
-final class SkipperTest extends TestCase
+final class SkipperTest extends AbstractConfigContainerAwareTestCase
 {
     /**
      * @var Skipper
@@ -20,110 +18,72 @@ final class SkipperTest extends TestCase
 
     protected function setUp(): void
     {
-        $this->skipper = new Skipper($this->createSkipParameter(), []);
-    }
-
-    public function testNotSkipped(): void
-    {
-        $this->assertFalse($this->skipper->shouldSkipCheckerAndFile(
-            FinalInterfaceFixer::class,
-            new SmartFileInfo(__DIR__ . '/Source/someFile')
-        ));
-
-        $this->assertFalse($this->skipper->shouldSkipCheckerAndFile(
-            FinalInterfaceFixer::class,
-            new SmartFileInfo(__DIR__ . '/Source/someOtherFile')
-        ));
-
-        $this->assertFalse(
-            $this->skipper->shouldSkipCodeAndFile('someSniff.someForeignCode', new SmartFileInfo(
-                __DIR__ . '/Source/someFile'
-            ))
-        );
-        $this->assertFalse(
-            $this->skipper->shouldSkipCodeAndFile('someFixer.someOtherCode', new SmartFileInfo(
-                __DIR__ . '/Source/someFile'
-            ))
-        );
-    }
-
-    public function testSkipMessage(): void
-    {
-        $container = (new ContainerFactory())->createWithConfigs([__DIR__ . '/skip.yml']);
-
-        /** @var SniffFileProcessor $processor */
-        $processor = $container->get(SniffFileProcessor::class);
-        $processor->processFile(new SmartFileInfo(__DIR__ . '/skip.php.inc'));
-
-        /** @var ErrorAndDiffCollector $errorCollector */
-        $errorCollector = $container->get(ErrorAndDiffCollector::class);
-
-        $this->assertCount(1, $errors = array_values($errorCollector->getErrors()));
-        $this->assertCount(1, $errors[0]);
-
-        $this->assertSame(11, $errors[0][0]->getLine());
-        $this->assertSame(
-            'Cognitive complexity for method "bar" is 2 but has to be less than or equal to 1.',
-            $errors[0][0]->getMessage()
-        );
-    }
-
-    public function testSkipped(): void
-    {
-        $this->assertTrue($this->skipper->shouldSkipCheckerAndFile(
-            DeclareStrictTypesFixer::class,
-            new SmartFileInfo(__DIR__ . '/Source/someFile')
-        ));
-
-        $this->assertTrue($this->skipper->shouldSkipCheckerAndFile(
-            DeclareStrictTypesFixer::class,
-            new SmartFileInfo(__DIR__ . '/Source/someDirectory/anotherFile.php')
-        ));
-
-        $this->assertTrue($this->skipper->shouldSkipCheckerAndFile(
-            DeclareStrictTypesFixer::class,
-            new SmartFileInfo(__DIR__ . '/Source/someDirectory/anotherFile.php')
-        ));
-
-        $this->assertTrue(
-            $this->skipper->shouldSkipCodeAndFile(
-                DeclareStrictTypesFixer::class . '.someCode',
-                new SmartFileInfo(__DIR__ . '/Source/someFile')
-            )
-        );
-        $this->assertTrue($this->skipper->shouldSkipCodeAndFile(
-            DeclareStrictTypesFixer::class . '.someOtherCode',
-            new SmartFileInfo(__DIR__ . '/Source/someDirectory/someFile')
-        ));
-
-        $this->assertTrue($this->skipper->shouldSkipCodeAndFile(
-            DeclareStrictTypesFixer::class . '.someAnotherCode',
-            new SmartFileInfo(__DIR__ . '/Source/someDirectory/someFile')
-        ));
-
-        $this->assertTrue($this->skipper->shouldSkipMessageAndFile(
-            'some fishy code at line 5!',
-            new SmartFileInfo(__DIR__ . '/Source/someFile')
-        ));
-
-        $this->assertTrue($this->skipper->shouldSkipMessageAndFile(
-            'some another fishy code at line 5!',
-            new SmartFileInfo(__DIR__ . '/Source/someDirectory/someFile.php')
-        ));
+        $this->skipper = $this->container->get(Skipper::class);
     }
 
     /**
-     * @return mixed[]
+     * @dataProvider provideCheckerAndFile()
      */
-    private function createSkipParameter(): array
+    public function testCheckerAndFile(string $checker, string $filePath, bool $expected): void
     {
-        return [
-            DeclareStrictTypesFixer::class => ['Source/someFile', '*/someDirectory/*'],
-            DeclareStrictTypesFixer::class . '.someCode' => null,
-            DeclareStrictTypesFixer::class . '.someOtherCode' => ['*/someDirectory/*'],
-            DeclareStrictTypesFixer::class . '.someAnotherCode' => ['someDirectory/*'],
-            'some fishy code at line 5!' => null,
-            'some another fishy code at line 5!' => ['someDirectory/*'],
+        $this->assertSame($expected, $this->skipper->shouldSkipCheckerAndFile($checker, new SmartFileInfo($filePath)));
+    }
+
+    public function provideCheckerAndFile(): Iterator
+    {
+        yield [DeclareStrictTypesFixer::class, __DIR__ . '/Source/someFile', true];
+        yield [DeclareStrictTypesFixer::class, __DIR__ . '/Source/someDirectory/anotherFile.php', true];
+        yield [DeclareStrictTypesFixer::class, __DIR__ . '/Source/someDirectory/anotherFile.php', true];
+
+        yield [FinalInterfaceFixer::class, __DIR__ . '/Source/someFile', false];
+        yield [FinalInterfaceFixer::class, __DIR__ . '/Source/someOtherFile', false];
+    }
+
+    /**
+     * @dataProvider provideCodeAndFile()
+     */
+    public function testCodeAndFile(string $checker, string $filePath, bool $expected): void
+    {
+        $this->assertSame($expected, $this->skipper->shouldSkipCodeAndFile($checker, new SmartFileInfo($filePath)));
+    }
+
+    public function provideCodeAndFile(): Iterator
+    {
+        yield [DeclareStrictTypesFixer::class . '.someCode', __DIR__ . '/Source/someFile', true];
+        yield [DeclareStrictTypesFixer::class . '.someOtherCode', __DIR__ . '/Source/someDirectory/someFile', true];
+        yield [DeclareStrictTypesFixer::class . '.someAnotherCode', __DIR__ . '/Source/someDirectory/someFile', true];
+
+        yield ['someSniff.someForeignCode', __DIR__ . '/Source/someFile', false];
+        yield ['someSniff.someOtherCode', __DIR__ . '/Source/someFile', false];
+    }
+
+    /**
+     * @dataProvider provideMessageAndFile()
+     */
+    public function testMessageAndFile(string $message, string $filePath, bool $expected): void
+    {
+        $this->assertSame($expected, $this->skipper->shouldSkipMessageAndFile($message, new SmartFileInfo($filePath)));
+    }
+
+    public function provideMessageAndFile(): Iterator
+    {
+        yield ['some fishy code at line 5!', __DIR__ . '/Source/someFile', true];
+        yield ['some another fishy code at line 5!', __DIR__ . '/Source/someDirectory/someFile.php', true];
+
+        yield [
+            'Cognitive complexity for method "foo" is 2 but has to be less than or equal to 1.',
+            __DIR__ . '/Source/skip.php.inc',
+            true,
         ];
+        yield [
+            'Cognitive complexity for method "bar" is 2 but has to be less than or equal to 1.',
+            __DIR__ . '/Source/skip.php.inc',
+            false,
+        ];
+    }
+
+    protected function provideConfig(): string
+    {
+        return __DIR__ . '/config.yml';
     }
 }
