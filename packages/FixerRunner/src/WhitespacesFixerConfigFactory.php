b4f5df17366a0843fd755999ddad2aede05cad16
Tomas Votruba <tomas.vot@gmail.com>
2018-11-20 18:38:05 +0100

    [ECS] Add support for 2 spaces indent

--- packages/FixerRunner/src/WhitespacesFixerConfigFactory.php
+++ packages/FixerRunner/src/WhitespacesFixerConfigFactory.php
@@ -3,10 +3,17 @@
 namespace Symplify\EasyCodingStandard\FixerRunner;
 
 use PhpCsFixer\WhitespacesFixerConfig;
+use Symplify\EasyCodingStandard\Exception\Configuration\WhitespaceConfigurationException;
 use Symplify\PackageBuilder\Parameter\ParameterProvider;
+use function Safe\sprintf;
 
 final class WhitespacesFixerConfigFactory
 {
+    /**
+     * @var string
+     */
+    private const TWO_SPACES = '  ';
+
     /**
      * @var string
      */
@@ -41,10 +48,25 @@ final class WhitespacesFixerConfigFactory
     private function resolveIndentation(): string
     {
         $indentation = $this->parameterProvider->provideParameter('indentation');
-        if ($indentation === 'tab') {
+        if ($indentation === 'tab' || $indentation === self::ONE_TAB) {
             return self::ONE_TAB;
         }
 
-        return self::FOUR_SPACES;
+        if ($indentation === self::TWO_SPACES) {
+            return self::TWO_SPACES;
+        }
+
+        if ($indentation === 'spaces' || $indentation === self::FOUR_SPACES) {
+            return self::FOUR_SPACES;
+        }
+
+        $allowedValues = ['tab', 'spaces', self::TWO_SPACES, self::FOUR_SPACES, self::ONE_TAB];
+
+        throw new WhitespaceConfigurationException(sprintf(
+            'Value "%s" is not supported in "parameters > indentation".%sUse one of: "%s".',
+            $indentation,
+            PHP_EOL,
+            implode('", "', $allowedValues)
+        ));
     }
 }
