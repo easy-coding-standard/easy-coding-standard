7fd63996a7755b4158336a3dcdb17238403d63bb
Anton Zdanov <azdanov@pm.me>
2018-11-30 08:32:10 +0200

    feat(ECS): add OutputFormatter to CheckCommand initialize method
    Sets OutputFormatter inside CheckCommand#initialize.
    $input and
    $container are required to properly decide, and get a formatter.

--- src/Console/Output/OutputFormatterFactory.php
+++ src/Console/Output/OutputFormatterFactory.php
@@ -2,13 +2,21 @@
 
 namespace Symplify\EasyCodingStandard\Console\Output;
 
+use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symplify\EasyCodingStandard\Configuration\Option;
 use Symplify\EasyCodingStandard\Contract\Console\Output\OutputFormatterInterface;
 
 final class OutputFormatterFactory
 {
-    public function create(ContainerInterface $container): OutputFormatterInterface
+    public static function create(ContainerInterface $container, InputInterface $input): OutputFormatterInterface
     {
-        return $container->get(JsonOutputFormatter::class);
+        $formatOption = $input->getOption(Option::OUTPUT_FORMAT_OPTION);
+
+        if ($formatOption === Option::JSON_OUTPUT_FORMAT) {
+            return $container->get(JsonOutputFormatter::class);
+        }
+
+        return $container->get(TableOutputFormatter::class);
     }
 }
