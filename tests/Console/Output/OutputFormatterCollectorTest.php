95feae675e8d8647d6fefc025660bfa0e9d87c10
Anton Zdanov <azdanov@pm.me>
2018-11-30 16:21:23 +0200

    test(ECS): add OutputFormatterCollectorTest

---
+++ tests/Console/Output/OutputFormatterCollectorTest.php
@@ -0,0 +1,42 @@
+<?php declare(strict_types=1);
+
+namespace Symplify\EasyCodingStandard\Tests\Console\Output;
+
+use Symplify\EasyCodingStandard\Console\Output\JsonOutputFormatter;
+use Symplify\EasyCodingStandard\Console\Output\OutputFormatterCollector;
+use Symplify\EasyCodingStandard\Contract\Console\Output\OutputFormatterInterface;
+use Symplify\EasyCodingStandard\Tests\AbstractContainerAwareTestCase;
+
+final class OutputFormatterCollectorTest extends AbstractContainerAwareTestCase
+{
+    /**
+     * @var OutputFormatterInterface
+     */
+    private $outputFormatter;
+
+    protected function setUp(): void
+    {
+        $this->outputFormatter = $this->container->get(JsonOutputFormatter::class);
+    }
+
+    public function testCanGetFormatterByName(): void
+    {
+        $formatters = [$this->outputFormatter];
+        $collector = new OutputFormatterCollector($formatters);
+        $name = $this->outputFormatter->getName();
+
+        $this->assertSame($this->outputFormatter, $collector->getByName($name));
+    }
+
+    /**
+     * @expectedException \Symplify\EasyCodingStandard\Configuration\Exception\NoOutputFormatterException
+     */
+    public function testThrowOnWrongName(): void
+    {
+        $formatters = [$this->outputFormatter];
+        $collector = new OutputFormatterCollector($formatters);
+        $name = 'wrong';
+
+        $collector->getByName($name);
+    }
+}
