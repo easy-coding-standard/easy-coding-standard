c0121e530fbd99d84ab177eb5efa86d36af78d4b
Anton Zdanov <azdanov@pm.me>
2018-11-30 21:25:24 +0200

    feat(ECS): add $output to report()
    This is necessary for JsonOutputFormatter.
    For some reason when testing
    only $output was able to print to console.

--- src/Contract/Console/Output/OutputFormatterInterface.php
+++ src/Contract/Console/Output/OutputFormatterInterface.php
@@ -2,9 +2,11 @@
 
 namespace Symplify\EasyCodingStandard\Contract\Console\Output;
 
+use Symfony\Component\Console\Output\OutputInterface;
+
 interface OutputFormatterInterface
 {
-    public function report(int $processedFilesCount): int;
+    public function report(int $processedFilesCount, OutputInterface $output): int;
 
     public function getName(): string;
 }
