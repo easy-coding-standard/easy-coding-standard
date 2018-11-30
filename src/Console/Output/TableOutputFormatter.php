bd57cba98f75648659301b5838efedcfba8e426d
Anton Zdanov <azdanov@pm.me>
2018-11-30 15:35:26 +0200

    feat(ECS): add OutputFormatterCollector to replace OutputFormatterFactory
    Replaces factory pattern with collector pattern.
    Fix small typo in
    CheckCommand error description.

--- src/Console/Output/TableOutputFormatter.php
+++ src/Console/Output/TableOutputFormatter.php
@@ -3,12 +3,12 @@
 namespace Symplify\EasyCodingStandard\Console\Output;
 
 use Symplify\EasyCodingStandard\Configuration\Configuration;
+use Symplify\EasyCodingStandard\Configuration\Option;
 use Symplify\EasyCodingStandard\Console\Style\EasyCodingStandardStyle;
 use Symplify\EasyCodingStandard\Contract\Console\Output\OutputFormatterInterface;
 use Symplify\EasyCodingStandard\Error\ErrorAndDiffCollector;
 use Symplify\EasyCodingStandard\Error\FileDiff;
 use Symplify\PackageBuilder\Console\ShellCode;
-use function Safe\sprintf;
 
 final class TableOutputFormatter implements OutputFormatterInterface
 {
@@ -55,6 +55,11 @@ final class TableOutputFormatter implements OutputFormatterInterface
         $this->easyCodingStandardStyle->newLine();
 
         return $this->configuration->isFixer() ? $this->printAfterFixerStatus() : $this->printNoFixerStatus();
+    }
+
+    public function getName(): string
+    {
+        return Option::TABLE_OUTPUT_FORMAT;
     }
 
     /**
