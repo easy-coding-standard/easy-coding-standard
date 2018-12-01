a64607704592d6ee566fb031858b136e980707bf
Anton Zdanov <azdanov@pm.me>
2018-12-01 08:03:55 +0200

    test(ECS): update JsonOutputFormatterTest
    Delete fixtures since output data can change and use structural check.

--- tests/Console/Output/JsonOutputFormatterTest.php
+++ tests/Console/Output/JsonOutputFormatterTest.php
@@ -2,6 +2,7 @@
 
 namespace Symplify\EasyCodingStandard\Tests\Console\Output;
 
+use Nette\Utils\Json;
 use Symfony\Component\Console\Tester\ApplicationTester;
 use Symplify\EasyCodingStandard\Configuration\Option;
 use Symplify\EasyCodingStandard\Console\Application;
@@ -23,19 +24,39 @@ final class JsonOutputFormatterTest extends AbstractConfigContainerAwareTestCase
     {
         $this->application->setAutoExit(false);
         $applicationTester = new ApplicationTester($this->application);
+        $source = 'wrong/wrong.php.inc';
+        $config = 'config/config.yml';
 
-        $status = $applicationTester->run([
+        $currentDir = \substr(__DIR__, \strlen($_SERVER['PWD'] . '/'));
+        $sourceLocation = ($currentDir ? ($currentDir . '/') : '') . $source;
+
+        $exitCode = $applicationTester->run([
             'command' => 'check',
-            'source' => [__DIR__ . '/wrong/wrong.php.inc'],
-            '--config' => __DIR__ . '/config/config.yml',
+            'source' => [__DIR__ . '/' . $source],
+            '--config' => __DIR__ . '/' . $config,
             '--' . Option::OUTPUT_FORMAT_OPTION => Option::JSON_OUTPUT_FORMAT,
         ]);
+        $output = Json::decode($applicationTester->getDisplay(), true);
 
-        $this->assertJsonStringEqualsJsonFile(
-            __DIR__ . '/fixtures/json-output.json',
-            $applicationTester->getDisplay()
-        );
-        $this->assertSame(1, $status);
+        static::assertSame(1, $exitCode);
+
+        static::assertArrayHasKey('meta', $output);
+        static::assertArrayHasKey('version', $output['meta']);
+        static::assertArrayHasKey('config', $output['meta']);
+        static::assertContains($config, $output['meta']['config']);
+
+        static::assertArrayHasKey('totals', $output);
+        static::assertArrayHasKey('errors', $output['totals']);
+        static::assertArrayHasKey('diffs', $output['totals']);
+
+        static::assertArrayHasKey('files', $output);
+        static::assertArrayHasKey($sourceLocation, $output['files']);
+
+        static::assertArrayHasKey('line', $output['files'][$sourceLocation]['errors'][0]);
+        static::assertArrayHasKey('message', $output['files'][$sourceLocation]['errors'][0]);
+        static::assertArrayHasKey('sourceClass', $output['files'][$sourceLocation]['errors'][0]);
+        static::assertArrayHasKey('diff', $output['files'][$sourceLocation]['diffs'][0]);
+        static::assertArrayHasKey('appliedCheckers', $output['files'][$sourceLocation]['diffs'][0]);
     }
 
     protected function provideConfig(): string
