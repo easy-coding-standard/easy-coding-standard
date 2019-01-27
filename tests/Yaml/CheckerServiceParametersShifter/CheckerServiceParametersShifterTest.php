c78bbb9af510cc15d3f702e4945fa50bd16d9e67
Tomas Votruba <tomas.vot@gmail.com>
2019-01-27 22:42:45 +0100

    move invalid property test to CheckerServiceParametersShifterTest

--- tests/Yaml/CheckerServiceParametersShifter/CheckerServiceParametersShifterTest.php
+++ tests/Yaml/CheckerServiceParametersShifter/CheckerServiceParametersShifterTest.php
@@ -3,9 +3,12 @@
 namespace Symplify\EasyCodingStandard\Tests\Yaml\CheckerServiceParametersShifter;
 
 use Iterator;
+use PHP_CodeSniffer\Standards\Generic\Sniffs\Files\LineLengthSniff;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\Yaml\Yaml;
+use Symplify\EasyCodingStandard\Exception\DependencyInjection\Extension\InvalidSniffPropertyException;
 use Symplify\EasyCodingStandard\Yaml\CheckerServiceParametersShifter;
+use function Safe\sprintf;
 
 final class CheckerServiceParametersShifterTest extends TestCase
 {
@@ -37,5 +40,29 @@ final class CheckerServiceParametersShifterTest extends TestCase
 
         // mainly "@"
         yield [__DIR__ . '/Source/before/escaping.yaml', __DIR__ . '/Source/after/escaping.yaml'];
+    }
+
+    /**
+     * @dataProvider provideInvalidConfigs()
+     */
+    public function testInvalidConfiguration(string $inputYaml, string $exceptionMessage): void
+    {
+        $this->expectException(InvalidSniffPropertyException::class);
+        $this->expectExceptionMessage($exceptionMessage);
+
+        $inputContent = Yaml::parseFile($inputYaml);
+        $this->checkerServiceParametersShifter->process($inputContent);
+    }
+
+    public function provideInvalidConfigs(): Iterator
+    {
+        yield [
+            __DIR__ . '/Source/invalid/sniff_parameter_typo.yaml',
+            sprintf(
+                'Property "lineLimid" was not found on "%s" sniff class in configuration. Did you mean "lineLimit"?',
+                LineLengthSniff::class
+            ),
+        ];
+        /** fixer covers this internally in @see AbstractFixer::configure() */
     }
 }
