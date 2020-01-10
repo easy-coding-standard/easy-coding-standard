f59dd03f415c54f86fac62089627c0debb8c698b
TomasVotruba <tomas.vot@gmail.com>
2020-01-10 17:48:58 +0100

    cleanup CompileProcessFactory

--- compiler/src/Console/CompileCommand.php
+++ compiler/src/Console/CompileCommand.php
@@ -12,7 +12,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Filesystem\Filesystem;
 use Symplify\EasyCodingStandard\Compiler\Packagist\SymplifyStableVersionProvider;
-use Symplify\EasyCodingStandard\Compiler\Process\CompileProcessFactory;
+use Symplify\EasyCodingStandard\Compiler\Process\SymfonyProcess;
 
 /**
  * Inspired by @see https://github.com/phpstan/phpstan-src/blob/f939d23155627b5c2ec6eef36d976dddea22c0c5/compiler/src/Console/CompileCommand.php
@@ -40,22 +40,18 @@ final class CompileCommand extends Command
     private $filesystem;
 
     /**
-     * @var CompileProcessFactory
-     */
-    private $compileProcessFactory;
-
-    /**
      * @var SymplifyStableVersionProvider
      */
     private $symplifyStableVersionProvider;
 
-    public function __construct(CompileProcessFactory $compileProcessFactory, string $dataDir, string $buildDir)
+    public function __construct(string $dataDir, string $buildDir)
     {
         parent::__construct();
-        $this->filesystem = new Filesystem();
-        $this->compileProcessFactory = $compileProcessFactory;
+
         $this->dataDir = $dataDir;
         $this->buildDir = $buildDir;
+
+        $this->filesystem = new Filesystem();
         $this->symplifyStableVersionProvider = new SymplifyStableVersionProvider();
     }
 
@@ -67,19 +63,19 @@ final class CompileCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $this->compileProcessFactory->setOutput($output);
-
         $composerJsonFile = $this->buildDir . '/composer.json';
 
         $this->fixComposerJson($composerJsonFile);
+
         // @see https://github.com/dotherightthing/wpdtrt-plugin-boilerplate/issues/52
-        $this->compileProcessFactory->create(
+        new SymfonyProcess(
             ['composer', 'update', '--no-dev', '--prefer-dist', '--no-interaction', '--classmap-authoritative'],
-            $this->buildDir
+            $this->buildDir,
+            $output
         );
 
         // parallel prevention is just for single less-buggy process
-        $this->compileProcessFactory->create(['php', 'box.phar', 'compile', '--no-parallel'], $this->dataDir);
+        new SymfonyProcess(['php', 'box.phar', 'compile', '--no-parallel'], $this->dataDir, $output);
 
         $this->restoreComposerJson($composerJsonFile);
 
