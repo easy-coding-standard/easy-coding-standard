6c3bf335ba1358e44c6627c476a272adfea12a4e
TomasVotruba <tomas.vot@gmail.com>
2020-01-25 09:14:44 +0100

    test every package

---
+++ packages/changed-files-detector/tests/ChangedFilesDetectorTest.php
@@ -0,0 +1,76 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\EasyCodingStandard\ChangedFilesDetector\Tests;
+
+use Symplify\EasyCodingStandard\ChangedFilesDetector\ChangedFilesDetector;
+use Symplify\EasyCodingStandard\HttpKernel\EasyCodingStandardKernel;
+use Symplify\PackageBuilder\Tests\AbstractKernelTestCase;
+use Symplify\SmartFileSystem\SmartFileInfo;
+
+final class ChangedFilesDetectorTest extends AbstractKernelTestCase
+{
+    /**
+     * @var SmartFileInfo
+     */
+    private $smartFileInfo;
+
+    /**
+     * @var ChangedFilesDetector
+     */
+    private $changedFilesDetector;
+
+    protected function setUp(): void
+    {
+        $this->bootKernel(EasyCodingStandardKernel::class);
+
+        $this->smartFileInfo = new SmartFileInfo(__DIR__ . '/ChangedFilesDetectorSource/OneClass.php');
+
+        $this->changedFilesDetector = self::$container->get(ChangedFilesDetector::class);
+        $this->changedFilesDetector->changeConfigurationFile(
+            __DIR__ . '/ChangedFilesDetectorSource/easy-coding-standard.yml'
+        );
+    }
+
+    public function testAddFile(): void
+    {
+        $this->assertFileHasChanged($this->smartFileInfo);
+        $this->assertFileHasChanged($this->smartFileInfo);
+    }
+
+    public function testHasFileChanged(): void
+    {
+        $this->changedFilesDetector->addFileInfo($this->smartFileInfo);
+
+        $this->assertFileHasNotChanged($this->smartFileInfo);
+    }
+
+    public function testInvalidateCacheOnConfigurationChange(): void
+    {
+        $this->changedFilesDetector->addFileInfo($this->smartFileInfo);
+        $this->assertFileHasNotChanged($this->smartFileInfo);
+
+        $this->changedFilesDetector->changeConfigurationFile(
+            __DIR__ . '/ChangedFilesDetectorSource/another-configuration.yml'
+        );
+
+        $this->assertFileHasChanged($this->smartFileInfo);
+    }
+
+    private function assertFileHasChanged(SmartFileInfo $smartFileInfo): void
+    {
+        $this->assertTrue(
+            $this->changedFilesDetector->hasFileInfoChanged($smartFileInfo),
+            sprintf('Failed asserting that file "%s" has changed.', $smartFileInfo->getRelativeFilePath())
+        );
+    }
+
+    private function assertFileHasNotChanged(SmartFileInfo $smartFileInfo): void
+    {
+        $this->assertFalse(
+            $this->changedFilesDetector->hasFileInfoChanged($smartFileInfo),
+            sprintf('Failed asserting that file "%s" has not changed.', $smartFileInfo->getRelativeFilePath())
+        );
+    }
+}
---
+++ src/DependencyInjection/CompilerPass/RemoveExcludedCheckersCompilerPass.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\EasyCodingStandard\DependencyInjection\CompilerPass;
+
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+
+final class RemoveExcludedCheckersCompilerPass implements CompilerPassInterface
+{
+    public function process(ContainerBuilder $containerBuilder): void
+    {
+        $excludedCheckers = $this->getExcludedCheckersFromParameterBag($containerBuilder->getParameterBag());
+
+        foreach ($containerBuilder->getDefinitions() as $id => $definition) {
+            if (! in_array($definition->getClass(), $excludedCheckers, true)) {
+                continue;
+            }
+
+            $containerBuilder->removeDefinition($id);
+        }
+    }
+
+    /**
+     * @return string[]
+     */
+    private function getExcludedCheckersFromParameterBag(ParameterBagInterface $parameterBag): array
+    {
+        $excludedCheckers = [];
+
+        // parts of "skip" parameter
+        if ($parameterBag->has('skip')) {
+            $skip = $parameterBag->get('skip');
+            foreach ($skip as $key => $value) {
+                if ($value !== null) {
+                    continue;
+                }
+
+                if (! class_exists($key)) {
+                    continue;
+                }
+
+                $excludedCheckers[] = $key;
+            }
+        }
+
+        return array_unique($excludedCheckers);
+    }
+}
