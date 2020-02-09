f22da3a4715ca5342aeaed9f3e8db986a5fe97eb
Tomas Votruba <tomas.vot@gmail.com>
2020-02-09 18:17:56 +0100

    Merge pull request #1773 from symplify/sprintf-depre
    [CodingStandard] Deprecate SprintfOverContactSniff

---
+++ src/HttpKernel/EasyCodingStandardKernel.php
@@ -0,0 +1,112 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\EasyCodingStandard\HttpKernel;
+
+use PHP_CodeSniffer\Sniffs\Sniff;
+use PhpCsFixer\Fixer\FixerInterface;
+use Symfony\Component\Config\Loader\DelegatingLoader;
+use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpKernel\Bundle\BundleInterface;
+use Symfony\Component\HttpKernel\Kernel;
+use Symplify\AutoBindParameter\DependencyInjection\CompilerPass\AutoBindParameterCompilerPass;
+use Symplify\AutowireArrayParameter\DependencyInjection\CompilerPass\AutowireArrayParameterCompilerPass;
+use Symplify\EasyCodingStandard\ChangedFilesDetector\CompilerPass\AddGetCwdWebalizedParameterCompilerPass;
+use Symplify\EasyCodingStandard\ChangedFilesDetector\CompilerPass\AddSysGetTempDirParameterCompilerPass;
+use Symplify\EasyCodingStandard\Contract\Console\Output\OutputFormatterInterface;
+use Symplify\EasyCodingStandard\DependencyInjection\CompilerPass\ConflictingCheckersCompilerPass;
+use Symplify\EasyCodingStandard\DependencyInjection\CompilerPass\FixerWhitespaceConfigCompilerPass;
+use Symplify\EasyCodingStandard\DependencyInjection\CompilerPass\RemoveExcludedCheckersCompilerPass;
+use Symplify\EasyCodingStandard\DependencyInjection\CompilerPass\RemoveMutualCheckersCompilerPass;
+use Symplify\EasyCodingStandard\DependencyInjection\DelegatingLoaderFactory;
+use Symplify\PackageBuilder\Contract\HttpKernel\ExtraConfigAwareKernelInterface;
+use Symplify\PackageBuilder\DependencyInjection\CompilerPass\AutoReturnFactoryCompilerPass;
+use Symplify\PackageBuilder\DependencyInjection\CompilerPass\AutowireInterfacesCompilerPass;
+
+final class EasyCodingStandardKernel extends Kernel implements ExtraConfigAwareKernelInterface
+{
+    /**
+     * @var string[]
+     */
+    private $configs = [];
+
+    public function registerContainerConfiguration(LoaderInterface $loader): void
+    {
+        $loader->load(__DIR__ . '/../../config/config.yaml');
+
+        foreach ($this->configs as $config) {
+            $loader->load($config);
+        }
+    }
+
+    public function getCacheDir(): string
+    {
+        return sys_get_temp_dir() . '/easy_coding_standard';
+    }
+
+    public function getLogDir(): string
+    {
+        return sys_get_temp_dir() . '/easy_coding_standard_logs';
+    }
+
+    /**
+     * @return BundleInterface[]
+     */
+    public function registerBundles(): iterable
+    {
+        return [];
+    }
+
+    /**
+     * @param string[] $configs
+     */
+    public function setConfigs(array $configs): void
+    {
+        $this->configs = $configs;
+    }
+
+    /**
+     * Order matters!
+     */
+    protected function build(ContainerBuilder $containerBuilder): void
+    {
+        // needs to be first, since it's adding new service definitions
+        $containerBuilder->addCompilerPass(new AutoReturnFactoryCompilerPass());
+
+        // cleanup
+        $containerBuilder->addCompilerPass(new RemoveExcludedCheckersCompilerPass());
+        $containerBuilder->addCompilerPass(new RemoveMutualCheckersCompilerPass());
+
+        // autowire
+        $containerBuilder->addCompilerPass(new AutowireInterfacesCompilerPass([
+            FixerInterface::class,
+            Sniff::class,
+            OutputFormatterInterface::class,
+        ]));
+        $containerBuilder->addCompilerPass(new AutowireArrayParameterCompilerPass());
+
+        // exceptions
+        $containerBuilder->addCompilerPass(new ConflictingCheckersCompilerPass());
+
+        // parameters
+        $containerBuilder->addCompilerPass(new AddSysGetTempDirParameterCompilerPass());
+        $containerBuilder->addCompilerPass(new AddGetCwdWebalizedParameterCompilerPass());
+        $containerBuilder->addCompilerPass(new AutoBindParameterCompilerPass());
+
+        // method calls
+        $containerBuilder->addCompilerPass(new FixerWhitespaceConfigCompilerPass());
+    }
+
+    /**
+     * @param ContainerInterface|ContainerBuilder $container
+     */
+    protected function getContainerLoader(ContainerInterface $container): DelegatingLoader
+    {
+        $delegatingLoaderFactory = new DelegatingLoaderFactory();
+
+        return $delegatingLoaderFactory->createFromContainerBuilderAndKernel($container, $this);
+    }
+}
