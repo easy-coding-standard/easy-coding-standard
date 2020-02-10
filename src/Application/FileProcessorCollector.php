04d4b7d683fde4fa37dcc2bfcc861617abc89811
TomasVotruba <tomas.vot@gmail.com>
2020-02-10 22:18:15 +0100

    [Statie] Remove MigratorSculpin, Remove MigratorJekyll, unused

---
+++ src/Application/FileProcessorCollector.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\EasyCodingStandard\Application;
+
+use Symplify\EasyCodingStandard\Contract\Application\FileProcessorInterface;
+use Symplify\EasyCodingStandard\FixerRunner\Application\FixerFileProcessor;
+use Symplify\EasyCodingStandard\SniffRunner\Application\SniffFileProcessor;
+
+final class FileProcessorCollector
+{
+    /**
+     * @var FileProcessorInterface[]
+     */
+    private $fileProcessors = [];
+
+    /**
+     * orders matters, so Fixer can cleanup after Sniffer
+     */
+    public function __construct(SniffFileProcessor $sniffFileProcessor, FixerFileProcessor $fixerFileProcessor)
+    {
+        $this->fileProcessors[] = $sniffFileProcessor;
+        $this->fileProcessors[] = $fixerFileProcessor;
+    }
+
+    /**
+     * @return FileProcessorInterface[]
+     */
+    public function getFileProcessors(): array
+    {
+        return $this->fileProcessors;
+    }
+}
