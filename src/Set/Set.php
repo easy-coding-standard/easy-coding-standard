2437c91931da9a5486c03a5bd0eecd5d801798f5
Tomas Votruba <tomas.vot@gmail.com>
2020-02-10 17:10:55 +0100

    Merge pull request #1776 from symplify/cs-cogni-comple-class
    [CodingStandard] Add ClassCognitiveComplexitySniff

---
+++ src/Set/Set.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\EasyCodingStandard\Set;
+
+final class Set
+{
+    /**
+     * @var string
+     */
+    public const SET_DIRECTORY = __DIR__ . '/../../config/set';
+}
