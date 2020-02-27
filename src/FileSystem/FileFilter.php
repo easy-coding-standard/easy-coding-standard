da8b617804677b7e8c1aa45e5b8205cbac0b0e8f
Tomas Votruba <tomas.vot@gmail.com>
2020-02-27 17:34:13 +0100

    Merge pull request #1794 from symplify/mb-revert-split
    [MB] Revert split --branch feature

---
+++ src/FileSystem/FileFilter.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\EasyCodingStandard\FileSystem;
+
+use Symplify\EasyCodingStandard\ChangedFilesDetector\ChangedFilesDetector;
+use Symplify\SmartFileSystem\SmartFileInfo;
+
+final class FileFilter
+{
+    /**
+     * @var ChangedFilesDetector
+     */
+    private $changedFilesDetector;
+
+    public function __construct(ChangedFilesDetector $changedFilesDetector)
+    {
+        $this->changedFilesDetector = $changedFilesDetector;
+    }
+
+    /**
+     * @param SmartFileInfo[] $fileInfos
+     * @return SmartFileInfo[]
+     */
+    public function filterOnlyChangedFiles(array $fileInfos): array
+    {
+        return array_filter($fileInfos, function (SmartFileInfo $smartFileInfo): bool {
+            return $this->changedFilesDetector->hasFileInfoChanged($smartFileInfo);
+        });
+    }
+}
