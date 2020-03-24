6e6a8d6a1ee90f18492fcc0bbcf6776b170b2035
Benjamin Kluge <b.kluge@neusta.de>
2020-03-24 11:16:34 +0100

    fix cs

---
+++ tests/Issues/Issue923Test.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\EasyCodingStandard\Tests\Issues;
+
+use Symplify\EasyCodingStandardTester\Testing\AbstractCheckerTestCase;
+
+final class Issue923Test extends AbstractCheckerTestCase
+{
+    public function test(): void
+    {
+        // from "\r\n" to "\n"
+        $this->doTestFiles([[__DIR__ . '/wrong/wrong923.php.inc', __DIR__ . '/fixed/fixed923.php.inc']]);
+    }
+
+    protected function provideConfig(): string
+    {
+        return __DIR__ . '/config/config923.yml';
+    }
+}
