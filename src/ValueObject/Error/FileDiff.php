fb20e96376c8954d61c4d72b7cbde0b112ab825f
Nathan Page <nathan.page@eonx.com>
2020-04-07 11:27:21 +1000

    Fix command in README.md
    I've just tried to use the package following the readme but the actual command is `dump-static-site` so just fixing it for everybody else.

---
+++ src/ValueObject/Error/FileDiff.php
@@ -0,0 +1,62 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\EasyCodingStandard\ValueObject\Error;
+
+use PhpCsFixer\Differ\DiffConsoleFormatter;
+
+final class FileDiff
+{
+    /**
+     * @var string
+     */
+    private $diff;
+
+    /**
+     * @var string[]
+     */
+    private $appliedCheckers = [];
+
+    /**
+     * @var DiffConsoleFormatter
+     */
+    private $diffConsoleFormatter;
+
+    /**
+     * @param string[] $appliedCheckers
+     */
+    public function __construct(string $diff, array $appliedCheckers)
+    {
+        $this->diff = $diff;
+        $this->appliedCheckers = $appliedCheckers;
+
+        $this->diffConsoleFormatter = new DiffConsoleFormatter(true, sprintf(
+            '<comment>    ---------- begin diff ----------</comment>%s%%s%s' .
+            '<comment>    ----------- end diff -----------</comment>',
+            PHP_EOL,
+            PHP_EOL
+        ));
+    }
+
+    public function getDiff(): string
+    {
+        return $this->diff;
+    }
+
+    public function getDiffConsoleFormatted(): string
+    {
+        return $this->diffConsoleFormatter->format($this->diff);
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getAppliedCheckers(): array
+    {
+        $this->appliedCheckers = array_unique($this->appliedCheckers);
+        sort($this->appliedCheckers);
+
+        return $this->appliedCheckers;
+    }
+}
