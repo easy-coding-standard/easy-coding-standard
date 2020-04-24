bf41d86249edab1ea3936730c6d3ff465e96c4af
TomasVotruba <tomas.vot@gmail.com>
2020-04-24 02:37:52 +0200

    open 7.3-dev

---
+++ src/DependencyInjection/CompilerPass/FixerWhitespaceConfigCompilerPass.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\EasyCodingStandard\DependencyInjection\CompilerPass;
+
+use PhpCsFixer\Fixer\WhitespacesAwareFixerInterface;
+use PhpCsFixer\WhitespacesFixerConfig;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Reference;
+
+final class FixerWhitespaceConfigCompilerPass implements CompilerPassInterface
+{
+    public function process(ContainerBuilder $containerBuilder): void
+    {
+        foreach ($containerBuilder->getDefinitions() as $definition) {
+            if ($definition->getClass() === null) {
+                continue;
+            }
+
+            if (! is_a($definition->getClass(), WhitespacesAwareFixerInterface::class, true)) {
+                continue;
+            }
+
+            $definition->addMethodCall('setWhitespacesConfig', [new Reference(WhitespacesFixerConfig::class)]);
+        }
+    }
+}
