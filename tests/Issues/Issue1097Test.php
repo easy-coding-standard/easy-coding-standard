aa1107bf6e6621c2f86dac2674d5b0b9ccc5f1b9
Tomas Votruba <tomas.vot@gmail.com>
2020-04-24 02:36:25 +0200

    Merge pull request #1836 from Agares/do_not_replace_packages
    Do not replace the symfony php70 polyfill in easy-coding-standard

---
+++ tests/Issues/Issue1097Test.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\EasyCodingStandard\Tests\Issues;
+
+use Symplify\EasyCodingStandardTester\Testing\AbstractCheckerTestCase;
+
+final class Issue1097Test extends AbstractCheckerTestCase
+{
+    public function test(): void
+    {
+        $this->doTestFiles([[__DIR__ . '/wrong/wrong1097.php.inc', __DIR__ . '/fixed/fixed1097.php.inc']]);
+    }
+
+    protected function provideConfig(): string
+    {
+        return __DIR__ . '/config/config1097.yml';
+    }
+}
