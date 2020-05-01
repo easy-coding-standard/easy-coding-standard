a886ef2b32c8089a859736d45d97d4ae5ca34476
Tomas Votruba <tomas.vot@gmail.com>
2020-05-02 01:08:07 +0200

    Merge pull request #1888 from symplify/fixing-compile

---
+++ src/Error/ErrorFactory.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\EasyCodingStandard\Error;
+
+use Symplify\EasyCodingStandard\ValueObject\Error\Error;
+use Symplify\SmartFileSystem\SmartFileInfo;
+
+final class ErrorFactory
+{
+    public function create(int $line, string $message, string $sourceClass, SmartFileInfo $smartFileInfo): Error
+    {
+        return new Error($line, $message, $sourceClass, $smartFileInfo);
+    }
+}
