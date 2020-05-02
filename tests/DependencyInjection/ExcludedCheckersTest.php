08b65b115a9610e325eec52db32da563727a0c9c
TomasVotruba <tomas.vot@gmail.com>
2020-05-02 22:48:47 +0200

    [CS] Remove unused MethodWrapperFactory, PropertyAccessWrapperFactory

---
+++ tests/DependencyInjection/ExcludedCheckersTest.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\EasyCodingStandard\Tests\DependencyInjection;
+
+use Symplify\EasyCodingStandard\FixerRunner\Application\FixerFileProcessor;
+use Symplify\EasyCodingStandard\HttpKernel\EasyCodingStandardKernel;
+use Symplify\PackageBuilder\Tests\AbstractKernelTestCase;
+
+final class ExcludedCheckersTest extends AbstractKernelTestCase
+{
+    protected function setUp(): void
+    {
+        $this->bootKernelWithConfigs(EasyCodingStandardKernel::class, [__DIR__ . '/ExcludedCheckersSource/config.yml']);
+    }
+
+    public function test(): void
+    {
+        $fixerFileProcessor = self::$container->get(FixerFileProcessor::class);
+        $this->assertCount(0, $fixerFileProcessor->getCheckers());
+    }
+}
---
+++ tests/Indentation/IndentationTest.php
@@ -0,0 +1,75 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\EasyCodingStandard\Tests\Indentation;
+
+use PhpCsFixer\Fixer\Whitespace\IndentationTypeFixer;
+use PhpCsFixer\Fixer\WhitespacesAwareFixerInterface;
+use PhpCsFixer\WhitespacesFixerConfig;
+use Psr\Container\ContainerInterface;
+use Symplify\EasyCodingStandard\FixerRunner\Application\FixerFileProcessor;
+use Symplify\EasyCodingStandard\HttpKernel\EasyCodingStandardKernel;
+use Symplify\PackageBuilder\Configuration\StaticEolConfiguration;
+use Symplify\PackageBuilder\Reflection\PrivatesAccessor;
+use Symplify\PackageBuilder\Tests\AbstractKernelTestCase;
+
+final class IndentationTest extends AbstractKernelTestCase
+{
+    /**
+     * @var PrivatesAccessor
+     */
+    private $privatesAccessor;
+
+    protected function setUp(): void
+    {
+        $this->privatesAccessor = new PrivatesAccessor();
+    }
+
+    public function testSpaces(): void
+    {
+        $this->bootKernelWithConfigs(
+            EasyCodingStandardKernel::class,
+            [__DIR__ . '/IndentationSource/config-with-spaces-indentation.yml']
+        );
+
+        $indentationTypeFixer = $this->getIndentationTypeFixerFromContainer(self::$container);
+
+        $this->assertInstanceOf(WhitespacesAwareFixerInterface::class, $indentationTypeFixer);
+        $spacesConfig = new WhitespacesFixerConfig('    ', StaticEolConfiguration::getEolChar());
+
+        $fixerWhitespaceConfig = $this->privatesAccessor->getPrivateProperty(
+            $indentationTypeFixer,
+            'whitespacesConfig'
+        );
+        $this->assertEquals($spacesConfig, $fixerWhitespaceConfig);
+    }
+
+    public function testTabs(): void
+    {
+        $this->bootKernelWithConfigs(
+            EasyCodingStandardKernel::class,
+            [__DIR__ . '/IndentationSource/config-with-tabs-indentation.yml']
+        );
+
+        $indentationTypeFixer = $this->getIndentationTypeFixerFromContainer(self::$container);
+
+        $this->assertInstanceOf(WhitespacesAwareFixerInterface::class, $indentationTypeFixer);
+        $tabsConfig = new WhitespacesFixerConfig('	', StaticEolConfiguration::getEolChar());
+
+        $fixerWhitespaceConfig = $this->privatesAccessor->getPrivateProperty(
+            $indentationTypeFixer,
+            'whitespacesConfig'
+        );
+        $this->assertEquals($tabsConfig, $fixerWhitespaceConfig);
+    }
+
+    private function getIndentationTypeFixerFromContainer(ContainerInterface $container): IndentationTypeFixer
+    {
+        $fixerFileProcessor = $container->get(FixerFileProcessor::class);
+        $checkers = $fixerFileProcessor->getCheckers();
+        $this->assertCount(1, $checkers);
+
+        return array_pop($checkers);
+    }
+}
