abcc9e0de090ab37f2de577ef1248c083f2935cb
TomasVotruba <tomas.vot@gmail.com>
2020-05-02 03:04:58 +0200

    [CS] Deprecate ForbiddenParentClassSniff

---
+++ packages/sniff-runner/src/File/FileFactory.php
@@ -0,0 +1,68 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\EasyCodingStandard\SniffRunner\File;
+
+use PHP_CodeSniffer\Fixer;
+use Symplify\EasyCodingStandard\Application\AppliedCheckersCollector;
+use Symplify\EasyCodingStandard\Console\Style\EasyCodingStandardStyle;
+use Symplify\EasyCodingStandard\Error\ErrorAndDiffCollector;
+use Symplify\EasyCodingStandard\Skipper;
+use Symplify\EasyCodingStandard\SniffRunner\ValueObject\File;
+use Symplify\SmartFileSystem\SmartFileInfo;
+
+final class FileFactory
+{
+    /**
+     * @var Fixer
+     */
+    private $fixer;
+
+    /**
+     * @var ErrorAndDiffCollector
+     */
+    private $errorAndDiffCollector;
+
+    /**
+     * @var Skipper
+     */
+    private $skipper;
+
+    /**
+     * @var AppliedCheckersCollector
+     */
+    private $appliedCheckersCollector;
+
+    /**
+     * @var EasyCodingStandardStyle
+     */
+    private $easyCodingStandardStyle;
+
+    public function __construct(
+        Fixer $fixer,
+        ErrorAndDiffCollector $errorAndDiffCollector,
+        Skipper $skipper,
+        AppliedCheckersCollector $appliedCheckersCollector,
+        EasyCodingStandardStyle $easyCodingStandardStyle
+    ) {
+        $this->fixer = $fixer;
+        $this->errorAndDiffCollector = $errorAndDiffCollector;
+        $this->skipper = $skipper;
+        $this->appliedCheckersCollector = $appliedCheckersCollector;
+        $this->easyCodingStandardStyle = $easyCodingStandardStyle;
+    }
+
+    public function createFromFileInfo(SmartFileInfo $smartFileInfo): File
+    {
+        return new File(
+            $smartFileInfo->getRelativeFilePath(),
+            $smartFileInfo->getContents(),
+            $this->fixer,
+            $this->errorAndDiffCollector,
+            $this->skipper,
+            $this->appliedCheckersCollector,
+            $this->easyCodingStandardStyle
+        );
+    }
+}
