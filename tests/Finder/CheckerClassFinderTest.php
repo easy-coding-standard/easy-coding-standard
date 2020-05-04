0ac9fcae92fdf63f96e356be3c8cc609fab9d6e2
TomasVotruba <tomas.vot@gmail.com>
2020-05-04 12:14:51 +0200

    open 7.4-dev

---
+++ tests/Finder/CheckerClassFinderTest.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\EasyCodingStandard\Tests\Finder;
+
+use PHPUnit\Framework\TestCase;
+use Symplify\EasyCodingStandard\Finder\CheckerClassFinder;
+use Symplify\PackageBuilder\Composer\StaticVendorDirProvider;
+
+final class CheckerClassFinderTest extends TestCase
+{
+    public function test(): void
+    {
+        $checkerClassFinder = new CheckerClassFinder();
+        $checkerClasses = $checkerClassFinder->findInDirectories([StaticVendorDirProvider::provide()]);
+
+        $this->assertGreaterThan(250, $checkerClasses);
+    }
+}
