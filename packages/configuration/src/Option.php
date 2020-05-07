68eef0b3cfd9130a4653da0f27f0c6f79ab62e5a
TomasVotruba <tomas.vot@gmail.com>
2020-05-07 15:41:49 +0200

    [CS] Add NoDynamicPropertyFetchNameRule

---
+++ packages/configuration/src/Option.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\EasyCodingStandard\Configuration;
+
+final class Option
+{
+    /**
+     * @var string
+     */
+    public const SOURCE = 'source';
+
+    /**
+     * @var string
+     */
+    public const FIX = 'fix';
+
+    /**
+     * @var string
+     */
+    public const CLEAR_CACHE = 'clear-cache';
+
+    /**
+     * @var string
+     */
+    public const NO_PROGRESS_BAR = 'no-progress-bar';
+
+    /**
+     * @var string
+     */
+    public const NO_ERROR_TABLE = 'no-error-table';
+
+    /**
+     * @var string
+     */
+    public const OUTPUT_FORMAT_OPTION = 'output-format';
+
+    /**
+     * @var string
+     */
+    public const SKIP = 'skip';
+}
