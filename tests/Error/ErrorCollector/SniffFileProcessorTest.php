abb010d36ff1feecfbe55bf9c0ec823b3de8f7ae
TomasVotruba <tomas.vot@gmail.com>
2020-05-07 15:55:57 +0200

    [CS] Add NoNullableParameterValueRule

---
+++ tests/Error/ErrorCollector/SniffFileProcessorTest.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\EasyCodingStandard\Tests\Error\ErrorCollector;
+
+use Symfony\Component\Console\Output\OutputInterface;
+use Symplify\EasyCodingStandard\ChangedFilesDetector\ChangedFilesDetector;
+use Symplify\EasyCodingStandard\Console\Style\EasyCodingStandardStyle;
+use Symplify\EasyCodingStandard\Error\ErrorAndDiffCollector;
+use Symplify\EasyCodingStandard\HttpKernel\EasyCodingStandardKernel;
+use Symplify\EasyCodingStandard\SniffRunner\Application\SniffFileProcessor;
+use Symplify\PackageBuilder\Tests\AbstractKernelTestCase;
+use Symplify\SmartFileSystem\SmartFileInfo;
+
+final class SniffFileProcessorTest extends AbstractKernelTestCase
+{
+    /**
+     * @var ErrorAndDiffCollector
+     */
+    private $errorAndDiffCollector;
+
+    /**
+     * @var SniffFileProcessor
+     */
+    private $sniffFileProcessor;
+
+    protected function setUp(): void
+    {
+        $this->bootKernelWithConfigs(
+            EasyCodingStandardKernel::class,
+            [__DIR__ . '/SniffRunnerSource/easy-coding-standard.yml']
+        );
+
+        $this->errorAndDiffCollector = self::$container->get(ErrorAndDiffCollector::class);
+        $this->sniffFileProcessor = self::$container->get(SniffFileProcessor::class);
+
+        // silent output
+        $easyCodingStandardStyle = self::$container->get(EasyCodingStandardStyle::class);
+        $easyCodingStandardStyle->setVerbosity(OutputInterface::VERBOSITY_QUIET);
+
+        $changedFilesDetector = self::$container->get(ChangedFilesDetector::class);
+        $changedFilesDetector->clearCache();
+    }
+
+    public function test(): void
+    {
+        $smartFileInfo = new SmartFileInfo(__DIR__ . '/ErrorCollectorSource/NotPsr2Class.php.inc');
+        $this->sniffFileProcessor->processFile($smartFileInfo);
+
+        $this->assertSame(3, $this->errorAndDiffCollector->getErrorCount());
+        $this->assertSame(0, $this->errorAndDiffCollector->getFileDiffsCount());
+    }
+}
