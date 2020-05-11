1707828204c75d8f9afbde31fa76d91e2f1a8574
TomasVotruba <tomas.vot@gmail.com>
2020-05-11 14:44:40 +0200

    [ECS] drop types set, mostly buggy

---
+++ src/EasyCodingStandardBundle.php
@@ -0,0 +1,63 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\EasyCodingStandard;
+
+use Nette\Utils\Strings;
+use PHP_CodeSniffer\Sniffs\Sniff;
+use PhpCsFixer\Fixer\FixerInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symplify\AutoBindParameter\DependencyInjection\CompilerPass\AutoBindParameterCompilerPass;
+use Symplify\AutowireArrayParameter\DependencyInjection\CompilerPass\AutowireArrayParameterCompilerPass;
+use Symplify\EasyCodingStandard\Contract\Console\Output\OutputFormatterInterface;
+use Symplify\EasyCodingStandard\DependencyInjection\CompilerPass\ConflictingCheckersCompilerPass;
+use Symplify\EasyCodingStandard\DependencyInjection\CompilerPass\FixerWhitespaceConfigCompilerPass;
+use Symplify\EasyCodingStandard\DependencyInjection\CompilerPass\RemoveExcludedCheckersCompilerPass;
+use Symplify\EasyCodingStandard\DependencyInjection\CompilerPass\RemoveMutualCheckersCompilerPass;
+use Symplify\EasyCodingStandard\DependencyInjection\Extension\EasyCodingStandardExtension;
+use Symplify\PackageBuilder\DependencyInjection\CompilerPass\AutoReturnFactoryCompilerPass;
+use Symplify\PackageBuilder\DependencyInjection\CompilerPass\AutowireInterfacesCompilerPass;
+
+final class EasyCodingStandardBundle extends Bundle
+{
+    /**
+     * Order of compiler passes matters!
+     */
+    public function build(ContainerBuilder $containerBuilder): void
+    {
+        $containerBuilder->setParameter('sys_get_temp_dir', sys_get_temp_dir());
+        $containerBuilder->setParameter('getcwd_webalized', Strings::webalize(getcwd()));
+
+        // needs to be first, since it's adding new service definitions
+        $containerBuilder->addCompilerPass(new AutoReturnFactoryCompilerPass());
+
+        // cleanup
+        $containerBuilder->addCompilerPass(new RemoveExcludedCheckersCompilerPass());
+        $containerBuilder->addCompilerPass(new RemoveMutualCheckersCompilerPass());
+
+        // autowire
+        $containerBuilder->addCompilerPass(new AutowireInterfacesCompilerPass([
+            FixerInterface::class,
+            Sniff::class,
+            OutputFormatterInterface::class,
+        ]));
+        $containerBuilder->addCompilerPass(new AutowireArrayParameterCompilerPass());
+
+        // exceptions
+        $containerBuilder->addCompilerPass(new ConflictingCheckersCompilerPass());
+
+        // parameters
+        $containerBuilder->addCompilerPass(new AutoBindParameterCompilerPass());
+
+        // method calls
+        $containerBuilder->addCompilerPass(new FixerWhitespaceConfigCompilerPass());
+    }
+
+    protected function createContainerExtension(): ?ExtensionInterface
+    {
+        return new EasyCodingStandardExtension();
+    }
+}
