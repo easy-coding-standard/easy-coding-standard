1c826d98be06f69c717a11419998f948ccac91e7
TomasVotruba <tomas.vot@gmail.com>
2020-05-15 22:36:22 +0200

    [ECS] Dislocate bundle locations to prevent symfony/flex autoregistration

---
+++ packages/fixer-runner/src/Exception/Application/FixerFailedException.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\EasyCodingStandard\FixerRunner\Exception\Application;
+
+use Exception;
+
+final class FixerFailedException extends Exception
+{
+}
