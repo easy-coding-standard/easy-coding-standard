bbf9027df22e96e70d23b138eb772e0e4f872b55
TomasVotruba <tomas.vot@gmail.com>
2020-05-16 13:17:43 +0200

    fix CheckersClassFinderTest

---
+++ tests/Issues/Issue1449Test.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\EasyCodingStandard\Tests\Issues;
+
+use Symplify\EasyCodingStandardTester\Testing\AbstractCheckerTestCase;
+
+/**
+ * @see https://github.com/symplify/symplify/issues/1449
+ */
+final class Issue1449Test extends AbstractCheckerTestCase
+{
+    public function test(): void
+    {
+        $this->doTestWrongToFixedFile(__DIR__ . '/wrong/wrong1449.php.inc', __DIR__ . '/fixed/fixed1449.php.inc');
+    }
+
+    protected function provideConfig(): string
+    {
+        return __DIR__ . '/config/config1449.yml';
+    }
+}
