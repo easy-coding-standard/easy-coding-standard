eca43ab5f50fb2e2de9c2606a2d25b724691e8e3
TomasVotruba <tomas.vot@gmail.com>
2020-05-18 18:45:40 +0200

    bump rector version

---
+++ src/Exception/Application/MissingCheckersForChangedFileException.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\EasyCodingStandard\Exception\Application;
+
+use Exception;
+
+final class MissingCheckersForChangedFileException extends Exception
+{
+}
---
+++ tests/Set/Psr12/Psr12Test.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\EasyCodingStandard\Tests\Set\Psr12;
+
+use Symplify\EasyCodingStandardTester\Testing\AbstractCheckerTestCase;
+
+final class Psr12Test extends AbstractCheckerTestCase
+{
+    public function test(): void
+    {
+        $this->doTestFiles([
+            [__DIR__ . '/wrong/wrong.php.inc', __DIR__ . '/fixed/fixed.php.inc'],
+            [__DIR__ . '/wrong/wrong2.php.inc', __DIR__ . '/fixed/fixed2.php.inc'],
+            // yield [__DIR__ . '/wrong/wrong3.php.inc', __DIR__ . '/fixed/fixed3.php.inc']; - not covered yet
+        ]);
+    }
+
+    protected function provideConfig(): string
+    {
+        return __DIR__ . '/../../../config/set/psr12.yaml';
+    }
+}
