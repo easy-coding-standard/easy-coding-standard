0f8ed7880a6d32a34ee5b0d3d0ae1c4c9e471df2
TomasVotruba <tomas.vot@gmail.com>
2020-05-18 20:20:49 +0200

    [CL] Simplify ChangelogLinkerTest

---
+++ packages/sniff-runner/tests/Error/ErrorSorterTest.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\EasyCodingStandard\SniffRunner\Tests\Error;
+
+use Symplify\EasyCodingStandard\Error\ErrorFactory;
+use Symplify\EasyCodingStandard\Error\ErrorSorter;
+use Symplify\EasyCodingStandard\HttpKernel\EasyCodingStandardKernel;
+use Symplify\EasyCodingStandard\ValueObject\Error\Error;
+use Symplify\PackageBuilder\Tests\AbstractKernelTestCase;
+use Symplify\SmartFileSystem\SmartFileInfo;
+
+final class ErrorSorterTest extends AbstractKernelTestCase
+{
+    /**
+     * @var ErrorSorter
+     */
+    private $errorSorter;
+
+    /**
+     * @var ErrorFactory
+     */
+    private $errorFactory;
+
+    protected function setUp(): void
+    {
+        $this->bootKernel(EasyCodingStandardKernel::class);
+
+        $this->errorSorter = self::$container->get(ErrorSorter::class);
+        $this->errorFactory = self::$container->get(ErrorFactory::class);
+    }
+
+    public function test(): void
+    {
+        /** @var Error[][] $sortedMessages */
+        $sortedMessages = $this->errorSorter->sortByFileAndLine($this->getUnsortedMessages());
+
+        $this->assertSame(['anotherFilePath', 'filePath'], array_keys($sortedMessages));
+        $this->assertSame(5, $sortedMessages['anotherFilePath'][0]->getLine());
+        $this->assertSame(15, $sortedMessages['anotherFilePath'][1]->getLine());
+    }
+
+    /**
+     * @return Error[][]
+     */
+    private function getUnsortedMessages(): array
+    {
+        $fileInfo = new SmartFileInfo(__DIR__ . '/ErrorSorterSource/SomeFile.php');
+
+        $firstError = $this->errorFactory->create(5, 'error message', 'SomeClass', $fileInfo);
+        $secondError = $this->errorFactory->create(15, 'error message', 'SomeClass', $fileInfo);
+        $thirdError = $this->errorFactory->create(5, 'error message', 'SomeClass', $fileInfo);
+
+        return [
+            'filePath' => [$firstError],
+            'anotherFilePath' => [$secondError, $thirdError],
+        ];
+    }
+}
---
+++ tests/Console/Output/JsonOutputFormatterTest.php
@@ -0,0 +1,82 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\EasyCodingStandard\Tests\Console\Output;
+
+use Symfony\Component\Console\Input\StringInput;
+use Symfony\Component\Console\Output\BufferedOutput;
+use Symfony\Component\Console\Terminal;
+use Symplify\EasyCodingStandard\Configuration\Configuration;
+use Symplify\EasyCodingStandard\Configuration\Option;
+use Symplify\EasyCodingStandard\Console\EasyCodingStandardConsoleApplication;
+use Symplify\EasyCodingStandard\Console\Output\JsonOutputFormatter;
+use Symplify\EasyCodingStandard\Console\Style\EasyCodingStandardStyle;
+use Symplify\EasyCodingStandard\HttpKernel\EasyCodingStandardKernel;
+use Symplify\PackageBuilder\Console\ShellCode;
+use Symplify\PackageBuilder\Tests\AbstractKernelTestCase;
+
+/**
+ * @covers \Symplify\EasyCodingStandard\Console\Output\JsonOutputFormatter
+ */
+final class JsonOutputFormatterTest extends AbstractKernelTestCase
+{
+    /**
+     * @var EasyCodingStandardConsoleApplication
+     */
+    private $easyCodingStandardConsoleApplication;
+
+    /**
+     * @var BufferedOutput
+     */
+    private $bufferedOutput;
+
+    protected function setUp(): void
+    {
+        $config = __DIR__ . '/config/config.yml';
+
+        $this->bootKernelWithConfigs(EasyCodingStandardKernel::class, [$config]);
+
+        $easyCodingStandardStyle = $this->createEasyCodingStandardStyleWithBufferOutput();
+        self::$container->set(EasyCodingStandardStyle::class, $easyCodingStandardStyle);
+
+        // simulate config autowiring
+        $configuration = self::$container->get(Configuration::class);
+        $configuration->setFirstResolverConfig($config);
+
+        $this->easyCodingStandardConsoleApplication = self::$container->get(
+            EasyCodingStandardConsoleApplication::class
+        );
+        $this->easyCodingStandardConsoleApplication->setAutoExit(false);
+    }
+
+    public function testCanPrintReport(): void
+    {
+        $escaped_path = addslashes(__DIR__);
+        $stringInput = [
+            'check',
+            $escaped_path . '/wrong/wrong.php.inc',
+            '--config',
+            $escaped_path . '/config/config.yml',
+            '--' . Option::OUTPUT_FORMAT_OPTION,
+            JsonOutputFormatter::NAME,
+        ];
+
+        $input = new StringInput(implode(' ', $stringInput));
+        $exitCode = $this->easyCodingStandardConsoleApplication->run($input);
+        $this->assertSame(ShellCode::ERROR, $exitCode);
+
+        $output = trim($this->bufferedOutput->fetch());
+        $this->assertStringMatchesFormatFile(__DIR__ . '/Source/expected_json_output.json', $output);
+    }
+
+    /**
+     * To catch printed content
+     */
+    private function createEasyCodingStandardStyleWithBufferOutput(): EasyCodingStandardStyle
+    {
+        $this->bufferedOutput = new BufferedOutput();
+
+        return new EasyCodingStandardStyle(new StringInput(''), $this->bufferedOutput, new Terminal());
+    }
+}
