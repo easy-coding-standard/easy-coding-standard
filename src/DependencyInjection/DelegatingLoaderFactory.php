8ceed45ce78be12be1120f551d3da0c58e843ed1
TomasVotruba <tomas.vot@gmail.com>
2020-07-14 17:10:38 +0200

    [CS] Switch symplify coding standard from YAML to PHP

--- src/DependencyInjection/DelegatingLoaderFactory.php
+++ src/DependencyInjection/DelegatingLoaderFactory.php
@@ -9,6 +9,7 @@ use Symfony\Component\Config\Loader\DelegatingLoader;
 use Symfony\Component\Config\Loader\GlobFileLoader;
 use Symfony\Component\Config\Loader\LoaderResolver;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\PhpFileLoader;
 use Symfony\Component\HttpKernel\Config\FileLocator;
 use Symfony\Component\HttpKernel\KernelInterface;
 use Symplify\EasyCodingStandard\Yaml\FileLoader\CheckerTolerantYamlFileLoader;
@@ -43,6 +44,7 @@ final class DelegatingLoaderFactory
     ): DelegatingLoader {
         $loaderResolver = new LoaderResolver([
             new GlobFileLoader($simpleFileLocator),
+            new PhpFileLoader($containerBuilder, $simpleFileLocator),
             new CheckerTolerantYamlFileLoader($containerBuilder, $simpleFileLocator),
         ]);
 
