6d05b0183131a024726c3324ea250bbec70fc79e
TomasVotruba <tomas.vot@gmail.com>
2020-07-14 18:38:39 +0200

    manual corrections

--- config/config.php
+++ config/config.php
@@ -2,21 +2,24 @@
 
 namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
+use Nette\Utils\Strings;
+
 return static function (ContainerConfigurator $containerConfigurator): void {
     $containerConfigurator->import(__DIR__ . '/services.php');
-    $containerConfigurator->import(__DIR__ . '/services/services_cache.php');
 
-    $containerConfigurator->import(__DIR__ . '/../packages/*/config/*.php', 'glob');
+    $containerConfigurator->import(__DIR__ . '/parameters/parameter_name_guard.php');
+
+    $containerConfigurator->import(__DIR__ . '/../packages/*/config/*.php');
 
     $parameters = $containerConfigurator->parameters();
 
     $parameters->set('indentation', 'spaces');
 
-    $parameters->set('line_ending', "\n");
+    $parameters->set('line_ending', PHP_EOL);
 
-    $parameters->set('cache_directory', '%sys_get_temp_dir%/_changed_files_detector%env(TEST_SUFFIX)%');
+    $parameters->set('cache_directory', sys_get_temp_dir() . '/_changed_files_detector%env(TEST_SUFFIX)%');
 
-    $parameters->set('cache_namespace', '%getcwd_webalized%');
+    $parameters->set('cache_namespace', Strings::webalize(getcwd()));
 
     $parameters->set('skip', []);
 
