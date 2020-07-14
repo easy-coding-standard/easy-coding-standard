21d481019ae3e767e51f847a41fbfb52d94e875a
TomasVotruba <tomas.vot@gmail.com>
2020-07-14 18:31:17 +0200

    [ECS] Move config from YAML to PHP

---
+++ packages/configuration/config/services.php
@@ -0,0 +1,14 @@
+<?php declare(strict_types=1);
+
+namespace Symfony\Component\DependencyInjection\Loader\Configurator;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    $services = $containerConfigurator->services();
+
+    $services->defaults()
+        ->public()
+        ->autowire();
+
+    $services->load('Symplify\EasyCodingStandard\Configuration\\', __DIR__ . '/../src')
+        ->exclude([__DIR__ . '/../src/Exception/*']);
+};
