82edba15369f7ca1b1c8f61e8c62d64df4214068
TomasVotruba <tomas.vot@gmail.com>
2020-07-15 14:49:46 +0200

    [ECS] Move sets from YAML to PHP, keep BC layer

---
+++ config/set/common/docblock.php
@@ -0,0 +1,45 @@
+<?php declare(strict_types=1);
+
+namespace Symfony\Component\DependencyInjection\Loader\Configurator;
+
+use PhpCsFixer\Fixer\Comment\NoTrailingWhitespaceInCommentFixer;
+use PhpCsFixer\Fixer\Phpdoc\NoEmptyPhpdocFixer;
+use PhpCsFixer\Fixer\Phpdoc\NoSuperfluousPhpdocTagsFixer;
+use PhpCsFixer\Fixer\Phpdoc\PhpdocIndentFixer;
+use PhpCsFixer\Fixer\Phpdoc\PhpdocLineSpanFixer;
+use PhpCsFixer\Fixer\Phpdoc\PhpdocNoEmptyReturnFixer;
+use PhpCsFixer\Fixer\Phpdoc\PhpdocReturnSelfReferenceFixer;
+use PhpCsFixer\Fixer\Phpdoc\PhpdocTrimConsecutiveBlankLineSeparationFixer;
+use PhpCsFixer\Fixer\Phpdoc\PhpdocTrimFixer;
+use PhpCsFixer\Fixer\Phpdoc\PhpdocTypesFixer;
+use PhpCsFixer\Fixer\Phpdoc\PhpdocVarWithoutNameFixer;
+use SlevomatCodingStandard\Sniffs\TypeHints\NullTypeHintOnLastPositionSniff;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    $services = $containerConfigurator->services();
+
+    $services->set(PhpdocLineSpanFixer::class);
+
+    $services->set(NullTypeHintOnLastPositionSniff::class);
+
+    $services->set(NoTrailingWhitespaceInCommentFixer::class);
+
+    $services->set(PhpdocTrimConsecutiveBlankLineSeparationFixer::class);
+
+    $services->set(PhpdocTrimFixer::class);
+
+    $services->set(NoEmptyPhpdocFixer::class);
+
+    $services->set(PhpdocNoEmptyReturnFixer::class);
+
+    $services->set(PhpdocIndentFixer::class);
+
+    $services->set(PhpdocTypesFixer::class);
+
+    $services->set(PhpdocReturnSelfReferenceFixer::class);
+
+    $services->set(PhpdocVarWithoutNameFixer::class);
+
+    $services->set(NoSuperfluousPhpdocTagsFixer::class)
+        ->call('configure', [['remove_inheritdoc' => true]]);
+};
