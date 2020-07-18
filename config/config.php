5e7801c83fa484b0bd851176d030adb659cf7c09
TomasVotruba <tomas.vot@gmail.com>
2020-07-18 12:45:26 +0200

    [SetConfigResolver] Prefer .php version, return instant match

---
+++ config/config.php
@@ -0,0 +1,39 @@
+<?php declare(strict_types=1);
+
+use Nette\Utils\Strings;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symplify\EasyCodingStandard\Configuration\Option;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    $containerConfigurator->import(__DIR__ . '/services.php');
+
+    $containerConfigurator->import(__DIR__ . '/parameters/parameter_name_guard.php');
+
+    $containerConfigurator->import(__DIR__ . '/../packages/*/config/*.php');
+
+    $parameters = $containerConfigurator->parameters();
+
+    $parameters->set('indentation', 'spaces');
+
+    $parameters->set('line_ending', PHP_EOL);
+
+    $parameters->set('cache_directory', sys_get_temp_dir() . '/_changed_files_detector%env(TEST_SUFFIX)%');
+
+    $parameters->set('cache_namespace', Strings::webalize(getcwd()));
+
+    $parameters->set(Option::SKIP, []);
+
+    $parameters->set(Option::ONLY, []);
+
+    $parameters->set(Option::PATHS, []);
+
+    $parameters->set(Option::SETS, []);
+
+    $parameters->set('file_extensions', ['php']);
+
+    $parameters->set(Option::EXCLUDE_PATHS, []);
+
+    $parameters->set(Option::EXCLUDE_FILES, []);
+
+    $parameters->set('env(TEST_SUFFIX)', '');
+};
