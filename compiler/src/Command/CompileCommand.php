bdb05979b6c57b33a345d68aa925faddb55123de
TomasVotruba <tomas.vot@gmail.com>
2020-07-21 01:57:57 +0200

    switch Rector YAML to PHP

--- compiler/src/Command/CompileCommand.php
+++ compiler/src/Command/CompileCommand.php
@@ -4,13 +4,14 @@ declare(strict_types=1);
 
 namespace Symplify\EasyCodingStandard\Compiler\Command;
 
-use Nette\Utils\FileSystem as NetteFileSystem;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use Symplify\EasyCodingStandard\Compiler\Composer\ComposerJsonManipulator;
 use Symplify\EasyCodingStandard\Compiler\Process\SymfonyProcess;
+use Symplify\PackageBuilder\Console\ShellCode;
+use Symplify\SmartFileSystem\SmartFileSystem;
 
 /**
  * Inspired by @see https://github.com/phpstan/phpstan-src/blob/f939d23155627b5c2ec6eef36d976dddea22c0c5/compiler/src/Console/CompileCommand.php
@@ -42,11 +43,17 @@ final class CompileCommand extends Command
      */
     private $symfonyStyle;
 
+    /**
+     * @var SmartFileSystem
+     */
+    private $smartFileSystem;
+
     public function __construct(
         string $dataDir,
         string $buildDir,
         SymfonyStyle $symfonyStyle,
-        ComposerJsonManipulator $composerJsonManipulator
+        ComposerJsonManipulator $composerJsonManipulator,
+        SmartFileSystem $smartFileSystem
     ) {
         parent::__construct();
 
@@ -55,6 +62,7 @@ final class CompileCommand extends Command
 
         $this->symfonyStyle = $symfonyStyle;
         $this->composerJsonManipulator = $composerJsonManipulator;
+        $this->smartFileSystem = $smartFileSystem;
     }
 
     protected function configure(): void
@@ -76,19 +84,15 @@ final class CompileCommand extends Command
 
         $this->symfonyStyle->title('2. Running "composer update" for new config');
         // @see https://github.com/dotherightthing/wpdtrt-plugin-boilerplate/issues/52
-        new SymfonyProcess(
-            [
-                'composer',
-                'update',
-                '--no-dev',
-                '--prefer-dist',
-                '--no-interaction',
-                '--classmap-authoritative',
-                '--ansi',
-            ],
-            $this->buildDir,
-            $output
-        );
+        new SymfonyProcess([
+            'composer',
+            'update',
+            '--no-dev',
+            '--prefer-dist',
+            '--no-interaction',
+            '--classmap-authoritative',
+            '--ansi',
+        ], $this->buildDir, $output);
 
         $this->symfonyStyle->title('3. Packing and prefixing ecs.phar with Box and PHP Scoper');
         // parallel prevention is just for single less-buggy process
@@ -101,7 +105,7 @@ final class CompileCommand extends Command
         $this->symfonyStyle->success('ecs.phar was generated');
 
         // success
-        return 0;
+        return ShellCode::SUCCESS;
     }
 
     private function cleanupPhpCsFixerBreakingFiles(): void
@@ -111,8 +115,6 @@ final class CompileCommand extends Command
             __DIR__ . '/../../../vendor/friendsofphp/php-cs-fixer/src/Test/AbstractIntegrationTestCase.php',
         ];
 
-        foreach ($filesToRemove as $fileToRemove) {
-            NetteFileSystem::delete($fileToRemove);
-        }
+        $this->smartFileSystem->remove($filesToRemove);
     }
 }
