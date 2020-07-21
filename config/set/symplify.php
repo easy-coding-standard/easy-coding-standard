179ba34604f9b63918f2244e1df26e004b382bb5
TomasVotruba <tomas.vot@gmail.com>
2020-07-21 14:35:29 +0200

    cleanup

---
+++ config/set/symplify.php
@@ -0,0 +1,14 @@
+<?php declare(strict_types=1);
+
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    # A. monorepo
+    $containerConfigurator->import(__DIR__ . '/../../../coding-standard/config/symplify.php', null, 'not_found');
+
+    # B. installed as dependency
+    $containerConfigurator->import(__DIR__ . '/../../vendor/symplify/coding-standard/config/symplify.php', null, 'not_found');
+
+    # C. installed in PHAR
+    $containerConfigurator->import('phar://vendor/symplify/coding-standard/config/symplify.php', null, 'not_found');
+};
