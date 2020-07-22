13bb5f16ffb97e8888bc12fe4e25237a7eacc9f3
TomasVotruba <tomas.vot@gmail.com>
2020-07-22 12:05:49 +0200

    ci: trigger

---
+++ src/HttpKernel/EasyCodingStandardKernel.php
@@ -0,0 +1,82 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\EasyCodingStandard\HttpKernel;
+
+use Symfony\Component\Config\Loader\DelegatingLoader;
+use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpKernel\Bundle\BundleInterface;
+use Symfony\Component\HttpKernel\Kernel;
+use Symplify\CodingStandard\Bundle\SymplifyCodingStandardBundle;
+use Symplify\ConsoleColorDiff\ConsoleColorDiffBundle;
+use Symplify\EasyCodingStandard\Bundle\EasyCodingStandardBundle;
+use Symplify\EasyCodingStandard\DependencyInjection\DelegatingLoaderFactory;
+use Symplify\PackageBuilder\Contract\HttpKernel\ExtraConfigAwareKernelInterface;
+use Symplify\ParameterNameGuard\Bundle\ParameterNameGuardBundle;
+use Symplify\SmartFileSystem\SmartFileInfo;
+
+final class EasyCodingStandardKernel extends Kernel implements ExtraConfigAwareKernelInterface
+{
+    /**
+     * @var string[]
+     */
+    private $configs = [];
+
+    public function registerContainerConfiguration(LoaderInterface $loader): void
+    {
+        foreach ($this->configs as $config) {
+            $loader->load($config);
+        }
+    }
+
+    public function getCacheDir(): string
+    {
+        return sys_get_temp_dir() . '/easy_coding_standard';
+    }
+
+    public function getLogDir(): string
+    {
+        return sys_get_temp_dir() . '/easy_coding_standard_logs';
+    }
+
+    /**
+     * @return BundleInterface[]
+     */
+    public function registerBundles(): iterable
+    {
+        return [
+            new EasyCodingStandardBundle(),
+            new SymplifyCodingStandardBundle(),
+            new ConsoleColorDiffBundle(),
+            new ParameterNameGuardBundle(),
+        ];
+    }
+
+    /**
+     * @param string[]|SmartFileInfo[] $configs
+     */
+    public function setConfigs(array $configs): void
+    {
+        foreach ($configs as $config) {
+            if ($config instanceof SmartFileInfo) {
+                // for phar compatibility @see https://github.com/rectorphp/rector/commit/e5d7cee69558f7e6b35d995a5ca03fa481b0407c
+                $config = $config->getRealPath() ?: $config->getPathname();
+            }
+
+            $this->configs[] = $config;
+        }
+    }
+
+    /**
+     * @param ContainerInterface|ContainerBuilder $container
+     */
+    protected function getContainerLoader(ContainerInterface $container): DelegatingLoader
+    {
+        $delegatingLoaderFactory = new DelegatingLoaderFactory();
+
+        return $delegatingLoaderFactory->createFromContainerBuilderAndKernel($container, $this);
+    }
+}
