6ef42c06975a7f5481dd02a9f286a2fba5ac55a7
Jeffrey Cafferata <jeffrey@jcid.nl>
2020-08-06 21:12:15 +0200

    Update ecs.php
    Removed the double `SetList::SYMPLIFY` from the `Option::SETS` list.

---
+++ src/DependencyInjection/CompilerPass/ConflictingCheckersCompilerPass.php
@@ -0,0 +1,75 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\EasyCodingStandard\DependencyInjection\CompilerPass;
+
+use PHP_CodeSniffer\Standards\Generic\Sniffs\PHP\LowerCaseConstantSniff;
+use PHP_CodeSniffer\Standards\Generic\Sniffs\PHP\UpperCaseConstantSniff;
+use PHP_CodeSniffer\Standards\PSR12\Sniffs\Files\FileHeaderSniff;
+use PhpCsFixer\Fixer\Casing\ConstantCaseFixer;
+use PhpCsFixer\Fixer\Casing\LowercaseConstantsFixer;
+use PhpCsFixer\Fixer\ControlStructure\YodaStyleFixer;
+use PhpCsFixer\Fixer\LanguageConstruct\DeclareEqualNormalizeFixer;
+use PhpCsFixer\Fixer\Operator\NewWithBracesFixer;
+use PhpCsFixer\Fixer\Phpdoc\NoBlankLinesAfterPhpdocFixer;
+use PhpCsFixer\Fixer\PhpTag\BlankLineAfterOpeningTagFixer;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symplify\EasyCodingStandard\Configuration\Exception\ConflictingCheckersLoadedException;
+
+final class ConflictingCheckersCompilerPass implements CompilerPassInterface
+{
+    /**
+     * These groups do the opposite of each other, e.g. Yoda vs NoYoda.
+     *
+     * @var string[][]
+     */
+    private const CONFLICTING_CHECKER_GROUPS = [
+        [
+            'SlevomatCodingStandard\Sniffs\ControlStructures\DisallowYodaComparisonSniff',
+            YodaStyleFixer::class,
+        ], [LowerCaseConstantSniff::class, UpperCaseConstantSniff::class], [
+            LowercaseConstantsFixer::class,
+            UpperCaseConstantSniff::class,
+        ], [ConstantCaseFixer::class, UpperCaseConstantSniff::class], [
+            'Symplify\CodingStandard\Sniffs\ControlStructures\NewClassSniff',
+            NewWithBracesFixer::class,
+        ], ['SlevomatCodingStandard\Sniffs\TypeHints\DeclareStrictTypesSniff', DeclareEqualNormalizeFixer::class], [
+            'SlevomatCodingStandard\Sniffs\TypeHints\DeclareStrictTypesSniff',
+            BlankLineAfterOpeningTagFixer::class,
+        ], [FileHeaderSniff::class, NoBlankLinesAfterPhpdocFixer::class],
+    ];
+
+    public function process(ContainerBuilder $containerBuilder): void
+    {
+        $checkers = $containerBuilder->getServiceIds();
+        if (count($checkers) === 0) {
+            return;
+        }
+
+        foreach (self::CONFLICTING_CHECKER_GROUPS as $viceVersaMatchingCheckerGroup) {
+            if (! $this->isMatch($checkers, $viceVersaMatchingCheckerGroup)) {
+                continue;
+            }
+
+            throw new ConflictingCheckersLoadedException(sprintf(
+                'Checkers "%s" mutually exclude each other. Use only one or exclude '
+                . 'the unwanted one in "parameters > skip" in your config.',
+                implode('" and "', $viceVersaMatchingCheckerGroup)
+            ));
+        }
+    }
+
+    /**
+     * @param mixed[] $checkers
+     * @param string[] $matchingCheckerGroup
+     */
+    private function isMatch(array $checkers, array $matchingCheckerGroup): bool
+    {
+        $checkers = array_flip($checkers);
+        $matchingCheckerGroup = array_flip($matchingCheckerGroup);
+
+        return count(array_intersect_key($matchingCheckerGroup, $checkers)) === count($matchingCheckerGroup);
+    }
+}
