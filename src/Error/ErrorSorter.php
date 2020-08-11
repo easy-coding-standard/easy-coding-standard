968393690ddee9fa82b038fba71d5282f78c60ac
Tomas Votruba <tomas.vot@gmail.com>
2020-08-11 23:30:58 +0200

    Merge pull request #2051 from seb-jean/patch-3
    Update php-cs-fixer-psr2.php

---
+++ src/Error/ErrorSorter.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\EasyCodingStandard\Error;
+
+use Symplify\EasyCodingStandard\ValueObject\Error\Error;
+
+final class ErrorSorter
+{
+    /**
+     * @param Error[][] $errorMessages
+     * @return Error[][]
+     */
+    public function sortByFileAndLine(array $errorMessages): array
+    {
+        ksort($errorMessages);
+
+        foreach ($errorMessages as $file => $errorMessagesForFile) {
+            usort($errorMessagesForFile, function (Error $first, Error $second): int {
+                return $first->getLine() <=> $second->getLine();
+            });
+
+            $errorMessages[$file] = $errorMessagesForFile;
+        }
+
+        return $errorMessages;
+    }
+}
