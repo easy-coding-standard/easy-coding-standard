c814593f4c4674a6cc5cc201db600205c8929220
Tomas Votruba <tomas.vot@gmail.com>
2020-08-12 14:00:59 +0200

    Merge branch 'master' into patch-1

---
+++ src/ValueObject/Error/Error.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\EasyCodingStandard\ValueObject\Error;
+
+use Symplify\SmartFileSystem\SmartFileInfo;
+
+final class Error
+{
+    /**
+     * @var int
+     */
+    private $line;
+
+    /**
+     * @var string
+     */
+    private $message;
+
+    /**
+     * @var string
+     */
+    private $sourceClass;
+
+    /**
+     * @var SmartFileInfo
+     */
+    private $fileInfo;
+
+    public function __construct(int $line, string $message, string $sourceClass, SmartFileInfo $fileInfo)
+    {
+        $this->line = $line;
+        $this->message = $message;
+        $this->sourceClass = $sourceClass;
+        $this->fileInfo = $fileInfo;
+    }
+
+    public function getLine(): int
+    {
+        return $this->line;
+    }
+
+    public function getMessage(): string
+    {
+        return $this->message;
+    }
+
+    public function getSourceClass(): string
+    {
+        return $this->sourceClass;
+    }
+
+    public function getFileInfo(): SmartFileInfo
+    {
+        return $this->fileInfo;
+    }
+}
