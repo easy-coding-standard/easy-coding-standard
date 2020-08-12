8f87078f1c2abcd210e9ee620d75731a65e3629f
TomasVotruba <tomas.vot@gmail.com>
2020-08-12 14:06:30 +0200

    [ParameterNameGuard] Deprecated for PHP config with constants

--- config/config.php
+++ config/config.php
@@ -1,4 +1,6 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 use Nette\Utils\Strings;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
@@ -6,33 +8,19 @@ use Symplify\EasyCodingStandard\Configuration\Option;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $containerConfigurator->import(__DIR__ . '/services.php');
-
-    $containerConfigurator->import(__DIR__ . '/parameters/parameter_name_guard.php');
-
     $containerConfigurator->import(__DIR__ . '/../packages/*/config/*.php');
 
     $parameters = $containerConfigurator->parameters();
-
     $parameters->set(Option::INDENTATION, Option::INDENTATION_SPACES);
-
     $parameters->set(Option::LINE_ENDING, PHP_EOL);
-
     $parameters->set(Option::CACHE_DIRECTORY, sys_get_temp_dir() . '/_changed_files_detector%env(TEST_SUFFIX)%');
-
     $parameters->set(Option::CACHE_NAMESPACE, Strings::webalize(getcwd()));
-
     $parameters->set(Option::SKIP, []);
-
     $parameters->set(Option::ONLY, []);
-
     $parameters->set(Option::PATHS, []);
-
     $parameters->set(Option::SETS, []);
-
     $parameters->set(Option::FILE_EXTENSIONS, ['php']);
-
     $parameters->set(Option::EXCLUDE_PATHS, []);
-
     $parameters->set(Option::EXCLUDE_FILES, []);
 
     $parameters->set('env(TEST_SUFFIX)', '');
