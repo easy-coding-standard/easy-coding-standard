9d5bfa49a6b533a9ccbd682ac30dd3197722d136
Tomas Votruba <tomas.vot@gmail.com>
2020-08-17 16:41:29 +0200

    [ECS] Add doctrine annotations set, switch set strings to constants (#2069)
    * [ECS] Add doctrine-annotations set
    
    * prepare to switch to set paths
    
    * [ECS] Use path-based sets
    
    * decopule invalid set reporter
    
    * fix ecs config invalidation
    
    * decopule bin classes to Bootstrap

--- config/set/clean-code.php
+++ config/set/clean-code.php
@@ -1,4 +1,6 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 use PhpCsFixer\Fixer\ArrayNotation\ArraySyntaxFixer;
 use PhpCsFixer\Fixer\ClassNotation\ProtectedToPrivateFixer;
