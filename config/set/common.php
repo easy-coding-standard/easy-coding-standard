fe51cd92879e6cc52c4d2008e12153f1271469e1
Jáchym Toušek <enumag@gmail.com>
2020-08-18 12:09:43 +0200

    [ECS] Fix common.php (#2075)

--- config/set/common.php
+++ config/set/common.php
@@ -5,5 +5,5 @@ declare(strict_types=1);
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
-    $containerConfigurator->import(__DIR__ . '/common/*');
+    $containerConfigurator->import(__DIR__ . '/common/*.php');
 };
