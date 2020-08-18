fc500e67cc9e0cde20f812195790a7326c2764c0
TomasVotruba <tomas.vot@gmail.com>
2020-08-18 15:37:45 +0200

    update CHANGELOG

---
+++ config/set/common/comments.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+use PHP_CodeSniffer\Standards\Generic\Sniffs\VersionControl\GitMergeConflictSniff;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    $services = $containerConfigurator->services();
+
+    $services->set(GitMergeConflictSniff::class);
+};
