ced97c15496400244e7e06b5f662e630cbc2c2d2
TomasVotruba <tomas.vot@gmail.com>
2020-08-18 17:45:03 +0200

    open 8.3-dev

---
+++ packages/sniff-runner/tests/ValueObject/FileTest.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\EasyCodingStandard\SniffRunner\Tests\ValueObject;
+
+use Symplify\EasyCodingStandard\Error\ErrorAndDiffCollector;
+use Symplify\EasyCodingStandard\HttpKernel\EasyCodingStandardKernel;
+use Symplify\EasyCodingStandard\SniffRunner\Exception\File\NotImplementedException;
+use Symplify\EasyCodingStandard\SniffRunner\File\FileFactory;
+use Symplify\EasyCodingStandard\SniffRunner\ValueObject\File;
+use Symplify\PackageBuilder\Tests\AbstractKernelTestCase;
+use Symplify\SmartFileSystem\SmartFileInfo;
+
+final class FileTest extends AbstractKernelTestCase
+{
+    /**
+     * @var File
+     */
+    private $file;
+
+    /**
+     * @var ErrorAndDiffCollector
+     */
+    private $errorAndDiffCollector;
+
+    protected function setUp(): void
+    {
+        $this->bootKernel(EasyCodingStandardKernel::class);
+
+        $this->errorAndDiffCollector = self::$container->get(ErrorAndDiffCollector::class);
+
+        $fileFactory = self::$container->get(FileFactory::class);
+        $fileInfo = new SmartFileInfo(__DIR__ . '/FileSource/SomeFile.php');
+
+        $this->file = $fileFactory->createFromFileInfo($fileInfo);
+        $this->file->processWithTokenListenersAndFileInfo([], $fileInfo);
+    }
+
+    public function testErrorDataCollector(): void
+    {
+        $this->assertSame(0, $this->errorAndDiffCollector->getErrorCount());
+
+        $this->file->addError('Some Error', 0, 'code');
+        $this->assertSame(1, $this->errorAndDiffCollector->getErrorCount());
+        $this->assertSame(0, $this->errorAndDiffCollector->getFileDiffsCount());
+    }
+
+    public function testNotImplementedGetErrorCount(): void
+    {
+        $this->expectException(NotImplementedException::class);
+        $this->file->getErrorCount();
+    }
+
+    public function testNotImplementedGetErrors(): void
+    {
+        $this->expectException(NotImplementedException::class);
+        $this->file->getErrors();
+    }
+}
