4d16694e3daee841f6d276461bb38287fb5cf57b
TomasVotruba <tomas.vot@gmail.com>
2020-08-19 16:13:41 +0200

    fix exception message

---
+++ config/set/clean-code.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+use PhpCsFixer\Fixer\ArrayNotation\ArraySyntaxFixer;
+use PhpCsFixer\Fixer\ClassNotation\ProtectedToPrivateFixer;
+use PhpCsFixer\Fixer\ControlStructure\NoUnneededControlParenthesesFixer;
+use PhpCsFixer\Fixer\ControlStructure\NoUnneededCurlyBracesFixer;
+use PhpCsFixer\Fixer\Import\NoUnusedImportsFixer;
+use PhpCsFixer\Fixer\Import\OrderedImportsFixer;
+use PhpCsFixer\Fixer\ReturnNotation\ReturnAssignmentFixer;
+use PhpCsFixer\Fixer\Semicolon\NoEmptyStatementFixer;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symplify\CodingStandard\Fixer\Commenting\ParamReturnAndVarTagMalformsFixer;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    $services = $containerConfigurator->services();
+
+    $services->set(ParamReturnAndVarTagMalformsFixer::class);
+
+    $services->set(ArraySyntaxFixer::class)
+        ->call('configure', [['syntax' => 'short']]);
+
+    $services->set(NoUnusedImportsFixer::class);
+
+    $services->set(OrderedImportsFixer::class);
+
+    $services->set(NoEmptyStatementFixer::class);
+
+    $services->set(ProtectedToPrivateFixer::class);
+
+    $services->set(NoUnneededControlParenthesesFixer::class);
+
+    $services->set(NoUnneededCurlyBracesFixer::class);
+
+    $services->set(ReturnAssignmentFixer::class);
+};
