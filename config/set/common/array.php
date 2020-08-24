ed735caf1f58025465bb1661a9c92a05c80a567a
Tomas Votruba <tomas.vot@gmail.com>
2020-08-24 15:39:46 +0200

    [CS] Improve array list indents (#2093)
    * [CS] Drop over-detailed rule registration from READMEs
    
    * remove unused classes
    
    * [CS] Add ArrayOpenerNewlineFixer
    
    * [rector] [CS] Add ArrayOpenerNewlineFixer
    
    Co-authored-by: rector-bot <tomas@getrector.org>

--- config/set/common/array.php
+++ config/set/common/array.php
@@ -10,22 +10,24 @@ use PhpCsFixer\Fixer\ArrayNotation\TrimArraySpacesFixer;
 use PhpCsFixer\Fixer\ArrayNotation\WhitespaceAfterCommaInArrayFixer;
 use PhpCsFixer\Fixer\Whitespace\ArrayIndentationFixer;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symplify\CodingStandard\Fixer\ArrayNotation\ArrayOpenerNewlineFixer;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $services = $containerConfigurator->services();
 
+    // array spacing
     $services->set(NoWhitespaceBeforeCommaInArrayFixer::class);
-
+    $services->set(ArrayOpenerNewlineFixer::class);
+    //$services->set(ArrayIndentationFixer::class);
     $services->set(TrimArraySpacesFixer::class);
+    $services->set(WhitespaceAfterCommaInArrayFixer::class);
 
+    // commas
     $services->set(TrailingCommaInMultilineArrayFixer::class);
-
     $services->set(NoTrailingCommaInSinglelineArrayFixer::class);
 
     $services->set(ArraySyntaxFixer::class)
-        ->call('configure', [['syntax' => 'short']]);
-
-    $services->set(WhitespaceAfterCommaInArrayFixer::class);
-
-    $services->set(ArrayIndentationFixer::class);
+        ->call('configure', [[
+            'syntax' => 'short',
+        ]]);
 };
