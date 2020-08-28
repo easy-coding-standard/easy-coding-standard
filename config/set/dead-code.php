3345858a822db2b60dec6cab6d6d4772c555fa8e
Tomas Votruba <tomas.vot@gmail.com>
2020-08-28 02:00:54 +0200

    restore slevomat, finally working with new phpdoc-parser (#2101)

--- config/set/dead-code.php
+++ config/set/dead-code.php
@@ -2,8 +2,25 @@
 
 declare(strict_types=1);
 
+use SlevomatCodingStandard\Sniffs\Classes\UnusedPrivateElementsSniff;
+use SlevomatCodingStandard\Sniffs\Functions\UnusedInheritedVariablePassedToClosureSniff;
+use SlevomatCodingStandard\Sniffs\Functions\UnusedParameterSniff;
+use SlevomatCodingStandard\Sniffs\Variables\UnusedVariableSniff;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symplify\EasyCodingStandard\Configuration\Option;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
-    // deprecated, better use much advanced Rector dead-code set
+    $services = $containerConfigurator->services();
+
+    $services->set(UnusedPrivateElementsSniff::class);
+
+    $services->set(UnusedInheritedVariablePassedToClosureSniff::class);
+
+    $services->set(UnusedParameterSniff::class);
+
+    $services->set(UnusedVariableSniff::class);
+
+    $parameters = $containerConfigurator->parameters();
+
+    $parameters->set(Option::SKIP, [UnusedParameterSniff::class . '.UnusedParameter' => null]);
 };
