5d30e5e1f50038ff962d8446b8074798ed476f77
Tomas Votruba <tomas.vot@gmail.com>
2020-08-31 19:02:22 +0200

    Allow PHP 8.0 (#2106)

---
+++ src/ValueObject/Set/SetList.php
@@ -0,0 +1,119 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\EasyCodingStandard\ValueObject\Set;
+
+/**
+ * @todo refactor to rull paths
+ */
+final class SetList
+{
+    /**
+     * @var string
+     * @api
+     */
+    public const PSR_12 = __DIR__ . '/../../../config/set/psr12.php';
+
+    /**
+     * @var string
+     * @api
+     */
+    public const PHP_70 = __DIR__ . '/../../../config/set/php70.php';
+
+    /**
+     * @var string
+     * @api
+     */
+    public const PHP_71 = __DIR__ . '/../../../config/set/php71.php';
+
+    /**
+     * @var string
+     * @api
+     */
+    public const CLEAN_CODE = __DIR__ . '/../../../config/set/clean-code.php';
+
+    /**
+     * @var string
+     * @api
+     */
+    public const SYMPLIFY = __DIR__ . '/../../../config/set/symplify.php';
+
+    /**
+     * @var string
+     * @api
+     */
+    public const ARRAY = __DIR__ . '/../../../config/set/array.php';
+
+    /**
+     * @var string
+     * @api
+     */
+    public const COMMON = __DIR__ . '/../../../config/set/common.php';
+
+    /**
+     * @var string
+     * @api
+     */
+    public const COMMENTS = __DIR__ . '/../../../config/set/common/comments.php';
+
+    /**
+     * @var string
+     * @api
+     */
+    public const CONTROL_STRUCTURES = __DIR__ . '/../../../config/set/common/control-structures.php';
+
+    /**
+     * @var string
+     * @api
+     */
+    public const DOCBLOCK = __DIR__ . '/../../../config/set/common/docblock.php';
+
+    /**
+     * @var string
+     * @api
+     */
+    public const NAMESPACES = __DIR__ . '/../../../config/set/common/namespaces.php';
+
+    /**
+     * @var string
+     * @api
+     */
+    public const PHPUNIT = __DIR__ . '/../../../config/set/phpunit.php';
+
+    /**
+     * @var string
+     * @api
+     */
+    public const SPACES = __DIR__ . '/../../../config/set/common/spaces.php';
+
+    /**
+     * @var string
+     * @api
+     */
+    public const STRICT = __DIR__ . '/../../../config/set/common/strict.php';
+
+    /**
+     * @var string
+     * @api
+     */
+    public const DEAD_CODE = __DIR__ . '/../../../config/set/dead-code.php';
+
+    /**
+     * @var string
+     * @api
+     */
+    public const SYMFONY = __DIR__ . '/../../../config/set/symfony.php';
+
+    /**
+     * @var string
+     * @api
+     */
+    public const SYMFONY_RISKY = __DIR__ . '/../../../config/set/symfony-risky.php';
+
+    /**
+     * @var string
+     * @api
+     */
+    public const DOCTRINE_ANNOTATIONS = __DIR__ . '/../../../config/set/doctrine-annotations.php';
+}
---
+++ tests/Application/ApplicationTest.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\EasyCodingStandard\Tests\Application;
+
+use Symplify\EasyCodingStandard\Application\EasyCodingStandardApplication;
+use Symplify\EasyCodingStandard\HttpKernel\EasyCodingStandardKernel;
+use Symplify\PackageBuilder\Tests\AbstractKernelTestCase;
+
+final class ApplicationTest extends AbstractKernelTestCase
+{
+    /**
+     * @var EasyCodingStandardApplication
+     */
+    private $easyCodingStandardApplication;
+
+    protected function setUp(): void
+    {
+        $this->bootKernel(EasyCodingStandardKernel::class);
+
+        $this->easyCodingStandardApplication = self::$container->get(EasyCodingStandardApplication::class);
+    }
+
+    /**
+     * @doesNotPerformAssertions
+     */
+    public function testRun(): void
+    {
+        $this->easyCodingStandardApplication->run();
+    }
+}
