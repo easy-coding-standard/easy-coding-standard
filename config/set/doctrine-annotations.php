04b45fe813af2d36478e33b8103b16ef2cad8797
TomasVotruba <tomas.vot@gmail.com>
2020-09-06 21:31:59 +0200

    [ECS] Restore original doctrine-annotations set

--- config/set/doctrine-annotations.php
+++ config/set/doctrine-annotations.php
@@ -1,8 +1,8 @@
 <?php
+
 declare(strict_types=1);
 
 use PhpCsFixer\Fixer\DoctrineAnnotation\DoctrineAnnotationArrayAssignmentFixer;
-use PhpCsFixer\Fixer\DoctrineAnnotation\DoctrineAnnotationBracesFixer;
 use PhpCsFixer\Fixer\DoctrineAnnotation\DoctrineAnnotationIndentationFixer;
 use PhpCsFixer\Fixer\DoctrineAnnotation\DoctrineAnnotationSpacesFixer;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
@@ -10,11 +10,19 @@ use Symplify\CodingStandard\Fixer\Annotation\DoctrineAnnotationNewlineInNestedAn
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $services = $containerConfigurator->services();
-    $services->set(DoctrineAnnotationArrayAssignmentFixer::class)
-        ->call('configure', [['operator' => ':']]);
-    $services->set(DoctrineAnnotationBracesFixer::class);
-    $services->set(DoctrineAnnotationIndentationFixer::class);
+
     $services->set(DoctrineAnnotationNewlineInNestedAnnotationFixer::class);
+
+    $services->set(DoctrineAnnotationIndentationFixer::class)
+        ->call('configure', [[
+            'indent_mixed_lines' => true,
+        ]]);
+
     $services->set(DoctrineAnnotationSpacesFixer::class)
-        ->call('configure', [['before_array_assignments_colon' => false]]);
+        ->call('configure', [[
+            'after_array_assignments_equals' => false,
+            'before_array_assignments_equals' => false,
+        ]]);
+
+    $services->set(DoctrineAnnotationArrayAssignmentFixer::class);
 };
