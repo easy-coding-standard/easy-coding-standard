15a127a1ec1aceede90932ce0b1b7849394d69b7
Clemens Krack <info@clemenskrack.com>
2020-09-06 19:09:27 +0200

    [ECS] Align rule-sets with PHP-CS-Fixer sets (#2125)
    * [Coding Standard] Align Doctrine Annotations set with php-cs-fixer-set
    
    * [Coding Standard] Align symfony and symfony:risky set with php-cs-fixer
    
    * [Coding Standard] Add missing php-cs-fixer sets
    
    * [ECS] Align Doctrine Annotations set with php-cs-fixer-set
    
    * [ECS] Align symfony and symfony:risky set with php-cs-fixer
    
    * [ECS] Add missing php-cs-fixer sets
    
    * [ECS] Fix indents
    
    Co-authored-by: Tomas Votruba <tomas.vot@gmail.com>

--- config/set/php-cs-fixer-risky.php
+++ config/set/php-cs-fixer-risky.php
@@ -7,27 +7,41 @@ use PhpCsFixer\Fixer\Alias\SetTypeToCastFixer;
 use PhpCsFixer\Fixer\Basic\NonPrintableCharacterFixer;
 use PhpCsFixer\Fixer\Basic\Psr4Fixer;
 use PhpCsFixer\Fixer\CastNotation\ModernizeTypesCastingFixer;
+use PhpCsFixer\Fixer\ClassNotation\FinalInternalClassFixer;
 use PhpCsFixer\Fixer\ClassNotation\NoUnneededFinalMethodFixer;
 use PhpCsFixer\Fixer\ClassNotation\SelfAccessorFixer;
+use PhpCsFixer\Fixer\Comment\CommentToPhpdocFixer;
 use PhpCsFixer\Fixer\ConstantNotation\NativeConstantInvocationFixer;
 use PhpCsFixer\Fixer\FunctionNotation\FopenFlagOrderFixer;
 use PhpCsFixer\Fixer\FunctionNotation\FopenFlagsFixer;
 use PhpCsFixer\Fixer\FunctionNotation\ImplodeCallFixer;
 use PhpCsFixer\Fixer\FunctionNotation\NativeFunctionInvocationFixer;
+use PhpCsFixer\Fixer\FunctionNotation\NoUnreachableDefaultArgumentValueFixer;
 use PhpCsFixer\Fixer\LanguageConstruct\DirConstantFixer;
 use PhpCsFixer\Fixer\LanguageConstruct\ErrorSuppressionFixer;
 use PhpCsFixer\Fixer\LanguageConstruct\FunctionToConstantFixer;
 use PhpCsFixer\Fixer\LanguageConstruct\IsNullFixer;
+use PhpCsFixer\Fixer\LanguageConstruct\NoUnsetOnPropertyFixer;
 use PhpCsFixer\Fixer\Naming\NoHomoglyphNamesFixer;
+use PhpCsFixer\Fixer\Operator\LogicalOperatorsFixer;
 use PhpCsFixer\Fixer\PhpUnit\PhpUnitConstructFixer;
 use PhpCsFixer\Fixer\PhpUnit\PhpUnitMockShortWillReturnFixer;
+use PhpCsFixer\Fixer\PhpUnit\PhpUnitSetUpTearDownVisibilityFixer;
+use PhpCsFixer\Fixer\PhpUnit\PhpUnitStrictFixer;
+use PhpCsFixer\Fixer\PhpUnit\PhpUnitTestAnnotationFixer;
+use PhpCsFixer\Fixer\PhpUnit\PhpUnitTestCaseStaticMethodCallsFixer;
+use PhpCsFixer\Fixer\Strict\StrictComparisonFixer;
+use PhpCsFixer\Fixer\Strict\StrictParamFixer;
+use PhpCsFixer\Fixer\StringNotation\StringLineEndingFixer;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $services = $containerConfigurator->services();
+    $services->set(CommentToPhpdocFixer::class);
     $services->set(DirConstantFixer::class);
     $services->set(EregToPregFixer::class);
     $services->set(ErrorSuppressionFixer::class);
+    $services->set(FinalInternalClassFixer::class);
     $services->set(FopenFlagOrderFixer::class);
     $services->set(FopenFlagsFixer::class)
         ->call('configure', [['b_mode' => false]]);
@@ -37,6 +51,7 @@ return static function (ContainerConfigurator $containerConfigurator): void {
         ]]);
     $services->set(ImplodeCallFixer::class);
     $services->set(IsNullFixer::class);
+    $services->set(LogicalOperatorsFixer::class);
     $services->set(ModernizeTypesCastingFixer::class);
     $services->set(NativeConstantInvocationFixer::class)
         ->call('configure', [[
@@ -46,17 +61,26 @@ return static function (ContainerConfigurator $containerConfigurator): void {
         ]]);
     $services->set(NativeFunctionInvocationFixer::class)
         ->call('configure', [[
-            'include' => [NativeFunctionInvocationFixer::SET_COMPILER_OPTIMIZED],
+            'include' => ['@compiler_optimized'],
             'scope' => 'namespaced',
             'strict' => true,
         ]]);
     $services->set(NoAliasFunctionsFixer::class);
     $services->set(NoHomoglyphNamesFixer::class);
     $services->set(NoUnneededFinalMethodFixer::class);
+    $services->set(NoUnreachableDefaultArgumentValueFixer::class);
+    $services->set(NoUnsetOnPropertyFixer::class);
     $services->set(NonPrintableCharacterFixer::class);
     $services->set(PhpUnitConstructFixer::class);
     $services->set(PhpUnitMockShortWillReturnFixer::class);
+    $services->set(PhpUnitSetUpTearDownVisibilityFixer::class);
+    $services->set(PhpUnitStrictFixer::class);
+    $services->set(PhpUnitTestAnnotationFixer::class);
+    $services->set(PhpUnitTestCaseStaticMethodCallsFixer::class);
     $services->set(Psr4Fixer::class);
     $services->set(SelfAccessorFixer::class);
     $services->set(SetTypeToCastFixer::class);
+    $services->set(StrictComparisonFixer::class);
+    $services->set(StrictParamFixer::class);
+    $services->set(StringLineEndingFixer::class);
 };
