a6443ddf9065ef141d52804092dc8d5c9fe13856
Abdul Malik Ikhsan <samsonasik@gmail.com>
2020-09-07 20:41:07 +0700

    Add HeredocNowdocPHPCodeFormatter to format php code inside heredoc (#2137)
    * add check-heredoc command
    
    * use PHP_CODE_SNIPPET_IN_HEREDOC constant
    
    * remove <?php prefix on inside heredoc generation
    
    * cs fix
    
    * check is_dir() for source
    
    * using HeredocPHPCodeFormatter
    
    * cs fix
    
    * allow nowdoc and heredoc
    
    * support both heredoc and nowdoc
    
    * support both heredoc and nowdoc
    
    * add left right check quote
    
    * regex uri
    
    * rename to HeredocNowdocPHPCodeFormatter
    
    * phpstan
    
    * rector
    
    * loop files inside directory
    
    * implemented
    
    * fix
    
    * handle with Throwable if parsing error inside heredoc/nowdoc
    
    * handle with Throwable if any heredoc/nowdoc got parser error
    
    * rector
    
    * ensure clean up php-code-* files that undeleted yet because of parse error inside heredoc/docblock
    
    * phpstan
    
    * update regex to allow non PHP open here/now doc
    
    * update regex link
    
    * more fixtures
    
    * add AbstractPHPFormatter
    
    * phpstan
    
    * ensure clean up php-code-* files that undeleted yet because of parse error in CheckMarkdownCommand as well
    
    * add missing var string
    
    * clean up
    
    * rector and phpstan
    
    * move delete file handle parse error to AbstractFormatter
    
    * add comment Skipped parsed error when processing php file

--- src/Markdown/MarkdownPHPCodeFormatter.php
+++ src/Markdown/MarkdownPHPCodeFormatter.php
@@ -4,94 +4,16 @@ declare(strict_types=1);
 
 namespace Symplify\EasyCodingStandard\Markdown;
 
-use Nette\Utils\Strings;
-use Symplify\EasyCodingStandard\Configuration\Configuration;
-use Symplify\EasyCodingStandard\FixerRunner\Application\FixerFileProcessor;
-use Symplify\EasyCodingStandard\SniffRunner\Application\SniffFileProcessor;
-use Symplify\SmartFileSystem\SmartFileInfo;
-use Symplify\SmartFileSystem\SmartFileSystem;
+use Symplify\EasyCodingStandard\Formatter\AbstractPHPFormatter;
 
 /**
  * @see \Symplify\EasyCodingStandard\Tests\Markdown\MarkdownPHPCodeFormatterTest
  */
-final class MarkdownPHPCodeFormatter
+final class MarkdownPHPCodeFormatter extends AbstractPHPFormatter
 {
     /**
      * @see https://regex101.com/r/4YUIu1/2
      * @var string
      */
-    private const PHP_CODE_SNIPPET_IN_MARKDOWN = '#(?<opening>\`\`\`php\s+)(?<content>[^\`\`\`]+\n)(?<closing>(\s+)?\`\`\`)#ms';
-
-    /**
-     * @var SmartFileSystem
-     */
-    private $smartFileSystem;
-
-    /**
-     * @var FixerFileProcessor
-     */
-    private $fixerFileProcessor;
-
-    /**
-     * @var SniffFileProcessor
-     */
-    private $sniffFileProcessor;
-
-    /**
-     * @var Configuration
-     */
-    private $configuration;
-
-    public function __construct(
-        SmartFileSystem $smartFileSystem,
-        FixerFileProcessor $fixerFileProcessor,
-        SniffFileProcessor $sniffFileProcessor,
-        Configuration $configuration
-    ) {
-        $this->smartFileSystem = $smartFileSystem;
-        $this->fixerFileProcessor = $fixerFileProcessor;
-        $this->sniffFileProcessor = $sniffFileProcessor;
-        $this->configuration = $configuration;
-    }
-
-    public function format(SmartFileInfo $fileInfo): string
-    {
-        // enable fixing
-        $this->configuration->resolveFromArray(['isFixer' => true]);
-
-        return (string) Strings::replace(
-            $fileInfo->getContents(),
-            self::PHP_CODE_SNIPPET_IN_MARKDOWN,
-            function ($match): string {
-                $fixedContent = $this->fixContent($match['content']);
-                return $match['opening'] . $fixedContent . $match['closing'];
-            }
-        );
-    }
-
-    private function fixContent(string $content): string
-    {
-        $key = md5($content);
-
-        /** @var string $file */
-        $file = sprintf('php-code-%s.php', $key);
-
-        if (! Strings::startsWith($content, '<?php')) {
-            $content = '<?php' . PHP_EOL . $content;
-        }
-
-        $fileContent = $content;
-
-        $this->smartFileSystem->dumpFile($file, $fileContent);
-
-        $fileInfo = new SmartFileInfo($file);
-        $this->fixerFileProcessor->processFile($fileInfo);
-        $this->sniffFileProcessor->processFile($fileInfo);
-
-        $fileContent = $fileInfo->getContents();
-
-        $this->smartFileSystem->remove($file);
-
-        return $fileContent;
-    }
+    protected const PHP_CODE_SNIPPET = '#(?<opening>\`\`\`php\s+)(?<content>[^\`\`\`]+\n)(?<closing>(\s+)?\`\`\`)#ms';
 }
