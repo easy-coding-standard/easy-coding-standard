a682e298f83bdde3ab2ba378d93869291ed8e9dc
Abdul Malik Ikhsan <samsonasik@gmail.com>
2020-09-09 21:27:22 +0700

    Add --fix option to formatter markdown/heredoc-nowdoc command (#2143)
    * adding --fix option to formatter markdown/heredoc-nowdoc command
    
    * pass fix option to formatter
    
    * enable fix if $fix is truthy
    
    * run dumpFile() when has --fix
    
    * revert diff
    
    * d
    
    * re-add back Option::FIX to CheckMarkdownCommand
    
    * apply for markdown
    
    * add ability to specify customFile definition in ConsoleOutputFormatter
    
    * ??
    
    * cs fix
    
    * add setCustomFileName() function
    
    * reduce complexity
    
    * rector
    
    * phpstan
    
    * adding --fix to CheckHeredocNowdoc command
    
    * adding multiple customFileNames
    
    * remove clone
    
    * cs
    
    * phpstan
    
    * reduce complexity
    
    * array
    
    * phpstan
    
    * move $alreadyFollowCodingStandard = false; flag before fix check
    
    * file diff is not file name
    
    * fix header file
    
    * add disableHeaderFileDiff for single file markdown check
    
    * cs
    
    * keep old behaviour show boldNumberedMessage with empty custom files
    
    * fix
    
    * reduce complexity
    
    * phpstan
    
    * clean up

--- src/Console/Command/CheckHeredocNowdocCommand.php
+++ src/Console/Command/CheckHeredocNowdocCommand.php
@@ -7,12 +7,17 @@ namespace Symplify\EasyCodingStandard\Console\Command;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Finder\Finder;
+use Symplify\EasyCodingStandard\Configuration\Configuration;
 use Symplify\EasyCodingStandard\Configuration\Exception\NoDirectoryException;
 use Symplify\EasyCodingStandard\Configuration\Exception\NoPHPFileException;
+use Symplify\EasyCodingStandard\Console\Output\ConsoleOutputFormatter;
+use Symplify\EasyCodingStandard\Console\Output\OutputFormatterCollector;
 use Symplify\EasyCodingStandard\Console\Style\EasyCodingStandardStyle;
 use Symplify\EasyCodingStandard\HeredocNowdoc\HeredocNowdocPHPCodeFormatter;
+use Symplify\EasyCodingStandard\ValueObject\Option;
 use Symplify\PackageBuilder\Console\Command\CommandNaming;
 use Symplify\PackageBuilder\Console\ShellCode;
 use Symplify\SmartFileSystem\SmartFileInfo;
@@ -31,6 +36,11 @@ final class CheckHeredocNowdocCommand extends Command
     private const NO_STRICT_TYPES_DECLARATION = 'no-strict-types-declaration';
 
     /**
+     * @var Configuration
+     */
+    protected $configuration;
+
+    /**
      * @var SmartFileSystem
      */
     private $smartFileSystem;
@@ -45,17 +55,25 @@ final class CheckHeredocNowdocCommand extends Command
      */
     private $heredocnowdocPHPCodeFormatter;
 
+    /**
+     * @var OutputFormatterCollector
+     */
+    private $outputFormatterCollector;
+
     public function __construct(
         SmartFileSystem $smartFileSystem,
         EasyCodingStandardStyle $easyCodingStandardStyle,
-        HeredocNowdocPHPCodeFormatter $heredocnowdocPHPCodeFormatter
+        HeredocNowdocPHPCodeFormatter $heredocnowdocPHPCodeFormatter,
+        OutputFormatterCollector $outputFormatterCollector,
+        Configuration $configuration
     ) {
         $this->smartFileSystem = $smartFileSystem;
         $this->easyCodingStandardStyle = $easyCodingStandardStyle;
+        $this->heredocnowdocPHPCodeFormatter = $heredocnowdocPHPCodeFormatter;
+        $this->outputFormatterCollector = $outputFormatterCollector;
+        $this->configuration = $configuration;
 
         parent::__construct();
-
-        $this->heredocnowdocPHPCodeFormatter = $heredocnowdocPHPCodeFormatter;
     }
 
     protected function configure(): void
@@ -68,37 +86,58 @@ final class CheckHeredocNowdocCommand extends Command
             'Path to the directory containing PHP Code with Heredoc/Nowdoc inside'
         );
         $this->addOption(self::NO_STRICT_TYPES_DECLARATION, null, null, 'No strict types declaration');
+        $this->addOption(Option::FIX, null, null, 'Fix found violations.');
+        $this->addOption(
+            Option::OUTPUT_FORMAT,
+            null,
+            InputOption::VALUE_REQUIRED,
+            'Select output format',
+            ConsoleOutputFormatter::NAME
+        );
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         /** @var string $heredocNowDocDirectory */
         $heredocNowDocDirectory = $input->getArgument(self::SOURCE);
-        if (! is_dir($heredocNowDocDirectory)) {
-            $message = sprintf('Directory "%s" not found', $heredocNowDocDirectory);
-            throw new NoDirectoryException($message);
-        }
+        $this->verifyDirectory($heredocNowDocDirectory);
 
         $finder = new Finder();
-        $finder->files()->in($heredocNowDocDirectory)->name('*.php');
-
-        if (! $finder->hasResults()) {
-            $message = sprintf('No file in "%s"', $heredocNowDocDirectory);
-            throw new NoPHPFileException($message);
-        }
+        $this->verifyHasFiles($finder, $heredocNowDocDirectory);
 
         $noStrictTypesDeclaration = (bool) $input->getOption(self::NO_STRICT_TYPES_DECLARATION);
+        $fix = (bool) $input->getOption(Option::FIX);
         $alreadyFollowCodingStandard = true;
+
+        $countFixable = 0;
+        $outputFormatter = null;
         foreach ($finder as $file) {
             $absoluteFilePath = $file->getRealPath();
 
             $phpFileInfo = new SmartFileInfo($absoluteFilePath);
             $fixedContent = $this->heredocnowdocPHPCodeFormatter->format($phpFileInfo, $noStrictTypesDeclaration);
 
-            if ($phpFileInfo->getContents() !== $fixedContent) {
-                $this->smartFileSystem->dumpFile($absoluteFilePath, (string) $fixedContent);
-                $alreadyFollowCodingStandard = false;
+            if ($phpFileInfo->getContents() === $fixedContent) {
+                continue;
             }
+
+            $alreadyFollowCodingStandard = false;
+            if ($fix) {
+                $this->smartFileSystem->dumpFile($absoluteFilePath, (string) $fixedContent);
+                continue;
+            }
+
+            $this->configuration->resolveFromArray(['isFixer' => false]);
+            $outputFormat = $this->resolveOutputFormat($input);
+
+            /** @var ConsoleOutputFormatter $outputFormatter */
+            $outputFormatter = $this->outputFormatterCollector->getByName($outputFormat);
+            $outputFormatter->addCustomFileName($absoluteFilePath);
+            $countFixable++;
+        }
+
+        if ($outputFormatter && $countFixable > 0) {
+            return $outputFormatter->report($countFixable);
         }
 
         if ($alreadyFollowCodingStandard) {
@@ -110,5 +149,35 @@ final class CheckHeredocNowdocCommand extends Command
         $this->easyCodingStandardStyle->success($successMessage);
 
         return ShellCode::SUCCESS;
+    }
+
+    private function verifyDirectory(string $heredocNowDocDirectory): void
+    {
+        if (! is_dir($heredocNowDocDirectory)) {
+            $message = sprintf('Directory "%s" not found', $heredocNowDocDirectory);
+            throw new NoDirectoryException($message);
+        }
+    }
+
+    private function verifyHasFiles(Finder $finder, string $heredocNowDocDirectory): void
+    {
+        $finder->files()->in($heredocNowDocDirectory)->name('*.php');
+
+        if (! $finder->hasResults()) {
+            $message = sprintf('No file in "%s"', $heredocNowDocDirectory);
+            throw new NoPHPFileException($message);
+        }
+    }
+
+    private function resolveOutputFormat(InputInterface $input): string
+    {
+        $outputFormat = (string) $input->getOption(Option::OUTPUT_FORMAT);
+
+        // Backwards compatibility with older version
+        if ($outputFormat === 'table') {
+            return ConsoleOutputFormatter::NAME;
+        }
+
+        return $outputFormat;
     }
 }
--- src/Console/Command/CheckMarkdownCommand.php
+++ src/Console/Command/CheckMarkdownCommand.php
@@ -7,10 +7,15 @@ namespace Symplify\EasyCodingStandard\Console\Command;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symplify\EasyCodingStandard\Configuration\Configuration;
 use Symplify\EasyCodingStandard\Configuration\Exception\NoMarkdownFileException;
+use Symplify\EasyCodingStandard\Console\Output\ConsoleOutputFormatter;
+use Symplify\EasyCodingStandard\Console\Output\OutputFormatterCollector;
 use Symplify\EasyCodingStandard\Console\Style\EasyCodingStandardStyle;
 use Symplify\EasyCodingStandard\Markdown\MarkdownPHPCodeFormatter;
+use Symplify\EasyCodingStandard\ValueObject\Option;
 use Symplify\PackageBuilder\Console\Command\CommandNaming;
 use Symplify\PackageBuilder\Console\ShellCode;
 use Symplify\SmartFileSystem\SmartFileInfo;
@@ -29,6 +34,11 @@ final class CheckMarkdownCommand extends Command
     private const NO_STRICT_TYPES_DECLARATION = 'no-strict-types-declaration';
 
     /**
+     * @var Configuration
+     */
+    protected $configuration;
+
+    /**
      * @var SmartFileSystem
      */
     private $smartFileSystem;
@@ -43,17 +53,25 @@ final class CheckMarkdownCommand extends Command
      */
     private $markdownPHPCodeFormatter;
 
+    /**
+     * @var OutputFormatterCollector
+     */
+    private $outputFormatterCollector;
+
     public function __construct(
         SmartFileSystem $smartFileSystem,
         EasyCodingStandardStyle $easyCodingStandardStyle,
-        MarkdownPHPCodeFormatter $markdownPHPCodeFormatter
+        MarkdownPHPCodeFormatter $markdownPHPCodeFormatter,
+        OutputFormatterCollector $outputFormatterCollector,
+        Configuration $configuration
     ) {
         $this->smartFileSystem = $smartFileSystem;
         $this->easyCodingStandardStyle = $easyCodingStandardStyle;
+        $this->markdownPHPCodeFormatter = $markdownPHPCodeFormatter;
+        $this->outputFormatterCollector = $outputFormatterCollector;
+        $this->configuration = $configuration;
 
         parent::__construct();
-
-        $this->markdownPHPCodeFormatter = $markdownPHPCodeFormatter;
     }
 
     protected function configure(): void
@@ -61,6 +79,15 @@ final class CheckMarkdownCommand extends Command
         $this->setName(CommandNaming::classToName(self::class));
         $this->setDescription('Format Markdown PHP code');
         $this->addArgument(self::SOURCE, InputArgument::REQUIRED, 'Path to the Markdown file');
+        $this->addOption(self::NO_STRICT_TYPES_DECLARATION, null, null, 'No strict types declaration');
+        $this->addOption(Option::FIX, null, null, 'Fix found violations.');
+        $this->addOption(
+            Option::OUTPUT_FORMAT,
+            null,
+            InputOption::VALUE_REQUIRED,
+            'Select output format',
+            ConsoleOutputFormatter::NAME
+        );
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
@@ -73,18 +100,40 @@ final class CheckMarkdownCommand extends Command
         }
 
         $noStrictTypesDeclaration = (bool) $input->getOption(self::NO_STRICT_TYPES_DECLARATION);
+        $fix = (bool) $input->getOption(Option::FIX);
         $markdownFileInfo = new SmartFileInfo($markdownFile);
         $fixedContent = $this->markdownPHPCodeFormatter->format($markdownFileInfo, $noStrictTypesDeclaration);
 
         if ($markdownFileInfo->getContents() === $fixedContent) {
             $successMessage = 'PHP code in Markdown already follow coding standard';
-        } else {
+        } elseif ($fix) {
             $this->smartFileSystem->dumpFile($markdownFile, (string) $fixedContent);
             $successMessage = 'PHP code in Markdown has been fixed to follow coding standard';
+        } else {
+            $this->configuration->resolveFromArray(['isFixer' => false]);
+
+            $outputFormat = $this->resolveOutputFormat($input);
+            /** @var ConsoleOutputFormatter $outputFormatter */
+            $outputFormatter = $this->outputFormatterCollector->getByName($outputFormat);
+            $outputFormatter->disableHeaderFileDiff();
+
+            return $outputFormatter->report(1);
         }
 
         $this->easyCodingStandardStyle->success($successMessage);
 
         return ShellCode::SUCCESS;
+    }
+
+    private function resolveOutputFormat(InputInterface $input): string
+    {
+        $outputFormat = (string) $input->getOption(Option::OUTPUT_FORMAT);
+
+        // Backwards compatibility with older version
+        if ($outputFormat === 'table') {
+            return ConsoleOutputFormatter::NAME;
+        }
+
+        return $outputFormat;
     }
 }
