4e0629cc280ec0b8a14b2d55ba86a7eed98fb00f
Tomas Votruba <tomas.vot@gmail.com>
2020-09-14 22:55:30 +0200

    [ECS] Decouple sub-package SnippetFormatter (#2174)
    * [ECS] Decouple sub-package SnippetFormatter
    
    * [ECS] Refactor *SnippetFormatters to single SnippetFormatter
    
    * [ECS] Decouple AbstractSnippetFormatterCommand
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- packages/snippet-formatter/src/Formatter/SnippetFormatter.php
+++ packages/snippet-formatter/src/Formatter/SnippetFormatter.php
@@ -2,39 +2,36 @@
 
 declare(strict_types=1);
 
-namespace Symplify\EasyCodingStandard\Formatter;
+namespace Symplify\EasyCodingStandard\SnippetFormatter\Formatter;
 
 use Nette\Utils\Strings;
-use Symplify\EasyCodingStandard\Configuration\Configuration;
-use Symplify\EasyCodingStandard\Contract\RegexAwareFormatterInterface;
 use Symplify\EasyCodingStandard\FixerRunner\Application\FixerFileProcessor;
-use Symplify\EasyCodingStandard\Provider\CurrentParentFileInfoProvider;
 use Symplify\EasyCodingStandard\SniffRunner\Application\SniffFileProcessor;
+use Symplify\EasyCodingStandard\SnippetFormatter\Provider\CurrentParentFileInfoProvider;
 use Symplify\SmartFileSystem\SmartFileInfo;
 use Symplify\SmartFileSystem\SmartFileSystem;
 use Throwable;
 
-abstract class AbstractPHPFormatter implements RegexAwareFormatterInterface
+/**
+ * @see \Symplify\EasyCodingStandard\SnippetFormatter\Tests\Markdown\MarkdownSnippetFormatterTest
+ * @see \Symplify\EasyCodingStandard\SnippetFormatter\Tests\HeredocNowdoc\HereNowDocSnippetFormatterTest
+ */
+final class SnippetFormatter
 {
     /**
      * @var SmartFileSystem
      */
-    protected $smartFileSystem;
+    private $smartFileSystem;
 
     /**
      * @var FixerFileProcessor
      */
-    protected $fixerFileProcessor;
+    private $fixerFileProcessor;
 
     /**
      * @var SniffFileProcessor
      */
-    protected $sniffFileProcessor;
-
-    /**
-     * @var Configuration
-     */
-    protected $configuration;
+    private $sniffFileProcessor;
 
     /**
      * @var CurrentParentFileInfoProvider
@@ -45,21 +42,19 @@ abstract class AbstractPHPFormatter implements RegexAwareFormatterInterface
         SmartFileSystem $smartFileSystem,
         FixerFileProcessor $fixerFileProcessor,
         SniffFileProcessor $sniffFileProcessor,
-        Configuration $configuration,
         CurrentParentFileInfoProvider $currentParentFileInfoProvider
     ) {
         $this->smartFileSystem = $smartFileSystem;
         $this->fixerFileProcessor = $fixerFileProcessor;
         $this->sniffFileProcessor = $sniffFileProcessor;
-        $this->configuration = $configuration;
         $this->currentParentFileInfoProvider = $currentParentFileInfoProvider;
     }
 
-    public function format(SmartFileInfo $fileInfo): string
+    public function format(SmartFileInfo $fileInfo, string $snippetRegex): string
     {
         $this->currentParentFileInfoProvider->setParentFileInfo($fileInfo);
 
-        return (string) Strings::replace($fileInfo->getContents(), $this->provideRegex(), function ($match): string {
+        return (string) Strings::replace($fileInfo->getContents(), $snippetRegex, function ($match): string {
             return $this->fixContentAndPreserveFormatting($match);
         });
     }
@@ -79,8 +74,8 @@ abstract class AbstractPHPFormatter implements RegexAwareFormatterInterface
         $content = trim($content);
         $key = md5($content);
 
-        /** @var string $temporaryFile */
-        $temporaryFile = sys_get_temp_dir() . '/ecs_temp/' . sprintf('php-code-%s.php', $key);
+        /** @var string $temporaryFilePath */
+        $temporaryFilePath = sys_get_temp_dir() . '/ecs_temp/' . sprintf('php-code-%s.php', $key);
 
         $hasPreviouslyOpeningPHPTag = true;
         if (! Strings::startsWith($content, '<?php')) {
@@ -90,8 +85,8 @@ abstract class AbstractPHPFormatter implements RegexAwareFormatterInterface
 
         $fileContent = $content;
 
-        $this->smartFileSystem->dumpFile($temporaryFile, $fileContent);
-        $temporaryFileInfo = new SmartFileInfo($temporaryFile);
+        $this->smartFileSystem->dumpFile($temporaryFilePath, $fileContent);
+        $temporaryFileInfo = new SmartFileInfo($temporaryFilePath);
 
         try {
             $this->fixerFileProcessor->processFile($temporaryFileInfo);
@@ -102,7 +97,7 @@ abstract class AbstractPHPFormatter implements RegexAwareFormatterInterface
             // Skipped parsed error when processing php temporaryFile
         } finally {
             // remove temporary temporaryFile
-            $this->smartFileSystem->remove($temporaryFile);
+            $this->smartFileSystem->remove($temporaryFilePath);
         }
 
         if (! $hasPreviouslyOpeningPHPTag) {
--- packages/snippet-formatter/tests/HeredocNowdoc/HereNowDocSnippetFormatterTest.php
+++ packages/snippet-formatter/tests/HeredocNowdoc/HereNowDocSnippetFormatterTest.php
@@ -2,14 +2,15 @@
 
 declare(strict_types=1);
 
-namespace Symplify\EasyCodingStandard\Tests\Markdown;
+namespace Symplify\EasyCodingStandard\SnippetFormatter\Tests\HeredocNowdoc;
 
 use Iterator;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symplify\EasyCodingStandard\Configuration\Configuration;
 use Symplify\EasyCodingStandard\Console\Style\EasyCodingStandardStyle;
 use Symplify\EasyCodingStandard\HttpKernel\EasyCodingStandardKernel;
-use Symplify\EasyCodingStandard\Markdown\MarkdownPHPCodeFormatter;
+use Symplify\EasyCodingStandard\SnippetFormatter\Formatter\SnippetFormatter;
+use Symplify\EasyCodingStandard\SnippetFormatter\ValueObject\SnippetPattern;
 use Symplify\EasyTesting\DataProvider\StaticFixtureFinder;
 use Symplify\EasyTesting\StaticFixtureSplitter;
 use Symplify\PackageBuilder\Tests\AbstractKernelTestCase;
@@ -18,17 +19,17 @@ use Symplify\SmartFileSystem\SmartFileInfo;
 /**
  * For testing approach @see https://github.com/symplify/easy-testing
  */
-final class MarkdownPHPCodeFormatterTest extends AbstractKernelTestCase
+final class HereNowDocSnippetFormatterTest extends AbstractKernelTestCase
 {
     /**
-     * @var MarkdownPHPCodeFormatter
+     * @var SnippetFormatter
      */
-    private $markdownPHPCodeFormatter;
+    private $snippetFormatter;
 
     protected function setUp(): void
     {
         self::bootKernelWithConfigs(EasyCodingStandardKernel::class, [__DIR__ . '/config/array_fixer.php']);
-        $this->markdownPHPCodeFormatter = self::$container->get(MarkdownPHPCodeFormatter::class);
+        $this->snippetFormatter = self::$container->get(SnippetFormatter::class);
 
         /** @var EasyCodingStandardStyle $easyCodingStandardStyle */
         $easyCodingStandardStyle = self::$container->get(EasyCodingStandardStyle::class);
@@ -49,13 +50,17 @@ final class MarkdownPHPCodeFormatterTest extends AbstractKernelTestCase
             $fixtureFileInfo
         );
 
-        $changedContent = $this->markdownPHPCodeFormatter->format($inputAndExpectedFileInfos->getInputFileInfo());
+        $changedContent = $this->snippetFormatter->format(
+            $inputAndExpectedFileInfos->getInputFileInfo(),
+            SnippetPattern::HERENOWDOC_SNIPPET_PATTERN
+        );
+
         $contents = $inputAndExpectedFileInfos->getExpectedFileInfo()->getContents();
         $this->assertSame($contents, $changedContent, $fixtureFileInfo->getRelativeFilePathFromCwd());
     }
 
     public function provideData(): Iterator
     {
-        return StaticFixtureFinder::yieldDirectory(__DIR__ . '/Fixture', '*.md');
+        return StaticFixtureFinder::yieldDirectory(__DIR__ . '/Fixture', '*.php.inc');
     }
 }
