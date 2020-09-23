942964191f6f69702c3a2e584a2eb10530b3ba86
Abdul Malik Ikhsan <samsonasik@gmail.com>
2020-09-23 18:54:58 +0700

    [ECS] Fixes #2219 : Fixes StandardizeHereNowDocKeywordFixer indentation closing heredoc/nowdoc to not be removed  (#2224)
    * Fixes #2219 : Fixes StandardizeHereNowDocKeywordFixer indentation closing heredoc/nowdoc to not be removed
    
    * cs fix
    
    * fixing rector
    
    * another try, final
    
    * really final touch
    
    * split into 2 tests files

---
+++ packages/snippet-formatter/tests/HeredocNowdoc/Php73Test.php
@@ -0,0 +1,67 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\EasyCodingStandard\SnippetFormatter\Tests\HeredocNowdoc;
+
+use Iterator;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symplify\EasyCodingStandard\Configuration\Configuration;
+use Symplify\EasyCodingStandard\Console\Style\EasyCodingStandardStyle;
+use Symplify\EasyCodingStandard\HttpKernel\EasyCodingStandardKernel;
+use Symplify\EasyCodingStandard\SnippetFormatter\Formatter\SnippetFormatter;
+use Symplify\EasyCodingStandard\SnippetFormatter\ValueObject\SnippetPattern;
+use Symplify\EasyTesting\DataProvider\StaticFixtureFinder;
+use Symplify\EasyTesting\StaticFixtureSplitter;
+use Symplify\PackageBuilder\Tests\AbstractKernelTestCase;
+use Symplify\SmartFileSystem\SmartFileInfo;
+
+/**
+ * @requires PHP >= 7.3
+ * For testing approach @see https://github.com/symplify/easy-testing
+ */
+final class Php73Test extends AbstractKernelTestCase
+{
+    /**
+     * @var SnippetFormatter
+     */
+    private $snippetFormatter;
+
+    protected function setUp(): void
+    {
+        self::bootKernelWithConfigs(EasyCodingStandardKernel::class, [__DIR__ . '/config/array_fixer.php']);
+        $this->snippetFormatter = self::$container->get(SnippetFormatter::class);
+
+        /** @var EasyCodingStandardStyle $easyCodingStandardStyle */
+        $easyCodingStandardStyle = self::$container->get(EasyCodingStandardStyle::class);
+        $easyCodingStandardStyle->setVerbosity(OutputInterface::VERBOSITY_QUIET);
+
+        // enable fixing
+        /** @var Configuration $configuration */
+        $configuration = self::$container->get(Configuration::class);
+        $configuration->enableFixing();
+    }
+
+    /**
+     * @dataProvider provideData()
+     */
+    public function test(SmartFileInfo $fixtureFileInfo): void
+    {
+        $inputAndExpectedFileInfos = StaticFixtureSplitter::splitFileInfoToLocalInputAndExpectedFileInfos(
+            $fixtureFileInfo
+        );
+
+        $changedContent = $this->snippetFormatter->format(
+            $inputAndExpectedFileInfos->getInputFileInfo(),
+            SnippetPattern::HERENOWDOC_SNIPPET_REGEX
+        );
+
+        $contents = $inputAndExpectedFileInfos->getExpectedFileInfo()->getContents();
+        $this->assertSame($contents, $changedContent, $fixtureFileInfo->getRelativeFilePathFromCwd());
+    }
+
+    public function provideData(): Iterator
+    {
+        return StaticFixtureFinder::yieldDirectory(__DIR__ . '/FixturePhp73', '*.php.inc');
+    }
+}
