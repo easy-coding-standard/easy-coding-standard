2bf9d5cae7fd7bbc7abb1dc5bbdf512683c5d7b2
Tomas Votruba <tomas.vot@gmail.com>
2020-10-02 19:59:01 +0200

    various fixes (#2299)
    * static fixes
    
    * [CS] Skip interesection in ContainsTypeAnalyser
    
    * misc
    
    * allow method call in dim fethc
    
    * cleanup ctor static

---
+++ src/Error/ErrorAndDiffResultFactory.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\EasyCodingStandard\Error;
+
+use Symplify\EasyCodingStandard\ValueObject\Error\ErrorAndDiffResult;
+
+final class ErrorAndDiffResultFactory
+{
+    public function create(ErrorAndDiffCollector $errorAndDiffCollector): ErrorAndDiffResult
+    {
+        return new ErrorAndDiffResult($errorAndDiffCollector->getErrors(), $errorAndDiffCollector->getFileDiffs());
+    }
+}
