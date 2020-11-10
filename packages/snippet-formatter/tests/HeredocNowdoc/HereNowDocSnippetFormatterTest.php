4c21426a8e692d2a7c43a7621df8c8c6d71dd241
symplify <tomas@getrector.org>
2020-11-10 13:42:17 +0000

    Update from https://github.com/symplify/symplify/commit/491b8c1083b06d0fe0e407a353f352e23097b186

--- packages/snippet-formatter/tests/HeredocNowdoc/HereNowDocSnippetFormatterTest.php
+++ packages/snippet-formatter/tests/HeredocNowdoc/HereNowDocSnippetFormatterTest.php
@@ -5,9 +5,7 @@ declare(strict_types=1);
 namespace Symplify\EasyCodingStandard\SnippetFormatter\Tests\HeredocNowdoc;
 
 use Iterator;
-use Symfony\Component\Console\Output\OutputInterface;
 use Symplify\EasyCodingStandard\Configuration\Configuration;
-use Symplify\EasyCodingStandard\Console\Style\EasyCodingStandardStyle;
 use Symplify\EasyCodingStandard\HttpKernel\EasyCodingStandardKernel;
 use Symplify\EasyCodingStandard\SnippetFormatter\Formatter\SnippetFormatter;
 use Symplify\EasyCodingStandard\SnippetFormatter\ValueObject\SnippetPattern;
@@ -30,10 +28,6 @@ final class HereNowDocSnippetFormatterTest extends AbstractKernelTestCase
     {
         self::bootKernelWithConfigs(EasyCodingStandardKernel::class, [__DIR__ . '/config/array_fixer.php']);
         $this->snippetFormatter = self::$container->get(SnippetFormatter::class);
-
-        /** @var EasyCodingStandardStyle $easyCodingStandardStyle */
-        $easyCodingStandardStyle = self::$container->get(EasyCodingStandardStyle::class);
-        $easyCodingStandardStyle->setVerbosity(OutputInterface::VERBOSITY_QUIET);
 
         // enable fixing
         /** @var Configuration $configuration */
