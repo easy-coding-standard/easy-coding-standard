1472fcc04498e22c20d7f71f206c3f5e82669190
kaizen-ci <info@kaizen-ci.org>
2020-11-25 14:18:07 +0000

    Package split https://github.com/symplify/symplify/commit/5d71a6e58d62f9ff9ef7b8944e7e977a40006f73

--- packages/sniff-runner/src/ValueObject/File.php
+++ packages/sniff-runner/src/ValueObject/File.php
@@ -288,13 +288,13 @@ final class File extends BaseFile
     {
         $fullyQualifiedCode = $this->resolveFullyQualifiedCode($code);
 
-        if ($this->skipper->shouldSkipCodeAndFile($fullyQualifiedCode, $this->fileInfo)) {
+        if ($this->skipper->shouldSkipElementAndFileInfo($fullyQualifiedCode, $this->fileInfo)) {
             return true;
         }
 
         $message = count($data) > 0 ? vsprintf($error, $data) : $error;
 
-        return $this->skipper->shouldSkipMessageAndFile($message, $this->fileInfo);
+        return $this->skipper->shouldSkipElementAndFileInfo($message, $this->fileInfo);
     }
 
     private function isSniffClassWarningAllowed(string $sniffClass): bool
