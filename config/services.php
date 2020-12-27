c20ba7049fd1ea57ab67b5bd647dc813a9cf3547
kaizen-ci <info@kaizen-ci.org>
2020-12-27 15:37:56 +0000

    do not load bundles in services

--- config/services.php
+++ config/services.php
@@ -31,6 +31,7 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $services->load('Symplify\EasyCodingStandard\\', __DIR__ . '/../src')
         ->exclude([
             // onyl for "bin/ecs" file, where containre does not exist yet
+            __DIR__ . '/../src/Bundle',
             __DIR__ . '/../src/Bootstrap',
             __DIR__ . '/../src/DependencyInjection',
             __DIR__ . '/../src/HttpKernel',
