e2c35e45779261288ce52df4501c7758895f5ae7
kaizen-ci <info@kaizen-ci.org>
2021-01-14 10:33:45 +0000

    typo

--- config/set/dead-code.php
+++ config/set/dead-code.php
@@ -6,7 +6,7 @@ use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigura
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     trigger_error(
-        'ECS set DEAD_CODE from  is deprecated. Use more advanced and precise Rector set https://github.com/rectorphp/rector/blob/master/config/set/dead-code.php instead.'
+        'ECS set DEAD_CODE is deprecated. Use more advanced and precise Rector set https://github.com/rectorphp/rector/blob/master/config/set/dead-code.php instead.'
     );
     sleep(3);
 };
