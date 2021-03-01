3e04e036e73619d6e2683915ea1f465372333bab
kaizen-ci <info@kaizen-ci.org>
2021-03-01 11:50:24 +0000

    Fix symfony polyfills in scoper (#2998)

--- scoper.inc.php
+++ scoper.inc.php
@@ -2,12 +2,28 @@
 
 declare(strict_types=1);
 
+use Isolated\Symfony\Component\Finder\Finder;
+
+$symfonyPolyfillPhpFilesFinder = Finder::create();
+$symfonyPolyfillPhpFilesFinder = $symfonyPolyfillPhpFilesFinder->files();
+$symfonyPolyfillPhpFilesFinder = $symfonyPolyfillPhpFilesFinder->in(__DIR__ . '/vendor/symfony/polyfill-*');
+$symfonyPolyfillPhpFilesFinder = $symfonyPolyfillPhpFilesFinder->name('*.php');
+
+$symfonyPolyfillPhpFilesArray = \iterator_to_array($symfonyPolyfillPhpFilesFinder);
+$symfonyPolyfillPhpFilenames = \array_values($symfonyPolyfillPhpFilesArray);
+$symfonyPolyfillAllowlist = \array_map(
+    static function ($file) {
+        return $file->getPathName();
+    },
+    $symfonyPolyfillPhpFilenames
+);
+
 return [
     'files-whitelist' => [
-        // do not prefix "trigger_deprecatoin" from symfony - https://github.com/symfony/symfony/commit/0032b2a2893d3be592d4312b7b098fb9d71aca03
+        // do not prefix "trigger_deprecation" from symfony - https://github.com/symfony/symfony/commit/0032b2a2893d3be592d4312b7b098fb9d71aca03
         // these paths are relative to this file location, so it should be in the root directory
         'vendor/symfony/deprecation-contracts/function.php',
-    ],
+    ] + $symfonyPolyfillAllowlist,
     'whitelist' => [
         // needed for autoload, that is not prefixed, since it's in bin/* file
         'Symplify\*',
@@ -16,5 +32,6 @@ return [
         'SlevomatCodingStandard\*',
         'Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator',
         'Symfony\Component\DependencyInjection\Extension\ExtensionInterface',
+        'Symfony\Polyfill\*',
     ],
 ];
