d9805370a66165d02fd224d441da2c8730dafaa7
kaizen-ci <info@kaizen-ci.org>
2021-03-17 11:32:35 +0000

    [EasyCodingStandard] Fixes Scoper replace Composer\InstalledVersions prefix on ECS scoper config (#3062)
    Co-authored-by: kaizen-ci <info@kaizen-ci.org>

--- scoper.inc.php
+++ scoper.inc.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+use Nette\Utils\Strings;
 use Isolated\Symfony\Component\Finder\Finder;
 
 $symfonyPolyfillPhpFilesFinder = Finder::create();
@@ -23,7 +24,6 @@ return [
         // do not prefix "trigger_deprecation" from symfony - https://github.com/symfony/symfony/commit/0032b2a2893d3be592d4312b7b098fb9d71aca03
         // these paths are relative to this file location, so it should be in the root directory
         'vendor/symfony/deprecation-contracts/function.php',
-        'vendor/jean85/pretty-package-versions/src/PrettyVersions.php',
     ] + $symfonyPolyfillAllowlist,
     'whitelist' => [
         // needed for autoload, that is not prefixed, since it's in bin/* file
@@ -34,5 +34,15 @@ return [
         'Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator',
         'Symfony\Component\DependencyInjection\Extension\ExtensionInterface',
         'Symfony\Polyfill\*',
+    ],
+    'patchers' => [
+        function (string $filePath, string $prefix, string $content): string {
+            if (! Strings::endsWith($filePath, 'vendor/jean85/pretty-package-versions/src/PrettyVersions.php')) {
+                return $content;
+            }
+
+            // see https://regex101.com/r/v8zRMm/1
+            return Strings::replace($content, '#' . $prefix . '\\\\Composer\\\\InstalledVersions#', 'Composer\InstalledVersions');
+        },
     ],
 ];
