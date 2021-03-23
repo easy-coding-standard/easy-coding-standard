ab22e7fc6973f7c171114679062c1e3c2405b9f2
kaizen-ci <info@kaizen-ci.org>
2021-03-23 14:51:10 +0000

    [ECS] Unprefix class InstalledVersions

--- scoper.inc.php
+++ scoper.inc.php
@@ -5,26 +5,24 @@ declare(strict_types=1);
 use Nette\Utils\Strings;
 use Isolated\Symfony\Component\Finder\Finder;
 
-$symfonyPolyfillPhpFilesFinder = Finder::create();
-$symfonyPolyfillPhpFilesFinder = $symfonyPolyfillPhpFilesFinder->files();
-$symfonyPolyfillPhpFilesFinder = $symfonyPolyfillPhpFilesFinder->in(__DIR__ . '/vendor/symfony/polyfill-*');
-$symfonyPolyfillPhpFilesFinder = $symfonyPolyfillPhpFilesFinder->name('*.php');
+$polyfillFilesFinder = (new Finder)
+    ->files()
+    ->in(__DIR__ . '/vendor/symfony/polyfill-*')
+    ->name('*.php');
 
-$symfonyPolyfillPhpFilesArray = \iterator_to_array($symfonyPolyfillPhpFilesFinder);
-$symfonyPolyfillPhpFilenames = \array_values($symfonyPolyfillPhpFilesArray);
-$symfonyPolyfillAllowlist = \array_map(
-    static function ($file) {
-        return $file->getPathName();
-    },
-    $symfonyPolyfillPhpFilenames
-);
+$polyfillFilePaths = [];
+foreach ($polyfillFilesFinder->getIterator() as $fileInfo) {
+    $polyfillFilePaths[] = $fileInfo->getPathname();
+}
 
 return [
     'files-whitelist' => [
         // do not prefix "trigger_deprecation" from symfony - https://github.com/symfony/symfony/commit/0032b2a2893d3be592d4312b7b098fb9d71aca03
         // these paths are relative to this file location, so it should be in the root directory
         'vendor/symfony/deprecation-contracts/function.php',
-    ] + $symfonyPolyfillAllowlist,
+        // for package versions - https://github.com/symplify/easy-coding-standard-prefixed/runs/2176047833
+        'Composer\InstalledVersions',
+    ] + $polyfillFilePaths,
     'whitelist' => [
         // needed for autoload, that is not prefixed, since it's in bin/* file
         'Symplify\*',
