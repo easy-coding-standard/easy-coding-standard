bfa7b241e1e724947f86be83a5d72b98abf58965
kaizen-ci <info@kaizen-ci.org>
2021-04-26 22:17:58 +0000

    [ECS] Add test for #3113 (#3146)

---
+++ tests/Issues/Issue3113Test.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\EasyCodingStandard\Tests\Issues;
+
+use Iterator;
+use Symplify\EasyCodingStandardTester\Testing\AbstractCheckerTestCase;
+use Symplify\SmartFileSystem\SmartFileInfo;
+
+/**
+ * @see https://github.com/symplify/symplify/issues/3113
+ */
+final class Issue3113Test extends AbstractCheckerTestCase
+{
+    /**
+     * @dataProvider provideData()
+     */
+    public function test(SmartFileInfo $fileInfo): void
+    {
+        $this->doTestFileInfo($fileInfo);
+    }
+
+    /**
+     * @return Iterator<SmartFileInfo[]>
+     */
+    public function provideData(): Iterator
+    {
+        yield [new SmartFileInfo(__DIR__ . '/Fixture/fixture3113.php.inc')];
+    }
+
+    protected function provideConfig(): string
+    {
+        return __DIR__ . '/config/config3113.php';
+    }
+}
---
+++ tests/Issues/config/config3113.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    $containerConfigurator->import(SetList::ARRAY);
+};
