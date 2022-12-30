77ede05b000622cd4aecc28af811fa47cfb209af
Tomas Votruba <tomas.vot@gmail.com>
2022-12-30 13:08:45 +0100

    update full_ecs_build

--- full_ecs_build.sh
+++ full_ecs_build.sh
@@ -4,8 +4,10 @@ composer update --no-progress --ansi
 
 rm -rf tests packages-tests
 
-# downgrade
-vendor/bin/rector process . --config build/config/config-downgrade.php --ansi
+# downgrade with rector
+mkdir rector-local
+composer require rector/rector --working-dir rector-local
+rector-local/vendor/bin/rector process . --config build/rector-downgrade-php-72.php --ansi
 
 # prefix
 sh build/build-ecs-scoped.sh . .
