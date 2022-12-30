2b18e323d8f3b016ccd330af17c2bc64682ce5ab
Tomas Votruba <tomas.vot@gmail.com>
2022-12-30 13:15:13 +0100

    update

--- full_ecs_build.sh
+++ full_ecs_build.sh
@@ -11,7 +11,7 @@ rm -rf tests packages-tests
 # downgrade with rector
 mkdir rector-local
 composer require rector/rector --working-dir rector-local
-rector-local/vendor/bin/rector process . --config build/rector-downgrade-php-72.php --ansi
+rector-local/vendor/bin/rector process bin src packages vendor --config build/rector-downgrade-php-72.php --ansi
 
 # prefix
 sh build/build-ecs-scoped.sh . .
