efef2606b8c1b9117aeb9d547c582eb12d66bc0a
GitHub Action <action@github.com>
2023-01-11 14:42:18 +0000

    release PHP 7.2 downgraded

---
+++ vendor/friendsofphp/php-cs-fixer/src/Fixer/Phpdoc/PhpdocNoAliasTagFixer.php
@@ -0,0 +1,96 @@
+<?php
+
+declare (strict_types=1);
+/*
+ * This file is part of PHP CS Fixer.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *     Dariusz Rumiński <dariusz.ruminski@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+namespace PhpCsFixer\Fixer\Phpdoc;
+
+use PhpCsFixer\AbstractProxyFixer;
+use PhpCsFixer\ConfigurationException\InvalidConfigurationException;
+use PhpCsFixer\ConfigurationException\InvalidFixerConfigurationException;
+use PhpCsFixer\Fixer\ConfigurableFixerInterface;
+use PhpCsFixer\FixerConfiguration\FixerConfigurationResolver;
+use PhpCsFixer\FixerConfiguration\FixerConfigurationResolverInterface;
+use PhpCsFixer\FixerConfiguration\FixerOptionBuilder;
+use PhpCsFixer\FixerDefinition\CodeSample;
+use PhpCsFixer\FixerDefinition\FixerDefinition;
+use PhpCsFixer\FixerDefinition\FixerDefinitionInterface;
+use PhpCsFixer\Preg;
+/**
+ * Case-sensitive tag replace fixer (does not process inline tags like {@inheritdoc}).
+ *
+ * @author Graham Campbell <hello@gjcampbell.co.uk>
+ * @author Dariusz Rumiński <dariusz.ruminski@gmail.com>
+ */
+final class PhpdocNoAliasTagFixer extends AbstractProxyFixer implements ConfigurableFixerInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getDefinition() : FixerDefinitionInterface
+    {
+        return new FixerDefinition('No alias PHPDoc tags should be used.', [new CodeSample('<?php
+/**
+ * @property string $foo
+ * @property-read string $bar
+ *
+ * @link baz
+ */
+final class Example
+{
+}
+'), new CodeSample('<?php
+/**
+ * @property string $foo
+ * @property-read string $bar
+ *
+ * @link baz
+ */
+final class Example
+{
+}
+', ['replacements' => ['link' => 'website']])]);
+    }
+    /**
+     * {@inheritdoc}
+     *
+     * Must run before PhpdocAddMissingParamAnnotationFixer, PhpdocAlignFixer, PhpdocSingleLineVarSpacingFixer.
+     * Must run after AlignMultilineCommentFixer, CommentToPhpdocFixer, PhpdocIndentFixer, PhpdocScalarFixer, PhpdocToCommentFixer, PhpdocTypesFixer.
+     */
+    public function getPriority() : int
+    {
+        return parent::getPriority();
+    }
+    public function configure(array $configuration) : void
+    {
+        parent::configure($configuration);
+        /** @var GeneralPhpdocTagRenameFixer $generalPhpdocTagRenameFixer */
+        $generalPhpdocTagRenameFixer = $this->proxyFixers['general_phpdoc_tag_rename'];
+        try {
+            $generalPhpdocTagRenameFixer->configure(['fix_annotation' => \true, 'fix_inline' => \false, 'replacements' => $this->configuration['replacements'], 'case_sensitive' => \true]);
+        } catch (InvalidConfigurationException $exception) {
+            throw new InvalidFixerConfigurationException($this->getName(), Preg::replace('/^\\[.+?\\] /', '', $exception->getMessage()), $exception);
+        }
+    }
+    /**
+     * {@inheritdoc}
+     */
+    protected function createConfigurationDefinition() : FixerConfigurationResolverInterface
+    {
+        return new FixerConfigurationResolver([(new FixerOptionBuilder('replacements', 'Mapping between replaced annotations with new ones.'))->setAllowedTypes(['array'])->setDefault(['property-read' => 'property', 'property-write' => 'property', 'type' => 'var', 'link' => 'see'])->getOption()]);
+    }
+    /**
+     * {@inheritdoc}
+     */
+    protected function createProxyFixers() : array
+    {
+        return [new \PhpCsFixer\Fixer\Phpdoc\GeneralPhpdocTagRenameFixer()];
+    }
+}
---
+++ vendor/symplify/easy-parallel/src/ValueObject/EasyParallelConfig.php
@@ -0,0 +1,12 @@
+<?php
+
+declare (strict_types=1);
+namespace ECSPrefix202301\Symplify\EasyParallel\ValueObject;
+
+final class EasyParallelConfig
+{
+    /**
+     * @var string
+     */
+    public const FILE_PATH = __DIR__ . '/../../config/config.php';
+}
