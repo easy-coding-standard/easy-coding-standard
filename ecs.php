7e6d3d6ab6a456e4eb638eb620c7100f44d9210a
Tomas Votruba <tomas.vot@gmail.com>
2023-01-21 14:42:48 +0000

    add basic mb config

--- ecs.php
+++ ecs.php
@@ -10,6 +10,7 @@ return static function (ECSConfig $ecsConfig): void {
     $ecsConfig->paths([
         __DIR__ . '/ecs.php',
         __DIR__ . '/rector.php',
+        __DIR__ . '/monorepo-builder.php',
         __DIR__ . '/config',
         __DIR__ . '/src',
         __DIR__ . '/packages',
