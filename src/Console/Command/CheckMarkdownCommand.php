b1e155c10df8cc52fd3e3ba44d6276dc41d9966c
Tomas Votruba <tomas.vot@gmail.com>
2023-01-30 11:28:28 +0000

    add BC info about check-markdown command

---
+++ src/Console/Command/CheckMarkdownCommand.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\EasyCodingStandard\Console\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+final class CheckMarkdownCommand extends Command
+{
+    public function __construct(
+        private readonly SymfonyStyle $symfonyStyle
+    ) {
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        $this->setName('check-markdown');
+        $this->setDescription('[DEPRECATED] Check markdown files');
+        parent::configure();
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $this->symfonyStyle->warning(
+            'This command is deprecated. Use "ecs check" or direct php-parser printer instead.'
+        );
+
+        return self::FAILURE;
+    }
+}
