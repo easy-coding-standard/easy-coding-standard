9e7c9a8225e65b7920846cdcc88a1991003fe1aa
Tomas Votruba <tomas.vot@gmail.com>
2023-01-30 11:29:01 +0000

    add paths

--- src/Console/Command/CheckMarkdownCommand.php
+++ src/Console/Command/CheckMarkdownCommand.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Symplify\EasyCodingStandard\Console\Command;
 
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
@@ -21,6 +22,9 @@ final class CheckMarkdownCommand extends Command
     {
         $this->setName('check-markdown');
         $this->setDescription('[DEPRECATED] Check markdown files');
+
+        $this->addArgument('paths', InputArgument::REQUIRED | InputArgument::IS_ARRAY, 'Paths to check');
+
         parent::configure();
     }
 
