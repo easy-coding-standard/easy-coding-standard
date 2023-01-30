7653943e87b724472494f98df219b9f6c37a4548
Tomas Votruba <tomas.vot@gmail.com>
2023-01-30 11:04:16 +0000

    add config initializer for easier 1st run

--- src/Console/Command/InitCommand.php
+++ src/Console/Command/InitCommand.php
@@ -4,12 +4,21 @@ declare(strict_types=1);
 
 namespace Symplify\EasyCodingStandard\Console\Command;
 
+use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symplify\PackageBuilder\Console\Command\AbstractSymplifyCommand;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Filesystem\Filesystem;
 
-final class InitCommand extends AbstractSymplifyCommand
+final class InitCommand extends Command
 {
+    public function __construct(
+        private readonly Filesystem $filesystem,
+        private readonly SymfonyStyle $symfonyStyle,
+    ) {
+        parent::__construct();
+    }
+
     protected function configure(): void
     {
         $this->setName('init');
@@ -18,10 +27,11 @@ final class InitCommand extends AbstractSymplifyCommand
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $rectorConfigFiles = $this->smartFileSystem->exists(getcwd() . '/ecs.php');
+        $doesConfigExists = $this->filesystem->exists(getcwd() . '/ecs.php');
 
-        if (! $rectorConfigFiles) {
-            $this->smartFileSystem->copy(__DIR__ . '/../../../templates/ecs.php.dist', getcwd() . '/ecs.php');
+        // @todo figure out a better versoin
+        if (! $doesConfigExists) {
+            $this->filesystem->copy(__DIR__ . '/../../../templates/ecs.php.dist', getcwd() . '/ecs.php');
             $this->symfonyStyle->success('ecs.php config file has been generated successfully');
         } else {
             $this->symfonyStyle->warning('The "ecs.php" configuration file already exists');
