d6ce952d166d0cca1d5a73f2af3b47c501e9466c
Tomas Votruba <tomas.vot@gmail.com>
2023-02-20 10:20:40 +0000

    Add support for dynamic sets from PHP CS Fixer

---
+++ src/ValueObject/Set/PHPCSFixerDynamicSetList.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\EasyCodingStandard\ValueObject\Set;
+
+/**
+ * @api
+ */
+final class PHPCSFixerDynamicSetList
+{
+    /**
+     * @var string
+     */
+    public const SYMFONY = '@Symfony';
+
+    /**
+     * @var string
+     */
+    public const SYMFONY_RISKY = '@Symfony:risky';
+}
