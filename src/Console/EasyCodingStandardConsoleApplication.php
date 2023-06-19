34d84552c430b7bf8d9777858f7a704a01a6f5a0
Tomas Votruba <tomas.vot@gmail.com>
2023-06-19 11:55:54 +0200

    cs

--- src/Console/EasyCodingStandardConsoleApplication.php
+++ src/Console/EasyCodingStandardConsoleApplication.php
@@ -6,7 +6,6 @@ namespace Symplify\EasyCodingStandard\Console;
 
 use Composer\XdebugHandler\XdebugHandler;
 use Symfony\Component\Console\Application;
-use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputDefinition;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
