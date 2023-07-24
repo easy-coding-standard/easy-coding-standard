18a473a485737075f5fa61d4e48a005cbc374d23
Tomas Votruba <tomas.vot@gmail.com>
2023-07-24 16:44:58 +0200

    fix hashing of container based on included files

--- packages/Caching/FileHashComputer.php
+++ packages/Caching/FileHashComputer.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 namespace Symplify\EasyCodingStandard\Caching;
 
+use Symplify\EasyCodingStandard\Config\ECSConfig;
 use Symplify\EasyCodingStandard\DependencyInjection\SimpleParameterProvider;
 use Symplify\EasyCodingStandard\Exception\Configuration\FileNotFoundException;
+use Webmozart\Assert\Assert;
 
 /**
  * @see \Symplify\EasyCodingStandard\Tests\ChangedFilesDetector\FileHashComputer\FileHashComputerTest
@@ -14,7 +16,15 @@ final class FileHashComputer
 {
     public function computeConfig(string $filePath): string
     {
-        return $this->compute($filePath) . SimpleParameterProvider::hash();
+
+        $callable = require $filePath;
+        Assert::isCallable($callable);
+
+        $ecsConfig = new ECSConfig();
+        $callable($ecsConfig);
+        $fileHash = sha1(json_encode($ecsConfig->getBindings()));
+
+        return $fileHash . SimpleParameterProvider::hash();
     }
 
     public function compute(string $filePath): string
