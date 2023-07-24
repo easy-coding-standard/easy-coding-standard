4b3ac83b26653bd2f84c240bba575df13019c709
Tomas Votruba <tomas.vot@gmail.com>
2023-07-24 17:46:51 +0200

    misc

--- tests/DependencyInjection/ExcludedCheckers/ExcludedCheckersTest.php
+++ tests/DependencyInjection/ExcludedCheckers/ExcludedCheckersTest.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Symplify\EasyCodingStandard\Tests\DependencyInjection;
+namespace Symplify\EasyCodingStandard\Tests\DependencyInjection\ExcludedCheckers;
 
 use Symplify\EasyCodingStandard\FixerRunner\Application\FixerFileProcessor;
 use Symplify\EasyCodingStandard\Tests\Testing\AbstractTestCase;
@@ -11,7 +11,7 @@ final class ExcludedCheckersTest extends AbstractTestCase
 {
     public function test(): void
     {
-        $this->createContainerWithConfigs([__DIR__ . '/ExcludedCheckersSource/config.php']);
+        $this->createContainerWithConfigs([__DIR__ . '/config/config.php']);
 
         $fixerFileProcessor = $this->make(FixerFileProcessor::class);
         $this->assertCount(0, $fixerFileProcessor->getCheckers());
