b53696c8073875e0a6fa361a29b5175b4d59f2e2
Tomas Votruba <tomas.vot@gmail.com>
2023-07-25 18:02:04 +0200

    add reflection accessible for PHP 7.2

--- src/Utils/PrivatesAccessorHelper.php
+++ src/Utils/PrivatesAccessorHelper.php
@@ -11,6 +11,8 @@ final class PrivatesAccessorHelper
     public static function getPropertyValue(object $object, string $propertyName): mixed
     {
         $reflectionProperty = new ReflectionProperty($object, $propertyName);
+        // this is needed for PHP 7.2 downgrade code
+        $reflectionProperty->setAccessible(true);
 
         return $reflectionProperty->getValue($object);
     }
@@ -18,6 +20,9 @@ final class PrivatesAccessorHelper
     public static function setPropertyValue(object $object, string $propertyName, mixed $value): void
     {
         $reflectionProperty = new ReflectionProperty($object, $propertyName);
+        // this is needed for PHP 7.2 downgrade code
+        $reflectionProperty->setAccessible(true);
+
         $reflectionProperty->setValue($object, $value);
     }
 }
