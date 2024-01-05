709edabf9751d4f72fa3d5680100bf79413e345e
Tomas Votruba <tomas.vot@gmail.com>
2024-01-05 13:52:13 +0100

    No "strict" set in "common" (#152)
    * move is null to contorl-structures set, as not risky
    
    * fixup! move is null to contorl-structures set, as not risky
    
    * remove strict set from common ones, as risky and should be used in explicit way

--- config/set/common/strict.php
+++ config/set/common/strict.php
@@ -2,17 +2,11 @@
 
 declare(strict_types=1);
 
-use PhpCsFixer\Fixer\LanguageConstruct\IsNullFixer;
 use PhpCsFixer\Fixer\Strict\DeclareStrictTypesFixer;
 use PhpCsFixer\Fixer\Strict\StrictComparisonFixer;
 use PhpCsFixer\Fixer\Strict\StrictParamFixer;
 use Symplify\EasyCodingStandard\Config\ECSConfig;
 
 return static function (ECSConfig $ecsConfig): void {
-    $ecsConfig->rules([
-        StrictComparisonFixer::class,
-        IsNullFixer::class,
-        StrictParamFixer::class,
-        DeclareStrictTypesFixer::class,
-    ]);
+    $ecsConfig->rules([StrictComparisonFixer::class, StrictParamFixer::class, DeclareStrictTypesFixer::class]);
 };
