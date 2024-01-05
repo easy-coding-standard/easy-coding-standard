d082c6f90e83ae85f7e63b03ea06780dcf765834
GitHub Action <action@github.com>
2024-01-05 13:20:55 +0000

    release PHP 7.2 downgraded

--- config/set/common.php
+++ config/set/common.php
@@ -1,19 +1,10 @@
 <?php
 
-declare(strict_types=1);
+declare (strict_types=1);
+namespace ECSPrefix202401;
 
 use Symplify\EasyCodingStandard\Config\ECSConfig;
 use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
-
-return static function (ECSConfig $ecsConfig): void {
-    $ecsConfig->sets([
-        SetList::ARRAY,
-        SetList::COMMENTS,
-        SetList::CONTROL_STRUCTURES,
-        SetList::DOCBLOCK,
-        SetList::NAMESPACES,
-        SetList::PHPUNIT,
-        SetList::SPACES,
-        SetList::CLEAN_CODE,
-    ]);
+return static function (ECSConfig $ecsConfig) : void {
+    $ecsConfig->sets([SetList::ARRAY, SetList::COMMENTS, SetList::CONTROL_STRUCTURES, SetList::DOCBLOCK, SetList::NAMESPACES, SetList::PHPUNIT, SetList::SPACES, SetList::CLEAN_CODE]);
 };
