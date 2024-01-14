b6a56ccdc090bba04d8f132dc5dc2a2ca83911a7
Tomas Votruba <tomas.vot@gmail.com>
2024-01-14 18:53:04 +0100

    downgrade config too

--- full_ecs_build.sh
+++ full_ecs_build.sh
@@ -12,7 +12,7 @@ rm -rf tests vendor/phpcsstandards/php_codesniffer/tests vendor/phpcsstandards/p
 # downgrade with rector
 mkdir rector-local
 composer require rector/rector --working-dir rector-local
-rector-local/vendor/bin/rector process bin src vendor --config build/rector-downgrade-php-72.php --ansi
+rector-local/vendor/bin/rector process bin config/config.php src vendor --config build/rector-downgrade-php-72.php --ansi
 
 # prefix
 sh prefix-code.sh
