11600af107980c788bc6c8be5aa4dfaff9d81999
Abdul Malik Ikhsan <samsonasik@gmail.com>
2024-05-18 09:11:19 +0700

    skip scoper.php

--- scoper.php
+++ scoper.php
@@ -1,9 +1,9 @@
 <?php
 
 declare(strict_types=1);
+
 use Isolated\Symfony\Component\Finder\Finder;
 use Nette\Utils\Strings;
-use PHPUnit\Framework\TestCase;
 
 require __DIR__ . '/vendor/autoload.php';
 
@@ -108,7 +108,7 @@ return [
         },
 
         // fixes https://github.com/symplify/symplify/issues/3205
-        static function (string $filePath, string $prefix, string $content): string {
+        function (string $filePath, string $prefix, string $content): string {
             if (
                 ! str_ends_with($filePath, 'src/Testing/PHPUnit/AbstractCheckerTestCase.php') &&
                 ! str_ends_with($filePath, 'src/Testing/PHPUnit/AbstractTestCase.php')
@@ -119,17 +119,18 @@ return [
             return Strings::replace(
                 $content,
                 '#' . $prefix . '\\\\PHPUnit\\\\Framework\\\\TestCase#',
-                TestCase::class
+                'PHPUnit\Framework\TestCase'
             );
         },
 
         // add static versions constant values
-        static function (string $filePath, string $prefix, string $content): string {
+        function (string $filePath, string $prefix, string $content): string {
             if (! str_ends_with($filePath, 'src/Application/Version/StaticVersionResolver.php')) {
                 return $content;
             }
 
             $releaseDateTime = StaticVersionResolver::resolverReleaseDateTime();
+
             return strtr($content, [
                 '@package_version@' => StaticVersionResolver::resolvePackageVersion(),
                 '@release_date@' => $releaseDateTime->format('Y-m-d H:i:s'),
