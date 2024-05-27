1439d1df3960d8f6cc8880d36630be7666b0641f
Kenneth Sills <132029135+Kenneth-Sills@users.noreply.github.com>
2024-05-27 23:06:59 +0000

    test(formatters): fix incorrect paths (and fingerprints) for Gitlab formatter
    These should have been relative, but they were absolute. There's no correctness
    issue in the formatter itself, it just repeats the paths it's given. But test
    cases should generally be correct regardless.

--- tests/Console/Output/GitlabOutputFormatterTest.php
+++ tests/Console/Output/GitlabOutputFormatterTest.php
@@ -10,6 +10,7 @@ use PHPUnit\Framework\Attributes\Depends;
 use Symplify\CodingStandard\Fixer\LineLength\LineLengthFixer;
 use Symplify\EasyCodingStandard\Console\Formatter\ColorConsoleDiffFormatter;
 use Symplify\EasyCodingStandard\Console\Output\GitlabOutputFormatter;
+use Symplify\EasyCodingStandard\FileSystem\StaticRelativeFilePathHelper;
 use Symplify\EasyCodingStandard\SniffRunner\ValueObject\Error\CodingStandardError;
 use Symplify\EasyCodingStandard\Testing\PHPUnit\AbstractTestCase;
 use Symplify\EasyCodingStandard\ValueObject\Configuration;
@@ -33,12 +34,129 @@ final class GitlabOutputFormatterTest extends AbstractTestCase
         $this->differ = $this->make(DifferInterface::class);
     }
 
+    public function testGracefullyHandlesNoIssues(): void
+    {
+        $configuration = new Configuration();
+        $filePathForExpectedOutput = $this->path('/Fixture/gitlab/no_issues.json');
+
+        $errorAndDiffResult = new ErrorAndDiffResult([], [], []);
+
+        $this->assertJsonStringEqualsJsonFile(
+            $filePathForExpectedOutput,
+            $this->gitlabOutputFormatter->generateReport($errorAndDiffResult, $configuration),
+        );
+    }
+
+    public function testReportsErrorsInTheRightFormat(): void
+    {
+        $configuration = new Configuration();
+        $filePathForExpectedOutput = $this->path('/Fixture/gitlab/only_errors.json');
+
+        [$simulatedErrors] = $this->getMockedIssues();
+
+        $errorAndDiffResult = new ErrorAndDiffResult($simulatedErrors, [], []);
+        $output = $this->gitlabOutputFormatter->generateReport($errorAndDiffResult, $configuration);
+
+        $this->assertJsonStringEqualsJsonFile($filePathForExpectedOutput, $output);
+    }
+
+    public function testReportsFixesInTheRightFormat(): void
+    {
+        $configuration = new Configuration();
+        $filePathForChanges = $this->path('/Source/RandomFileWithEdits.php');
+        $filePathForExpectedOutput = $this->path('/Fixture/gitlab/only_fixes.json');
+
+        [$_, $simulatedFixes] = $this->getMockedIssues($filePathForChanges);
+
+        $errorAndDiffResult = new ErrorAndDiffResult([], $simulatedFixes, []);
+        $output = $this->gitlabOutputFormatter->generateReport($errorAndDiffResult, $configuration);
+
+        $this->assertJsonStringEqualsJsonFile($filePathForExpectedOutput, $output);
+    }
+
+    public function testReportsErrorsAndFixesByDefault(): void
+    {
+        $configuration = new Configuration();
+        $filePathForChanges = $this->path('/Source/RandomFileWithEdits.php');
+        $filePathForExpectedOutput = $this->path('/Fixture/gitlab/errors_and_fixes.json');
+
+        [$simulatedErrors, $simulatedFixes] = $this->getMockedIssues($filePathForChanges);
+
+        $errorAndDiffResult = new ErrorAndDiffResult($simulatedErrors, $simulatedFixes, []);
+        $output = $this->gitlabOutputFormatter->generateReport($errorAndDiffResult, $configuration);
+
+        $this->assertJsonStringEqualsJsonFile($filePathForExpectedOutput, $output);
+    }
+
+    public function testReportsOnlyErrorsWithNoDiffsFlag(): void
+    {
+        $configuration = new Configuration(showDiffs: false);
+        $filePathForChanges = $this->path('/Source/RandomFileWithEdits.php');
+        $filePathForExpectedOutput = $this->path('/Fixture/gitlab/only_errors.json');
+
+        [$simulatedErrors, $simulatedFixes] = $this->getMockedIssues($filePathForChanges);
+
+        $errorAndDiffResult = new ErrorAndDiffResult($simulatedErrors, $simulatedFixes, []);
+        $output = $this->gitlabOutputFormatter->generateReport($errorAndDiffResult, $configuration);
+
+        $this->assertJsonStringEqualsJsonFile($filePathForExpectedOutput, $output);
+    }
+
+    public function testReportsOnlyErrorsWithFixFlag(): void
+    {
+        $configuration = new Configuration(isFixer: true);
+        $filePathForChanges = $this->path('/Source/RandomFileWithEdits.php');
+        $filePathForExpectedOutput = $this->path('/Fixture/gitlab/only_errors.json');
+
+        [$simulatedErrors, $simulatedFixes] = $this->getMockedIssues($filePathForChanges);
+
+        $errorAndDiffResult = new ErrorAndDiffResult($simulatedErrors, $simulatedFixes, []);
+        $output = $this->gitlabOutputFormatter->generateReport($errorAndDiffResult, $configuration);
+
+        $this->assertJsonStringEqualsJsonFile($filePathForExpectedOutput, $output);
+    }
+
+    #[Depends('testReportsFixesInTheRightFormat')]
+    public function testIssueFingerpintsDoNotChangeFromSimpleLineOffsets(): void
+    {
+        $configuration = new Configuration();
+        $filePathForOriginal = $this->path('/Source/RandomFileWithSimpleOffset.php');
+        $mockedFilePathForOriginal = $this->path('/Source/RandomFile.php');
+        $filePathForChanges = $this->path('/Source/RandomFileWithEditsAndSimpleOffset.php');
+        $filePathForExpectedOutput = $this->path('/Fixture/gitlab/only_fixes_with_offset.json');
+
+        $diff = $this->differ->diff(
+            file_get_contents($filePathForOriginal) ?: 'ERROR 1',
+            file_get_contents($filePathForChanges) ?: 'ERROR 2',
+        );
+
+        // We need to mock the filepath because it's used as fingerprint material.
+        $simulatedFixes = [
+            new FileDiff(
+                $mockedFilePathForOriginal,
+                $diff,
+                $this->colorConsoleDiffFormatter->format($diff),
+                [LineLengthFixer::class],
+            ),
+        ];
+
+        $errorAndDiffResult = new ErrorAndDiffResult([], $simulatedFixes, []);
+        $output = $this->gitlabOutputFormatter->generateReport($errorAndDiffResult, $configuration);
+
+        $this->assertJsonStringEqualsJsonFile($filePathForExpectedOutput, $output);
+    }
+
+    private function path(string $path): string
+    {
+        return StaticRelativeFilePathHelper::resolveFromCwd(__DIR__ . $path);
+    }
+
     /**
      * @return array{CodingStandardError[], FileDiff[]}
      */
-    public function getMockedIssues(string $filePathForChanges = null): array
+    private function getMockedIssues(string $filePathForChanges = null): array
     {
-        $filePathForOriginal = __DIR__ . '/Source/RandomFile.php';
+        $filePathForOriginal = $this->path('/Source/RandomFile.php');
 
         $simulatedErrors = [
             new CodingStandardError(3, 'This is a test', LineLengthSniff::class, $filePathForOriginal),
@@ -64,117 +182,5 @@ final class GitlabOutputFormatterTest extends AbstractTestCase
         ];
 
         return [$simulatedErrors, $simulatedFixes];
-    }
-
-    public function testGracefullyHandlesNoIssues(): void
-    {
-        $configuration = new Configuration();
-        $filePathForExpectedOutput = __DIR__ . '/Fixture/gitlab/no_issues.json';
-
-        $errorAndDiffResult = new ErrorAndDiffResult([], [], []);
-
-        $this->assertJsonStringEqualsJsonFile(
-            $filePathForExpectedOutput,
-            $this->gitlabOutputFormatter->generateReport($errorAndDiffResult, $configuration),
-        );
-    }
-
-    public function testReportsErrorsInTheRightFormat(): void
-    {
-        $configuration = new Configuration();
-        $filePathForExpectedOutput = __DIR__ . '/Fixture/gitlab/only_errors.json';
-
-        [$simulatedErrors] = $this->getMockedIssues();
-
-        $errorAndDiffResult = new ErrorAndDiffResult($simulatedErrors, [], []);
-        $output = $this->gitlabOutputFormatter->generateReport($errorAndDiffResult, $configuration);
-
-        $this->assertJsonStringEqualsJsonFile($filePathForExpectedOutput, $output);
-    }
-
-    public function testReportsFixesInTheRightFormat(): void
-    {
-        $configuration = new Configuration();
-        $filePathForChanges = __DIR__ . '/Source/RandomFileWithEdits.php';
-        $filePathForExpectedOutput = __DIR__ . '/Fixture/gitlab/only_fixes.json';
-
-        [$_, $simulatedFixes] = $this->getMockedIssues($filePathForChanges);
-
-        $errorAndDiffResult = new ErrorAndDiffResult([], $simulatedFixes, []);
-        $output = $this->gitlabOutputFormatter->generateReport($errorAndDiffResult, $configuration);
-
-        $this->assertJsonStringEqualsJsonFile($filePathForExpectedOutput, $output);
-    }
-
-    public function testReportsErrorsAndFixesByDefault(): void
-    {
-        $configuration = new Configuration();
-        $filePathForChanges = __DIR__ . '/Source/RandomFileWithEdits.php';
-        $filePathForExpectedOutput = __DIR__ . '/Fixture/gitlab/errors_and_fixes.json';
-
-        [$simulatedErrors, $simulatedFixes] = $this->getMockedIssues($filePathForChanges);
-
-        $errorAndDiffResult = new ErrorAndDiffResult($simulatedErrors, $simulatedFixes, []);
-        $output = $this->gitlabOutputFormatter->generateReport($errorAndDiffResult, $configuration);
-
-        $this->assertJsonStringEqualsJsonFile($filePathForExpectedOutput, $output);
-    }
-
-    public function testReportsOnlyErrorsWithNoDiffsFlag(): void
-    {
-        $configuration = new Configuration(showDiffs: false);
-        $filePathForChanges = __DIR__ . '/Source/RandomFileWithEdits.php';
-        $filePathForExpectedOutput = __DIR__ . '/Fixture/gitlab/only_errors.json';
-
-        [$simulatedErrors, $simulatedFixes] = $this->getMockedIssues($filePathForChanges);
-
-        $errorAndDiffResult = new ErrorAndDiffResult($simulatedErrors, $simulatedFixes, []);
-        $output = $this->gitlabOutputFormatter->generateReport($errorAndDiffResult, $configuration);
-
-        $this->assertJsonStringEqualsJsonFile($filePathForExpectedOutput, $output);
-    }
-
-    public function testReportsOnlyErrorsWithFixFlag(): void
-    {
-        $configuration = new Configuration(isFixer: true);
-        $filePathForChanges = __DIR__ . '/Source/RandomFileWithEdits.php';
-        $filePathForExpectedOutput = __DIR__ . '/Fixture/gitlab/only_errors.json';
-
-        [$simulatedErrors, $simulatedFixes] = $this->getMockedIssues($filePathForChanges);
-
-        $errorAndDiffResult = new ErrorAndDiffResult($simulatedErrors, $simulatedFixes, []);
-        $output = $this->gitlabOutputFormatter->generateReport($errorAndDiffResult, $configuration);
-
-        $this->assertJsonStringEqualsJsonFile($filePathForExpectedOutput, $output);
-    }
-
-    #[Depends('testReportsFixesInTheRightFormat')]
-    public function testIssueFingerpintsDoNotChangeFromSimpleLineOffsets(): void
-    {
-        $configuration = new Configuration();
-        $filePathForOriginal = __DIR__ . '/Source/RandomFileWithSimpleOffset.php';
-        $mockedFilePathForOriginal = __DIR__ . '/Source/RandomFile.php';
-        $filePathForChanges = __DIR__ . '/Source/RandomFileWithEditsAndSimpleOffset.php';
-        $filePathForExpectedOutput = __DIR__ . '/Fixture/gitlab/only_fixes_with_offset.json';
-
-        $diff = $this->differ->diff(
-            file_get_contents($filePathForOriginal) ?: 'ERROR 1',
-            file_get_contents($filePathForChanges) ?: 'ERROR 2',
-        );
-
-        // We need to mock the filepath because it's used as fingerprint material.
-        $simulatedFixes = [
-            new FileDiff(
-                $mockedFilePathForOriginal,
-                $diff,
-                $this->colorConsoleDiffFormatter->format($diff),
-                [LineLengthFixer::class],
-            ),
-        ];
-
-        $errorAndDiffResult = new ErrorAndDiffResult([], $simulatedFixes, []);
-        $output = $this->gitlabOutputFormatter->generateReport($errorAndDiffResult, $configuration);
-
-        $this->assertJsonStringEqualsJsonFile($filePathForExpectedOutput, $output);
     }
 }
