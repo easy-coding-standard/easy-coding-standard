ed749cd605ff1195e5d0d71f2e01cde8aeeb1320
Kenneth Sills <132029135+Kenneth-Sills@users.noreply.github.com>
2024-05-27 21:10:10 -0400

    Add Gitlab / Code Climate Output Format Support (#210)
    * refactor(formatters): improved discoverability of formatters & their properties
    
    Whether or not a formatter supports progress bars is now encapsulated
    in the formatter class itself, as opposed to being scattered in a couple places
    and relying on maintainers remembering.
    
    Both this progress bar support and the formatter names are also
    implemented as static methods now, whose inclusion can be
    promised by their shared interface.
    
    The console output retains its `::NAME` constant because it needs to be used
    in the default parameters of a configuration constructor (being a default
    setting).
    
    Finally, there remains one hard-coded reference to the `json` output format,
    but only over in the `ListCheckersCommand`. I've left this alone since this
    is not the same type of formatter. Don't want to step on any toes. :)
    
    * feat(formatters): added a Gitlab/Code Climate output formatter
    
    * docs(formatters): added a README section on output formats for clarity
    
    I, personally, didn't realize this tool supported output formatting at first,
    and the existing issues seemed to have been closed without mentioning
    the addition of them. I'd just like to make it more obvious for new users!
    
    * refactor(formatters): ran automated fixers on changed files
    
    * fix(formatters): set gitlab issue severity to 'minor'
    
    My initial line of thinking about setting to 'blocker' was flawed. Not everyone
    uses this tooling the same way and they may want it to show up in the MR
    widget/diffs without _actually_ blocking. Since ECS focuses on code style
    standards instead of correctness issues, this seems an appropriate level.
    
    * refactor(formatters): create separate method to generate report string
    
    This just makes testing easier, as seen in the existing test classes.
    
    * chore: stop linting test fixtures, which may intentionally contain errors
    
    * test(formatters): initial testing for the Gitlab output formatting
    
    This is by no means comprehensive, but it at least confirms it respects
    configurations as expected, formatting is right for trivial cases, and that
    fingerprints are deterministic.
    
    * test(formatters): minor test improvement for Gitlab formatter
    
    - Uncommenting an attribute I disabled for debugging.
    - Added an important clarifying comment.
    
    * test(formatters): fix incorrect paths (and fingerprints) for Gitlab formatter
    
    These should have been relative, but they were absolute. There's no correctness
    issue in the formatter itself, it just repeats the paths it's given. But test
    cases should generally be correct regardless.
    
    ---------
    
    Co-authored-by: Tomas Votruba <tomas.vot@gmail.com>

---
+++ src/Console/Output/GitlabOutputFormatter.php
@@ -0,0 +1,239 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\EasyCodingStandard\Console\Output;
+
+use SebastianBergmann\Diff\Chunk;
+use SebastianBergmann\Diff\Line;
+use SebastianBergmann\Diff\Parser as DiffParser;
+use Symplify\EasyCodingStandard\Console\ExitCode;
+use Symplify\EasyCodingStandard\Console\Style\EasyCodingStandardStyle;
+use Symplify\EasyCodingStandard\Contract\Console\Output\OutputFormatterInterface;
+use Symplify\EasyCodingStandard\SniffRunner\ValueObject\Error\CodingStandardError;
+use Symplify\EasyCodingStandard\ValueObject\Configuration;
+use Symplify\EasyCodingStandard\ValueObject\Error\ErrorAndDiffResult;
+use Symplify\EasyCodingStandard\ValueObject\Error\FileDiff;
+use function array_map as map;
+use function array_merge as merge;
+
+/**
+ * Generates a JSON file containing the Gitlab-supported variant of
+ * "Code Climate" issues for all unresolved errors.
+ *
+ * This is compatible with the specs of:
+ *   - Code Climate: ^0.3.1
+ *   - Gitlab: ^16.11 || ^17
+ *
+ * Applied diffs are NOT reported, but unapplied diffs ARE. This allows
+ * our users to choose between a CI/CD workflow where `--fix` is used to
+ * automatically commit trivial corrections OR one where any errors, even
+ * resolvable, cause the pipeline to fail.
+ *
+ * Unfortunately, because of the way sniffs apply fixes without provenance data
+ * and with cascading effects, multiple major refactors would be required in
+ * order to accurately report exactly which fixes apply to exactly which lines.
+ *
+ * Finally, all reported errors will be marked as "minor" severity, since
+ * DevOps downstream can choose if CI/CD ignores and our Sniffers don't
+ * currently provide their own levels.
+ *
+ * As a warning to future maintainers, I believe Gitlab's documentation may be
+ * slightly wrong. It's not a subset of the Code Climate format as insinuated,
+ * since it changes some fields from optional to required.
+ *
+ * @see https://docs.gitlab.com/ee/ci/testing/code_quality.html#implement-a-custom-tool
+ * @see https://github.com/codeclimate/platform/blob/master/spec/analyzers/SPEC.md#data-types
+ *
+ * @phpstan-type GitlabIssue array{
+ *     type: 'issue',
+ *     description: string,
+ *     check_name: string,
+ *     fingerprint: string,
+ *     severity: 'minor',
+ *     categories: array{'Style'},
+ *     remediation_points?: int,
+ *     location: array{
+ *         path: string,
+ *         lines: array{
+ *             begin: int,
+ *             end: int,
+ *         },
+ *     },
+ * }
+ */
+final readonly class GitlabOutputFormatter implements OutputFormatterInterface
+{
+    public function __construct(
+        private readonly EasyCodingStandardStyle $easyCodingStandardStyle,
+        private readonly ExitCodeResolver $exitCodeResolver,
+        private readonly DiffParser $diffParser,
+    ) {
+    }
+
+    public static function getName(): string
+    {
+        return 'gitlab';
+    }
+
+    public static function hasSupportForProgressBars(): bool
+    {
+        return false;
+    }
+
+    /**
+     * @return ExitCode::*
+     */
+    public function report(ErrorAndDiffResult $errorAndDiffResult, Configuration $configuration): int
+    {
+        $output = $this->generateReport($errorAndDiffResult, $configuration);
+        $this->easyCodingStandardStyle->writeln($output);
+        return $this->exitCodeResolver->resolve($errorAndDiffResult, $configuration);
+    }
+
+    public function generateReport(ErrorAndDiffResult $errorAndDiffResult, Configuration $configuration): string
+    {
+        $reportedQualityIssues = (! $configuration->isFixer() && $configuration->shouldShowDiffs())
+            ? merge(
+                $this->generateIssuesForErrors($errorAndDiffResult->getErrors()),
+                $this->generateIssuesForFixes($errorAndDiffResult->getFileDiffs()),
+            )
+            : $this->generateIssuesForErrors($errorAndDiffResult->getErrors());
+
+        return $this->encode($reportedQualityIssues);
+    }
+
+    /**
+     * @param CodingStandardError[] $errors
+     * @return GitlabIssue[]
+     */
+    private function generateIssuesForErrors(array $errors): array
+    {
+        return map(
+            fn (CodingStandardError $codingStandardError): array => [
+                'type' => 'issue',
+                'description' => $codingStandardError->getMessage(),
+                'check_name' => $codingStandardError->getCheckerClass(),
+                'fingerprint' => $this->generateFingerprint(
+                    $codingStandardError->getCheckerClass(),
+                    $codingStandardError->getMessage(),
+                    $codingStandardError->getRelativeFilePath(),
+                ),
+                'severity' => 'minor',
+                'categories' => ['Style'],
+                'location' => [
+                    'path' => $codingStandardError->getRelativeFilePath(),
+                    'lines' => [
+                        'begin' => $codingStandardError->getLine(),
+                        'end' => $codingStandardError->getLine(),
+                    ],
+                ],
+            ],
+            $errors,
+        );
+    }
+
+    /**
+     * Reports each chunk of changes as a separate issue.
+     *
+     * @param FileDiff[] $diffs
+     * @return GitlabIssue[]
+     */
+    private function generateIssuesForFixes(array $diffs): array
+    {
+        return merge(
+            ...map(
+                fn (FileDiff $fileDiff): array => map(
+                    fn (Chunk $chunk): array => $this->generateIssueForChunk($fileDiff, $chunk),
+                    $this->diffParser->parse($fileDiff->getDiff())[0]->chunks(),
+                ),
+                $diffs,
+            ),
+        );
+    }
+
+    /**
+     * @return GitlabIssue
+     */
+    private function generateIssueForChunk(FileDiff $fileDiff, Chunk $chunk): array
+    {
+        $checkersAsFqcns = implode(',', $fileDiff->getAppliedCheckers());
+        $checkersAsClasses = implode(', ', map(
+            static fn (string $checker): string => preg_replace('/.*\\\/', '', $checker) ?? $checker,
+            $fileDiff->getAppliedCheckers(),
+        ));
+
+        $message = 'Chunk has fixable errors: ' . $checkersAsClasses;
+        $lineStart = $chunk->start();
+        $lineEnd = $lineStart + $chunk->startRange() - 1;
+
+        return [
+            'type' => 'issue',
+            'description' => $message,
+            'check_name' => $checkersAsFqcns,
+            'fingerprint' => $this->generateFingerprint(
+                $checkersAsFqcns,
+                $message,
+                $fileDiff->getRelativeFilePath(),
+                implode(
+                    '\n',
+                    map(static fn (Line $line): string => sprintf(
+                        '%d:%s',
+                        $line->type(),
+                        $line->content()
+                    ), $chunk->lines())
+                ),
+            ),
+            'severity' => 'minor',
+            'categories' => ['Style'],
+            'remediation_points' => 50_000,
+            'location' => [
+                'path' => $fileDiff->getRelativeFilePath(),
+                'lines' => [
+                    'begin' => $lineStart,
+                    'end' => $lineEnd,
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * Generate a fingerprint for a given quality issue. This is used to
+     * track the presence of an issue between runs, so it should be unique
+     * and consistent for a given issue.
+     *
+     * Subsequently, changing the fingerprint or the data it uses is
+     * _technically_ a breaking change. Users would see existing issues being
+     * marked as "new" on the commit proceeding updating ECS.
+     *
+     * DO NOT include position information as salting, or every time
+     * lines are added/removed the lines below it will be reported as
+     * new errors.
+     */
+    private function generateFingerprint(
+        string $checker,
+        string $message,
+        string $relativeFilePath,
+        string $salt = '',
+    ): string {
+        // We implode to add a separator that cannot show up in PHP
+        // class names or Linux file names and SHOULD  never show up in
+        // messages. This guarantees the same fingerprint won't be generated
+        // by accident, by the associative property of concatenation. As in:
+        //
+        // (ABC + ABC = ABCABC) == (ABCA + BC = ABCABC)
+        // (ABC + \0 + ABC = ABC\0ABC) != (ABCA + \0 + BC = ABCA\0BC)
+        return md5(implode("\0", [$checker, $message, $relativeFilePath, $salt]));
+    }
+
+    /**
+     * @param GitlabIssue[] $lineItems
+     */
+    private function encode(array $lineItems): string
+    {
+        return json_encode(
+            $lineItems,
+            JSON_THROW_ON_ERROR | JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE
+        );
+    }
+}
