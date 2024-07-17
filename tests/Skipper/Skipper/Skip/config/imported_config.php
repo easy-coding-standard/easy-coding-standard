18df81fc432eecb3bdc2b25d025772a1d2ba4464
Greg Korba <greg@codito.dev>
2024-07-17 10:20:42 +0200

    Add failing test case

---
+++ tests/Skipper/Skipper/Skip/config/imported_config.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+use Symplify\EasyCodingStandard\Config\ECSConfig;
+use Symplify\EasyCodingStandard\Tests\Skipper\Skipper\Skip\Source\SomeClassToSkip;
+
+return static function (ECSConfig $config): void {
+    $config->skip([
+        SomeClassToSkip::class => ['Fixture/someDirectory/someFile.php'],
+    ]);
+};
