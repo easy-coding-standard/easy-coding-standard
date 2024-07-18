bdfc3325dfab20d6d27ed2dbf29e240a560a3977
Greg Korba <greg@codito.dev>
2024-07-18 10:48:11 +0200

    Apply Rector fixes

--- tests/Skipper/Skipper/Skip/config/config_with_import.php
+++ tests/Skipper/Skipper/Skip/config/config_with_import.php
@@ -6,10 +6,10 @@ use Symplify\EasyCodingStandard\Config\ECSConfig;
 use Symplify\EasyCodingStandard\Tests\Skipper\Skipper\Skip\Source\AnotherClassToSkip;
 use Symplify\EasyCodingStandard\Tests\Skipper\Skipper\Skip\Source\SomeClassToSkip;
 
-return static function (ECSConfig $config): void {
-    $config->import(__DIR__.'/imported_config.php');
+return static function (ECSConfig $ecsConfig): void {
+    $ecsConfig->import(__DIR__.'/imported_config.php');
 
-    $config->skip([
+    $ecsConfig->skip([
         SomeClassToSkip::class => 'Fixture/AlwaysSkippedPath',
         AnotherClassToSkip::class => null,
     ]);
--- tests/Skipper/Skipper/Skip/config/imported_config.php
+++ tests/Skipper/Skipper/Skip/config/imported_config.php
@@ -6,8 +6,8 @@ use Symplify\EasyCodingStandard\Config\ECSConfig;
 use Symplify\EasyCodingStandard\Tests\Skipper\Skipper\Skip\Source\AnotherClassToSkip;
 use Symplify\EasyCodingStandard\Tests\Skipper\Skipper\Skip\Source\SomeClassToSkip;
 
-return static function (ECSConfig $config): void {
-    $config->skip([
+return static function (ECSConfig $ecsConfig): void {
+    $ecsConfig->skip([
         SomeClassToSkip::class => ['Fixture/someDirectory/someFile.php'],
         AnotherClassToSkip::class => null,
     ]);
