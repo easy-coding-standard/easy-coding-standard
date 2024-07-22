bb03ef51575f893fe6c8933365ac2e4660b143f7
Kenneth Sills <132029135+Kenneth-Sills@users.noreply.github.com>
2024-07-22 01:54:58 +0000

    fix: correctly apply Symfony output flags
    That is, anything set by `Application::configureIO()` in a "normal"
    Symfony Application (we do things a little differently).
    
    Because we construct our styles separately from the application, during
    DI initialization, I had to copy the code that handles Symfony's arguments
    into here. This isn't an exact copy and paste, but given I did start from their
    source and work it into what's committed here, I felt it appropriate to include
    their MIT licensing in the NOTICE.
    
    Closes #213

--- src/Console/Style/EasyCodingStandardStyleFactory.php
+++ src/Console/Style/EasyCodingStandardStyleFactory.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Symplify\EasyCodingStandard\Console\Style;
 
 use Symfony\Component\Console\Input\ArgvInput;
+use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\ConsoleOutput;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Terminal;
@@ -27,6 +28,8 @@ final readonly class EasyCodingStandardStyleFactory
         $argvInput = new ArgvInput();
         $consoleOutput = new ConsoleOutput();
 
+        $this->applySymfonyConsoleArgs($argvInput, $consoleOutput);
+
         // --debug is called
         if ($argvInput->hasParameterOption('--debug')) {
             $consoleOutput->setVerbosity(OutputInterface::VERBOSITY_DEBUG);
@@ -38,5 +41,49 @@ final readonly class EasyCodingStandardStyleFactory
         }
 
         return new EasyCodingStandardStyle($argvInput, $consoleOutput, $this->terminal);
+    }
+
+    /**
+     * This method was derived from the `Application::configureIO` method of the Symfony Console
+     * component, under the MIT license. See NOTICE for the full license.
+     *
+     * @see https://github.com/symfony/console/blob/0aa29ca177f432ab68533432db0de059f39c92ae/Application.php#L905
+     */
+    private function applySymfonyConsoleArgs(InputInterface $input, OutputInterface $output): void
+    {
+        $enableAnsi = $input->hasParameterOption(['--ansi'], true);
+        $disableAnsi = $input->hasParameterOption(['--no-ansi'], true);
+
+        match (true) {
+            $enableAnsi => $output->setDecorated(true),
+            $disableAnsi => $output->setDecorated(false),
+            default => null,
+        };
+
+        $enableQuiet = $input->hasParameterOption(['--quiet', '-q'], true);
+
+        $enableVVV = $input->hasParameterOption('-vvv', true);
+        $enableVV = $input->hasParameterOption('-vv', true);
+        $enableV = $input->hasParameterOption('-v', true);
+
+        $enableVerbose3 = $input->hasParameterOption('--verbose=3', true);
+        $enableVerbose2 = $input->hasParameterOption('--verbose=2', true);
+        $enableVerbose1 = $input->hasParameterOption('--verbose=1', true);
+
+        $isVerbose3 = $input->getParameterOption('--verbose', false, true) === 3;
+        $isVerbose2 = $input->getParameterOption('--verbose', false, true) === 2;
+        $isVerbose1 = $input->getParameterOption('--verbose', false, true) === 1;
+
+        $isDebug = $enableVVV || $enableVerbose3 || $isVerbose3;
+        $isVeryVerbose = $enableVV || $enableVerbose2 || $isVerbose2;
+        $isVerbose = $enableV || $enableVerbose1 || $isVerbose1;
+
+        match (true) {
+            $enableQuiet => $output->setVerbosity(OutputInterface::VERBOSITY_QUIET),
+            $isDebug => $output->setVerbosity(OutputInterface::VERBOSITY_DEBUG),
+            $isVeryVerbose => $output->setVerbosity(OutputInterface::VERBOSITY_VERY_VERBOSE),
+            $isVerbose => $output->setVerbosity(OutputInterface::VERBOSITY_VERBOSE),
+            default => null,
+        };
     }
 }
--- src/Console/Style/SymfonyStyleFactory.php
+++ src/Console/Style/SymfonyStyleFactory.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Symplify\EasyCodingStandard\Console\Style;
 
 use Symfony\Component\Console\Input\ArgvInput;
+use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\ConsoleOutput;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
@@ -23,6 +24,8 @@ final class SymfonyStyleFactory
 
         $argvInput = new ArgvInput();
         $consoleOutput = new ConsoleOutput();
+
+        self::applySymfonyConsoleArgs($argvInput, $consoleOutput);
 
         // --debug is called
         if ($argvInput->hasParameterOption('--debug')) {
@@ -43,5 +46,49 @@ final class SymfonyStyleFactory
     private static function isPHPUnitRun(): bool
     {
         return defined('PHPUNIT_COMPOSER_INSTALL') || defined('__PHPUNIT_PHAR__');
+    }
+
+    /**
+     * This method was derived from the `Application::configureIO` method of the Symfony Console
+     * component, under the MIT license. See NOTICE for the full license.
+     *
+     * @see https://github.com/symfony/console/blob/0aa29ca177f432ab68533432db0de059f39c92ae/Application.php#L905
+     */
+    private static function applySymfonyConsoleArgs(InputInterface $input, OutputInterface $output): void
+    {
+        $enableAnsi = $input->hasParameterOption(['--ansi'], true);
+        $disableAnsi = $input->hasParameterOption(['--no-ansi'], true);
+
+        match (true) {
+            $enableAnsi => $output->setDecorated(true),
+            $disableAnsi => $output->setDecorated(false),
+            default => null,
+        };
+
+        $enableQuiet = $input->hasParameterOption(['--quiet', '-q'], true);
+
+        $enableVVV = $input->hasParameterOption('-vvv', true);
+        $enableVV = $input->hasParameterOption('-vv', true);
+        $enableV = $input->hasParameterOption('-v', true);
+
+        $enableVerbose3 = $input->hasParameterOption('--verbose=3', true);
+        $enableVerbose2 = $input->hasParameterOption('--verbose=2', true);
+        $enableVerbose1 = $input->hasParameterOption('--verbose=1', true);
+
+        $isVerbose3 = $input->getParameterOption('--verbose', false, true) === 3;
+        $isVerbose2 = $input->getParameterOption('--verbose', false, true) === 2;
+        $isVerbose1 = $input->getParameterOption('--verbose', false, true) === 1;
+
+        $isDebug = $enableVVV || $enableVerbose3 || $isVerbose3;
+        $isVeryVerbose = $enableVV || $enableVerbose2 || $isVerbose2;
+        $isVerbose = $enableV || $enableVerbose1 || $isVerbose1;
+
+        match (true) {
+            $enableQuiet => $output->setVerbosity(OutputInterface::VERBOSITY_QUIET),
+            $isDebug => $output->setVerbosity(OutputInterface::VERBOSITY_DEBUG),
+            $isVeryVerbose => $output->setVerbosity(OutputInterface::VERBOSITY_VERY_VERBOSE),
+            $isVerbose => $output->setVerbosity(OutputInterface::VERBOSITY_VERBOSE),
+            default => null,
+        };
     }
 }
