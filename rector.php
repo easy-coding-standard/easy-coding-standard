214bcf250db466fb17b88f6d413812ddbf3290a4
Tomas Votruba <tomas.vot@gmail.com>
2024-12-03 13:49:04 +0100

    bump

--- rector.php
+++ rector.php
@@ -3,19 +3,10 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\Naming\Rector\ClassMethod\RenameParamToMatchTypeRector;
 
 return RectorConfig::configure()
-    ->withPhpSets(php82: true)
-    ->withPreparedSets(
-        codeQuality: true,
-        deadCode: true,
-        codingStyle: true,
-        typeDeclarations: true,
-        naming: true,
-        privatization: true,
-        earlyReturn: true
-    )
+    ->withPhpSets()
+    ->withPreparedSets(codeQuality: true, deadCode: true, codingStyle: true, typeDeclarations: true, naming: true, privatization: true, earlyReturn: true)
     ->withPaths([__DIR__ . '/bin', __DIR__ . '/config', __DIR__ . '/src', __DIR__ . '/config', __DIR__ . '/tests'])
     ->withRootFiles()
     ->withImportNames()
@@ -25,6 +16,4 @@ return RectorConfig::configure()
         '*/Fixture/*',
         __DIR__ . '/src/SniffRunner/ValueObject/File.php',
         __DIR__ . '/scoper.php',
-
-        RenameParamToMatchTypeRector::class => [__DIR__ . '/src/FixerRunner/Application/FixerFileProcessor.php'],
     ]);
