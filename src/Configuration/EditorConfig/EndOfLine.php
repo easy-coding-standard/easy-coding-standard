9341ce3de336f3192a778521438cd6173a2866c8
Abdul Malik Ikhsan <samsonasik@gmail.com>
2025-01-31 20:50:04 +0700

    add back

---
+++ src/Configuration/EditorConfig/EndOfLine.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\EasyCodingStandard\Configuration\EditorConfig;
+
+final class EndOfLine
+{
+    public const Posix = 'lf';
+    public const Legacy = 'cr';
+    public const Windows = 'crlf';
+}
---
+++ src/Configuration/EditorConfig/IndentStyle.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\EasyCodingStandard\Configuration\EditorConfig;
+
+final class IndentStyle
+{
+    public const Space = 'space';
+    public const Tab = 'tab';
+}
