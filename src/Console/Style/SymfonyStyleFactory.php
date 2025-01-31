7a32b9c6041c200aab3daf5ffbd22be33a56776c
Kenneth Sills <132029135+Kenneth-Sills@users.noreply.github.com>
2025-01-31 07:46:30 -0500

    Fix: Correctly Apply Symfony Output Flags (#235)
    * fix: correctly apply Symfony output flags
    
    That is, anything set by `Application::configureIO()` in a "normal"
    Symfony Application (we do things a little differently).
    
    Because we construct our styles separately from the application, during
    DI initialization, I had to copy the code that handles Symfony's arguments
    into here. This isn't an exact copy and paste, but given I did start from their
    source and work it into what's committed here, I felt it appropriate to include
    their MIT licensing in the NOTICE.
    
    Closes #213
    
    * chore: remove code for unsupported feature
    
    Since the numeric `--verbose` doesn't work with Symfony,
    might as well remove the code to handle it.
    
    * Delete NOTICE
    
    ---------
    
    Co-authored-by: Tomas Votruba <tomas.vot@gmail.com>

--- src/Console/Style/SymfonyStyleFactory.php
+++ src/Console/Style/SymfonyStyleFactory.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Symplify\EasyCodingStandard\Console\Style;
 
 use Symfony\Component\Console\Input\ArgvInput;
+use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\ConsoleOutput;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
@@ -23,6 +24,8 @@ final class SymfonyStyleFactory
 
         $argvInput = new ArgvInput();
         $consoleOutput = new ConsoleOutput();
+
+        self::applySymfonyConsoleArgs($argvInput, $consoleOutput);
 
         // --debug is called
         if ($argvInput->hasParameterOption('--debug')) {
@@ -43,5 +46,37 @@ final class SymfonyStyleFactory
     private static function isPHPUnitRun(): bool
     {
         return defined('PHPUNIT_COMPOSER_INSTALL') || defined('__PHPUNIT_PHAR__');
+    }
+
+    /**
+     * This method was derived from the `Application::configureIO` method of the Symfony Console
+     * component, under the MIT license. See NOTICE for the full license.
+     *
+     * @see https://github.com/symfony/console/blob/0aa29ca177f432ab68533432db0de059f39c92ae/Application.php#L905
+     */
+    private static function applySymfonyConsoleArgs(InputInterface $input, OutputInterface $output): void
+    {
+        $enableAnsi = $input->hasParameterOption(['--ansi'], true);
+        $disableAnsi = $input->hasParameterOption(['--no-ansi'], true);
+
+        match (true) {
+            $enableAnsi => $output->setDecorated(true),
+            $disableAnsi => $output->setDecorated(false),
+            default => null,
+        };
+
+        $enableQuiet = $input->hasParameterOption(['--quiet', '-q'], true);
+
+        $isVVV = $input->hasParameterOption('-vvv', true);
+        $isVV = $input->hasParameterOption('-vv', true);
+        $isV = $input->hasParameterOption('-v', true);
+
+        match (true) {
+            $enableQuiet => $output->setVerbosity(OutputInterface::VERBOSITY_QUIET),
+            $isVVV => $output->setVerbosity(OutputInterface::VERBOSITY_DEBUG),
+            $isVV => $output->setVerbosity(OutputInterface::VERBOSITY_VERY_VERBOSE),
+            $isV => $output->setVerbosity(OutputInterface::VERBOSITY_VERBOSE),
+            default => null,
+        };
     }
 }
