f36d7f6cac9e24135a223cad55700da365ddd6f8
Tomas Votruba <tomas.vot@gmail.com>
2025-04-28 08:56:47 +0200

    Bump rector config syntax downgrade

--- build/rector-downgrade-php-72.php
+++ build/rector-downgrade-php-72.php
@@ -3,15 +3,11 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\Set\ValueObject\DowngradeLevelSetList;
 
-return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->parallel();
-    $rectorConfig->sets([DowngradeLevelSetList::DOWN_TO_PHP_72]);
-
-    $rectorConfig->skip([
+return RectorConfig::configure()
+    ->withDowngradeSets(php72: true)
+    ->withSkip([
         '*/Tests/*',
         '*/tests/*',
         '*/Fixtures/DirectoryExpansion/.hiddenAbove/*'
     ]);
-};
