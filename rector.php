4e4cb1c7893133141a5f9b0540e26f3d2ebed86b
Abdul Malik Ikhsan <samsonasik@gmail.com>
2025-05-30 18:26:14 +0700

    Bump to symplify/coding-standard:^12.4.3 (#279)

--- rector.php
+++ rector.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
+use Rector\DeadCode\Rector\ConstFetch\RemovePhpVersionIdCheckRector;
 
 return RectorConfig::configure()
     ->withPhpSets()
@@ -26,5 +27,5 @@ return RectorConfig::configure()
         __DIR__ . '/scoper.php',
 
         // conditional checks
-        \Rector\DeadCode\Rector\ConstFetch\RemovePhpVersionIdCheckRector::class,
+        RemovePhpVersionIdCheckRector::class,
     ]);
